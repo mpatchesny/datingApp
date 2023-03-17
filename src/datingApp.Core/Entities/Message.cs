3496d09369d8fe8f155555cf495a3c8f34c36d53
M. Patchesny <strielok11+github@gmail.com>
2023-03-17 22:11:13 +0100

    add

---
+++ src/datingApp.Core/Entities/Message.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace datingApp.Core.Entities
+{
+    public class Message
+    {
+        public long Id { get; }
+        
+        [Required]
+        public User SendFrom { get; private set; }
+
+        [Required]
+        public User SendTo { get; private set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "message must be maximum 255 characters long")]
+        public string Text {get; private set; }
+
+        [Required]
+        public DateTime CreatedAt { get; private set; }
+
+        public Message(User sendFrom, User sendTo, string text, DateTime createdAt)
+        {
+            SendFrom = sendFrom;
+            SendTo = sendTo;
+            Text = text;
+            CreatedAt = createdAt;
+        }
+    }
+}

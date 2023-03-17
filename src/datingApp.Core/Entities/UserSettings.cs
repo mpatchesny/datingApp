4656a157f6e73f9c3833a7a7a7c3110999e34fdf
M. Patchesny <strielok11+github@gmail.com>
2023-03-17 23:21:37 +0100

    add

---
+++ src/datingApp.Core/Entities/UserSettings.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace datingApp.Core.Entities
+{
+    public class UserSettings
+    {
+        public User User { get; private set; }
+
+        [Required]
+        [Range(0, 2)]
+        public int DiscoverSex { get; private set; }
+
+        [Required]
+        [Range(18, 100)]
+        public int DiscoverMinAge { get; private set; }
+
+        [Required]
+        [Range(18, 100)]
+        public int DiscoverMaxAge { get; private set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int DiscoverRange { get; private set; }
+
+        [Required]
+        public double Lat { get; private set; }
+        
+        [Required]
+        public double Lon { get; private set; }
+
+        public UserSettings(User user, int discoverSex, int discoverMinAge, int discoverMaxAge, int discoverRange, double lat, double lon)
+        {
+            User = user;
+            DiscoverSex = discoverSex;
+            DiscoverMaxAge = discoverMaxAge;
+            DiscoverMinAge = discoverMinAge;
+            DiscoverRange = discoverRange;
+            Lat = lat;
+            Lon = lon;
+        }
+
+        public void ChangeLocation(double lat, double lon)
+        {
+            Lat = lat;
+            Lon = lon;
+        }
+    }
+}

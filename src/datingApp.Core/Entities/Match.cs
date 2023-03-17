9c51ab4c7c76ebf7114c3518cdeb04e08d12737d
M. Patchesny <strielok11+github@gmail.com>
2023-03-17 21:03:57 +0100

    add

---
+++ src/datingApp.Core/Entities/Match.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace datingApp.Core.Entities
+{
+    public class Match
+    {
+        public Long Id { get; }
+        
+        [Required]
+        public User User1 { get; private set; }
+
+        [Required]
+        public User User2 { get; private set; }
+
+        [Required]
+        public DateTime CreatedAt { get; private set; }
+
+        public Match(User user1, User user2, DateTime createdAt)
+        {
+            User1 = user1;
+            User2 = user2;
+            CreatedAt = createdAt;
+        }
+    }
+}

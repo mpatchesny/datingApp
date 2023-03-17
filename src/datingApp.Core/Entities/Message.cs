7b8795c003d7f772cec27ed6e7cd38e818a15c09
M. Patchesny <strielok11+github@gmail.com>
2023-03-17 23:34:50 +0100

    add IsDisplayed property

--- src/datingApp.Core/Entities/Message.cs
+++ src/datingApp.Core/Entities/Message.cs
@@ -18,17 +18,25 @@ namespace datingApp.Core.Entities
 
         [Required(ErrorMessage = "message is required")]
         [StringLength(280, ErrorMessage = "message must be maximum 280 characters long")]
-        public string Text {get; private set; }
+        public string Text { get; private set; }
+
+        public bool IsDisplayed { get; private set; }
 
         [Required(ErrorMessage = "created at is required")]
         public DateTime CreatedAt { get; private set; }
 
-        public Message(User sendFrom, User sendTo, string text, DateTime createdAt)
+        public Message(User sendFrom, User sendTo, string text, bool isDisplayed, DateTime createdAt)
         {
             SendFrom = sendFrom;
             SendTo = sendTo;
             Text = text;
+            IsDisplayed = IsDisplayed;
             CreatedAt = createdAt;
+        }
+
+        public void SetAsDisplayed()
+        {
+            IsDisplayed = true;
         }
     }
 }

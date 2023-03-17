e1cfbeefd1a56d52972f9ee6b20ed0207733e39f
M. Patchesny <strielok11+github@gmail.com>
2023-03-17 20:56:38 +0100

    add validation, comment out RemoveMatch body; change constructor arg list

--- src/datingApp.Core/Entities/User.cs
+++ src/datingApp.Core/Entities/User.cs
@@ -2,21 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace datingApp.Core.Entities
 {
     public class User
     {
         public long Id { get; }
+        
+        [Required(ErrorMessage = "phone no is required")]
+        [StringLength(15, ErrorMessage = "phone no must be maximum 15 characters long")]
         public string PhoneNo { get; private set; }
+        
+        [Required(ErrorMessage = "name is required")]
+        [StringLength(15, ErrorMessage = "name must be maximum 15 characters long")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage = "invalid characters in name")]
         public string Name { get; private set; }
+        
+        [Required(ErrorMessage = "age is required")]
+        [Range(18, 100, ErrorMessage = "age must be between 18 and 100")]
         public int Age { get; private set; }
-        public int Sex { get; private set; }
+
+        [Required(ErrorMessage = "sex is required")]
+        [RegularExpression(@"[K|M]", ErrorMessage = "sex must be K or M")]
+        public string Sex { get; private set; }
         public string Bio { get; private set; }
         public IEnumerable<Object> Matches => _matches;
         private readonly HashSet<Object> _matches = new();
 
-        public User(long id, string phoneNo, string name, int age, int sex, string bio)
+        public User(long id, string phoneNo, string name, int age, string sex, string bio)
         {
             Id = id;
             PhoneNo = phoneNo;
@@ -33,7 +47,8 @@ namespace datingApp.Core.Entities
 
         public void RemoveMatch(long matchId)
         {
-            _matches.RemoveWhere(x => x.Id == matchId);
+            // TODO
+            // _matches.RemoveWhere(x => x.Id == matchId);
         }
     }
 }

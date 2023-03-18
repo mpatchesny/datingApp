2a781fb25b800bd9a3fd54d8ad8048819a0d866a
M. Patchesny <strielok11+github@gmail.com>
2023-03-18 10:46:36 +0100

    add unit test project

---
+++ tests/datingApp.Tests.Unit/Entities/UserTests.cs
@@ -0,0 +1,12 @@
+using datingApp.Core.Entities;
+
+namespace datingApp.Tests.Unit;
+
+public class UsersTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var user = new User();
+    }
+}

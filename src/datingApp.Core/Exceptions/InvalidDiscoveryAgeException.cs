2c3580d0c6d063b46fa10d7fd4ce54a52a577c80
M. Patchesny <strielok11+github@gmail.com>
2023-03-19 23:12:02 +0100

    add exception

---
+++ src/datingApp.Core/Exceptions/InvalidDiscoveryAgeException.cs
@@ -0,0 +1,10 @@
+using System.Runtime.Serialization;
+
+namespace datingApp.Core.Exceptions;
+
+public sealed class InvalidDiscoveryAgeException : CustomException
+{
+    public InvalidDiscoveryAgeException(string details) : base($"Invalid discovery age: {details}")
+    {
+    }
+}

5227e742be69030098ae803d40712096d72aa84e
M. Patchesny <strielok11+github@gmail.com>
2023-03-19 21:51:50 +0100

    new namespace format (no parenthesis)

--- src/datingApp.Core/Entities/Match.cs
+++ src/datingApp.Core/Entities/Match.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace datingApp.Core.Entities
-{
-    public class Match
-    {
-        public int Id { get; }
-        public int UserId1 { get; private set; }
-        public int UserId2 { get; private set; }
-        public Message? LastMessage { get; private set; }
-        public DateTime CreatedAt { get; private set; }
+namespace datingApp.Core.Entities;
 
-        public Match(int id, int userId1, int userId2, Message? lastMessage, DateTime createdAt)
-        {
-            Id = id;
-            UserId1 = userId1;
-            UserId2 = userId2;
-            LastMessage = lastMessage;
-            CreatedAt = createdAt;
-        }
+public class Match
+{
+    public int Id { get; }
+    public int UserId1 { get; private set; }
+    public int UserId2 { get; private set; }
+    public Message? LastMessage { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public Match(int id, int userId1, int userId2, Message? lastMessage, DateTime createdAt)
+    {
+        Id = id;
+        UserId1 = userId1;
+        UserId2 = userId2;
+        LastMessage = lastMessage;
+        CreatedAt = createdAt;
     }
 }
--- src/datingApp.Core/Entities/Photo.cs
+++ src/datingApp.Core/Entities/Photo.cs
@@ -3,31 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace datingApp.Core.Entities
+namespace datingApp.Core.Entities;
+
+public class Photo
 {
-    public class Photo
+    public int Id { get; }
+    public int UserId { get; private set; }
+    public string Path { get; private set; }
+    public int Oridinal { get; private set; }
+
+    public Photo(int id, int userId, string path, int oridinal)
     {
-        public int Id { get; }
-        public int UserId { get; private set; }
-        public string Path { get; private set; }
-        public int Oridinal { get; private set; }
+        Id = id;
+        UserId = userId;
+        SetPath(path);
+        Oridinal = oridinal;
+    }
 
-        public Photo(int id, int userId, string path, int oridinal)
+    private void SetPath(string path)
+    {
+        if (path.Length == 0)
         {
-            Id = id;
-            UserId = userId;
-            SetPath(path);
-            Oridinal = oridinal;
+            throw new Exception("path cannot be empty");
         }
-
-        private void SetPath(string path)
-        {
-            if (path.Length == 0)
-            {
-                throw new Exception("path cannot be empty");
-            }
-            if (Path == path) return;
-            Path = path;
-        }
+        if (Path == path) return;
+        Path = path;
     }
 }

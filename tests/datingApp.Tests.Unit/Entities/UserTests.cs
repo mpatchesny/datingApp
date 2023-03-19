16bb204a5ebd06fdab32ada4210482e2e74cddde
M. Patchesny <strielok11+github@gmail.com>
2023-03-20 00:05:52 +0100

    change constructor; refactor; User model not reposible for adding/ removing photos, matches; add Like property; fix tests

--- tests/datingApp.Tests.Unit/Entities/UserTests.cs
+++ tests/datingApp.Tests.Unit/Entities/UserTests.cs
@@ -9,7 +9,7 @@ public class UserTests
     [Fact]
     public void user_phone_should_not_be_emptystring()
     {
-        var exception = Record.Exception(() =>new User(1, "", "email@email.com", "janusz", new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "", "email@email.com", "janusz", new System.DateOnly(1999,1,1), Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidPhoneException>(exception);
     }
@@ -17,7 +17,7 @@ public class UserTests
     [Fact]
     public void user_phone_should_not_be_longer_than_9_chars()
     {
-        var exception = Record.Exception(() =>new User(1, "0123456789", "email@email.com", "janusz", new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "0123456789", "email@email.com", "janusz", new System.DateOnly(1999,1,1), Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidPhoneException>(exception);
     }
@@ -29,7 +29,7 @@ public class UserTests
     [InlineData("000a000=0")]
     public void user_phone_should_not_containt_not_numbers(string phone)
     {
-        var exception = Record.Exception(() =>new User(1, phone, "email@email.com", "janusz", new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, phone, "email@email.com", "janusz", new System.DateOnly(1999,1,1), Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidPhoneException>(exception);
     }
@@ -42,7 +42,7 @@ public class UserTests
     // [InlineData("user@ex ample.com")]
     public void user_email_should_be_proper_email(string badEmail)
     {
-        var exception = Record.Exception(() =>new User(1, "012345678", badEmail, "janusz", new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "012345678", badEmail, "janusz", new System.DateOnly(1999,1,1), Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidEmailException>(exception);
     }
@@ -56,7 +56,7 @@ public class UserTests
             badEmail += "a";
         }
         badEmail += "test@gmail.com";
-        var exception = Record.Exception(() =>new User(1, "012345678", badEmail, "janusz", new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "012345678", badEmail, "janusz", new System.DateOnly(1999,1,1), Sex.Male,null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidEmailException>(exception);
     }
@@ -64,7 +64,7 @@ public class UserTests
     [Fact]
     public void user_email_should_not_be_emptystring()
     {
-        var exception = Record.Exception(() =>new User(1, "012345678", "", "janusz", new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "012345678", "", "janusz", new System.DateOnly(1999,1,1), Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidEmailException>(exception);
     }
@@ -72,7 +72,7 @@ public class UserTests
     [Fact]
     public void user_name_should_not_be_emptystring()
     {
-        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "", new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "", new System.DateOnly(1999,1,1), Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidUsernameException>(exception);
     }
@@ -85,7 +85,7 @@ public class UserTests
     [InlineData("#")]
     public void user_name_should_not_contain_invalid_chars(string username)
     {
-        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", username, new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", username, new System.DateOnly(1999,1,1), Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidUsernameException>(exception);
     }
@@ -94,7 +94,7 @@ public class UserTests
     public void user_name_should_not_exceed_15_chars()
     {
         string username = "Janusz ma kota a";
-        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", username, new System.DateOnly(1999,1,1), Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", username, new System.DateOnly(1999,1,1), Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidUsernameException>(exception);
     }
@@ -107,7 +107,7 @@ public class UserTests
     {
         var invalidDob = DateTime.Parse(dateString);
         var invalidDob2 = new DateOnly(invalidDob.Year, invalidDob.Month, invalidDob.Day);
-        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "janusz", invalidDob2, Sex.Male));
+        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "janusz", invalidDob2, Sex.Male, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidDateOfBirthException>(exception);
     }
@@ -120,7 +120,7 @@ public class UserTests
         {
             bio += "a";
         }
-        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, "", bio));
+        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, null, null, null, "", bio));
         Assert.NotNull(exception);
         Assert.IsType<BioTooLongException>(exception);
     }
@@ -133,7 +133,7 @@ public class UserTests
         {
             job += "a";
         }
-        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, job));
+        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, null, null, null, job));
         Assert.NotNull(exception);
         Assert.IsType<JobTooLongException>(exception);
     }
@@ -143,7 +143,7 @@ public class UserTests
     {
         var actualYear = DateTime.UtcNow.Year;
         var dob = new DateOnly(actualYear - 18, 1, 1);
-        var user = new User(1, "012345678", "test@test.com", "janusz", dob, Sex.Male);
+        var user = new User(1, "012345678", "test@test.com", "janusz", dob, Sex.Male, null, null, null);
         Assert.Equal(18, user.GetAge());
     }
 
@@ -151,57 +151,15 @@ public class UserTests
     public void user_should_not_accept_two_sexes()
     {
         var sex = Sex.Male & Sex.Female;
-        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), sex));
+        var exception = Record.Exception(() =>new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), sex, null, null, null));
         Assert.NotNull(exception);
         Assert.IsType<InvalidUserSexException>(exception);
     }
 
     [Fact]
-    public void user_adding_two_matches_with_same_id_should_fail()
-    {
-        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male);
-        var match1 = new Match(1, 1, 1, null, DateTime.UtcNow);
-        var match2 = new Match(1, 1, 1, null, DateTime.UtcNow);
-        user.AddMatch(match1);
-        var exception = Record.Exception(() => user.AddMatch(match2));
-        Assert.NotNull(exception);
-    }
-
-    [Fact]
-    public void user_adding_two_photos_with_same_id_should_fail()
-    {
-        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male);
-        var photo1 = new Photo(1, 1, "abc", 1);
-        var photo2 = new Photo(1, 1, "abc", 2);
-        user.AddPhoto(photo1);
-        var exception = Record.Exception(() => user.AddPhoto(photo2));
-        Assert.NotNull(exception);
-    }
-
-    [Fact]
-    public void user_should_not_accept_two_photos_with_same_oridinal()
-    {
-        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male);
-        var photo1 = new Photo(1, 1, "abc", 1);
-        var photo2 = new Photo(2, 1, "abc", 1);
-        user.AddPhoto(photo1);
-        var exception = Record.Exception(() => user.AddPhoto(photo2));
-        Assert.NotNull(exception);
-    }
-
-    [Fact]
-    public void user_with_main_photo_isvisible_should_return_true()
-    {
-        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male);
-        var photo1 = new Photo(1, 1, "abc", 1);
-        user.AddPhoto(photo1);
-        Assert.Equal(true, user.IsVisible());
-    }
-
-    [Fact]
     public void change_user_date_of_birth_should_take_effect()
     {
-        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male);
+        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, null, null, null);
         user.ChangeDateOfBirth(new DateOnly(1999,1,2));
         Assert.Equal(new DateOnly(1999,1,2), user.DateOfBirth);
     }
@@ -209,7 +167,7 @@ public class UserTests
     [Fact]
     public void change_user_bio_should_take_effect()
     {
-        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, "", "bio");
+        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, null, null, null, "", "bio");
         user.ChangeBio("new bio");
         Assert.Equal("new bio", user.Bio);
     }
@@ -217,7 +175,7 @@ public class UserTests
     [Fact]
     public void change_user_job_should_take_effect()
     {
-        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, "job");
+        var user = new User(1, "012345678", "test@test.com", "janusz", new DateOnly(1999,1,1), Sex.Male, null, null, null, "job");
         user.ChangeJob("new job");
         Assert.Equal("new job", user.Job);
     }

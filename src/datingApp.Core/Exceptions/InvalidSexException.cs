d1e9896d85693769196cbe0f998a6979a2513e2c
M. Patchesny <strielok11+github@gmail.com>
2023-03-19 22:50:42 +0100

    more exceptions

---
+++ src/datingApp.Core/Exceptions/InvalidSexException.cs
@@ -0,0 +1,10 @@
+using System.Runtime.Serialization;
+
+namespace datingApp.Core.Exceptions;
+
+public sealed class InvalidUserSexException : CustomException
+{
+    public InvalidUserSexException() : base("User can't have two sexes")
+    {
+    }
+}

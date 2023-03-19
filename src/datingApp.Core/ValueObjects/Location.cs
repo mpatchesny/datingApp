b740b415196a1088fc9dada543fc7b578cecab16
M. Patchesny <strielok11+github@gmail.com>
2023-03-19 23:01:24 +0100

    move validation to Location value object

--- src/datingApp.Core/ValueObjects/Location.cs
+++ src/datingApp.Core/ValueObjects/Location.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using datingApp.Core.Exceptions;
 
 namespace datingApp.Core.ValueObjects;
 
@@ -12,6 +13,11 @@ public sealed record Location
 
     public Location(double lat, double lon)
     {
+        if (lat > 90 | lat < -90 |
+            lon > 180 | lon < -180)
+        {
+            throw new InvalidLocationException();
+        }
         Lat = lat;
         Lon = lon;
     }

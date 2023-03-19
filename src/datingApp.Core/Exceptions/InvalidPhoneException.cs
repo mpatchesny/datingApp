b50cdeff83eef5588a96007ff82be297da936a27
M. Patchesny <strielok11+github@gmail.com>
2023-03-19 22:35:08 +0100

    add

---
+++ src/datingApp.Core/Exceptions/InvalidPhoneException.cs
@@ -0,0 +1,11 @@
+using System.Runtime.Serialization;
+
+namespace datingApp.Core.Exceptions
+{
+    public sealed class InvalidPhoneException : CustomException
+    {
+        public InvalidPhoneException(string details) : base($"Phone number is invalid: {details}")
+        {
+        }
+    }
+}

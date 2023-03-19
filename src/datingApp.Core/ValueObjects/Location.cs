523029f0ace6374bab267bf2a87034d86ef43130
M. Patchesny <strielok11+github@gmail.com>
2023-03-19 21:51:36 +0100

    add sealed; change namespace

--- src/datingApp.Core/ValueObjects/Location.cs
+++ src/datingApp.Core/ValueObjects/Location.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace datingApp.Core.ValueObjects
-{
-    public record Location
-    {
-        public double Lat { get; }
-        public double Lon { get; }
+namespace datingApp.Core.ValueObjects;
 
-        public Location(double lat, double lon)
-        {
-            Lat = lat;
-            Lon = lon;
-        }
+public sealed record Location
+{
+    public double Lat { get; }
+    public double Lon { get; }
+
+    public Location(double lat, double lon)
+    {
+        Lat = lat;
+        Lon = lon;
     }
 }

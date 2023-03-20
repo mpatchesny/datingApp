d6c9f7d2185c48dc90c0d8548377975eb4cd3e80
M. Patchesny <strielok11+github@gmail.com>
2023-03-20 23:15:18 +0100

    change value object to primitives

--- src/datingApp.Application/Commands/ChangeDiscoverAge.cs
+++ src/datingApp.Application/Commands/ChangeDiscoverAge.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
-using datingApp.Core.ValueObjects;
 
 namespace datingApp.Application.Commands;
 
-public sealed record ChangeDiscoverAge(int userId, AgeRange age) : ICommand;
+public sealed record ChangeDiscoverAge(int userId, int ageFrom, int ageTo) : ICommand;
--- src/datingApp.Application/Commands/ChangeLocation.cs
+++ src/datingApp.Application/Commands/ChangeLocation.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
-using datingApp.Core.ValueObjects;
 
 namespace datingApp.Application.Commands;
 
-public sealed record ChangeLocation(int userId, Location location) : ICommand;
+public sealed record ChangeLocation(int userId, double lat, double lon) : ICommand;

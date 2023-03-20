813fdd14b1dfc2b587103957ee338111626fb362
M. Patchesny <strielok11+github@gmail.com>
2023-03-20 20:57:39 +0100

    extract UserLocation entity

--- src/datingApp.Core/Entities/UserSettings.cs
+++ src/datingApp.Core/Entities/UserSettings.cs
@@ -13,15 +13,13 @@ public class UserSettings
     public Sex DiscoverSex { get; private set; }
     public Tuple<int, int> DiscoverAgeRange { get; private set; }
     public int DiscoverRange { get; private set; }
-    public Location Location { get; private set; }
 
-    public UserSettings(int userId, Sex discoverSex, Tuple<int,int> discoverAgeRange, int discoverRange, Location location)
+    public UserSettings(int userId, Sex discoverSex, Tuple<int,int> discoverAgeRange, int discoverRange)
     {
         UserId = userId;
         DiscoverSex = discoverSex;
         SetDiscoverAge(discoverAgeRange);
         SetDiscoverRange(discoverRange);
-        SetLocation(location);
     }
 
     public void ChangeDiscoverSex(Sex sex)
@@ -37,11 +35,6 @@ public class UserSettings
     public void ChangeDiscoverRange(int discoverRange)
     {
         SetDiscoverRange(discoverRange);
-    }
-
-    public void ChangeLocation(Location location)
-    {
-        SetLocation(location);
     }
 
     private void SetDiscoverAge(Tuple<int, int> discoverAgeRange)
@@ -67,11 +60,5 @@ public class UserSettings
         }
         if (DiscoverRange == discoverRange) return;
         DiscoverRange = discoverRange;
-    }
-
-    private void SetLocation(Location location)
-    {
-        if (Location == location) return;
-        Location = location;
     }
 }

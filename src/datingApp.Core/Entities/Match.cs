9125a9f8bd4e6dff8a40e3c7d7b7d4231bfbf92a
M. Patchesny <strielok11+github@gmail.com>
2023-03-21 23:04:24 +0100

    change LastMessage to Messages list of message

--- src/datingApp.Core/Entities/Match.cs
+++ src/datingApp.Core/Entities/Match.cs
@@ -10,15 +10,16 @@ public class Match
     public int Id { get; }
     public int UserId1 { get; private set; }
     public int UserId2 { get; private set; }
-    public Message? LastMessage { get; private set; }
+    public IEnumerable<Message> Messages { get; private set; }
     public DateTime CreatedAt { get; private set; }
 
-    public Match(int id, int userId1, int userId2, Message? lastMessage, DateTime createdAt)
+    public Match(int id, int userId1, int userId2, IEnumerable<Message> messages, DateTime createdAt)
     {
         Id = id;
         UserId1 = userId1;
         UserId2 = userId2;
-        LastMessage = lastMessage;
+        if (messages == null) messages =  new List<Message>();
+        Messages = messages;
         CreatedAt = createdAt;
     }
 }

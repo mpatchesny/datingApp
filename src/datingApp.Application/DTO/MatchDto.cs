037311816fbadd95a6c8bfffde723958ac003a19
M. Patchesny <strielok11+github@gmail.com>
2023-03-21 23:05:43 +0100

    change property positon

--- src/datingApp.Application/DTO/MatchDto.cs
+++ src/datingApp.Application/DTO/MatchDto.cs
@@ -10,6 +10,6 @@ public class MatchDto
     public int Id { get; set; }
     public int UserId1 { get; set; }
     public int UserId2 { get; set; }
-    public DateTime CreatedAt { get; set; }
     public IEnumerable<MessageDto> Messages { get; set; }
+    public DateTime CreatedAt { get; set; }
 }

97f11bfcb160ec7e3d460222c48a4af3b693cdf4
M. Patchesny <strielok11+github@gmail.com>
2023-03-21 22:59:49 +0100

    change object to Dto object

--- src/datingApp.Application/Queries/GetSwipeCandidates.cs
+++ src/datingApp.Application/Queries/GetSwipeCandidates.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
+using datingApp.Application.DTO;
 
 namespace datingApp.Application.Queries;
 
-public class GetSwipeCandidates : IQuery<IEnumerable<object>>
+public class GetSwipeCandidates : IQuery<IEnumerable<PublicUserDto>>
 {
     public int UserId { get; set; }
 }
--- src/datingApp.Application/Queries/GetUser.cs
+++ src/datingApp.Application/Queries/GetUser.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
+using datingApp.Application.DTO;
 
 namespace datingApp.Application.Queries;
 
-public class GetUser : IQuery<object>
+public class GetUser : IQuery<PublicUserDto>
 {
     public int UserId { get; set; }
 }

453aa512f81751d8f692e7bfdaed317401823b65
M. Patchesny <strielok11+github@gmail.com>
2023-03-23 21:33:24 +0100

    add

---
+++ src/datingApp.Core/Repositories/ISwipeRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Core.Entities;
+
+namespace datingApp.Core.Repositories;
+
+public interface ISwipeRepository : IRepository
+{
+    Task AddAsync(Swipe swipe);
+}

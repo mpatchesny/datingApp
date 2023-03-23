d110b39d0185fbf64516046cd784d3c6809eda1b
M. Patchesny <strielok11+github@gmail.com>
2023-03-23 23:08:09 +0100

    change namespace format

--- tests/datingApp.Tests.Unit/Entities/UserLocationTests.cs
+++ tests/datingApp.Tests.Unit/Entities/UserLocationTests.cs
@@ -5,20 +5,19 @@ using System.Threading.Tasks;
 using datingApp.Core.Exceptions;
 using datingApp.Core.ValueObjects;
 
-namespace datingApp.Tests.Unit.Entities
+namespace datingApp.Tests.Unit.Entities;
+
+public class UserLocationTests
 {
-    public class UserLocationTests
+    [Theory]
+    [InlineData(0.0, -180.1)]
+    [InlineData(0.0, 180.1)]
+    [InlineData(90.1, 0.0)]
+    [InlineData(-90.1, 0.0)]
+    public void user_location_should_be_proper_location(double lat, double lon)
     {
-        [Theory]
-        [InlineData(0.0, -180.1)]
-        [InlineData(0.0, 180.1)]
-        [InlineData(90.1, 0.0)]
-        [InlineData(-90.1, 0.0)]
-        public void user_location_should_be_proper_location(double lat, double lon)
-        {
-            var exception = Record.Exception(() => new Location(lat, lon));
-            Assert.NotNull(exception);
-            Assert.IsType<InvalidLocationException>(exception);
-        }
+        var exception = Record.Exception(() => new Location(lat, lon));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidLocationException>(exception);
     }
 }
--- tests/datingApp.Tests.Unit/Entities/UserSettingsTests.cs
+++ tests/datingApp.Tests.Unit/Entities/UserSettingsTests.cs
@@ -6,37 +6,36 @@ using datingApp.Core.Entities;
 using datingApp.Core.Exceptions;
 using datingApp.Core.ValueObjects;
 
-namespace datingApp.Tests.Unit.Entities
+namespace datingApp.Tests.Unit.Entities;
+
+public class UserSettingsTests
 {
-    public class UserSettingsTests
+    [Fact]
+    public void user_settings_should_accept_male_female_discovery_sex()
     {
-        [Fact]
-        public void user_settings_should_accept_male_female_discovery_sex()
-        {
-            var discoverySex = Sex.Male & Sex.Female;
-            var exception = Record.Exception(() =>new UserSettings(1, discoverySex, new AgeRange(20, 21), 20));
-            Assert.Null(exception);
-        }
+        var discoverySex = Sex.Male & Sex.Female;
+        var exception = Record.Exception(() =>new UserSettings(1, discoverySex, new AgeRange(20, 21), 20));
+        Assert.Null(exception);
+    }
 
-        [Theory]
-        [InlineData(17, 18)]
-        [InlineData(18, 101)]
-        [InlineData(21, 20)]
-        public void user_settings_age_range_should_be_between_18_and_100_and_min_not_larger_than_max(int minAge, int maxAge)
-        {
-            var exception = Record.Exception(() =>new AgeRange(minAge, maxAge));
-            Assert.NotNull(exception);
-            Assert.IsType<InvalidDiscoveryAgeException>(exception);
-        }
+    [Theory]
+    [InlineData(17, 18)]
+    [InlineData(18, 101)]
+    [InlineData(21, 20)]
+    public void user_settings_age_range_should_be_between_18_and_100_and_min_not_larger_than_max(int minAge, int maxAge)
+    {
+        var exception = Record.Exception(() =>new AgeRange(minAge, maxAge));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidDiscoveryAgeException>(exception);
+    }
 
-        [Theory]
-        [InlineData(0)]
-        [InlineData(101)]
-        public void user_settings_discovery_range_should_be_between_1_and_100(int range)
-        {
-            var exception = Record.Exception(() =>new UserSettings(1, Sex.Male, new AgeRange(20, 25), range));
-            Assert.NotNull(exception);
-            Assert.IsType<InvalidDiscoveryRangeException>(exception);
-        }
+    [Theory]
+    [InlineData(0)]
+    [InlineData(101)]
+    public void user_settings_discovery_range_should_be_between_1_and_100(int range)
+    {
+        var exception = Record.Exception(() =>new UserSettings(1, Sex.Male, new AgeRange(20, 25), range));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidDiscoveryRangeException>(exception);
     }
 }

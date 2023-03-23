2c11d8260d3ccfb50d40864c6d2eba0a8f229e5a
M. Patchesny <strielok11+github@gmail.com>
2023-03-23 21:16:52 +0100

    add more getters

--- src/datingApp.Core/Repositories/IUserRepository.cs
+++ src/datingApp.Core/Repositories/IUserRepository.cs
@@ -8,8 +8,9 @@ namespace datingApp.Core.Repositories;
 
 public interface IUserRepository : IRepository
 {
-    Task<User> GetAsync(int userId); 
-    Task<IEnumerable<User>> GetAllAsync();
+    Task<User> GetByIdAsync(int userId);
+    Task<User> GetByEmailAsync(string email);
+    Task<User> GetByPhoneAsync(string phone);
     Task AddAsync(User user);
     Task UpdateAsync(User user);
     Task DeleteAsync(User user);

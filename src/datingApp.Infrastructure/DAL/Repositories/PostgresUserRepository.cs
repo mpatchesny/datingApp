22c40073cf3a2c1d56991e0acf2d066d02b995be
M. Patchesny <strielok11+github@gmail.com>
2023-03-26 22:44:36 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Repositories/PostgresUserRepository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Core.Entities;
+using datingApp.Core.Repositories;
+
+namespace datingApp.Infrastructure.DAL.Repositories;
+
+internal sealed class PostgresUserRepository : IUserRepository
+{
+    public PostgresUserRepository()
+    {
+        // todo
+    }
+    public Task AddAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByIdAsync(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByPhoneAsync(string phone)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+}

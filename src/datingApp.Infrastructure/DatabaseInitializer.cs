3157cb07c2720d6376063e9a6f153d09c9ff77a0
M. Patchesny <strielok11+github@gmail.com>
2023-03-27 22:28:37 +0200

    add

---
+++ src/datingApp.Infrastructure/DatabaseInitializer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace datingApp.Infrastructure
+{
+    internal sealed class DatabaseInitializer : IHostedService
+    {
+        private readonly IServiceProvider _serviceProdivder;
+        public DatabaseInitializer(IServiceProvider serviceProvider)
+        {
+            _serviceProdivder = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var scope = _serviceProdivder.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<DatingAppDbContext>();
+                dbContext.Database.Migrate();
+                // tutaj moze byc seed
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}

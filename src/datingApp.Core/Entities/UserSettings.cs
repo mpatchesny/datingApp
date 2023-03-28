1615d2efd2416e736acd743c05074adbbd3d68dc
M. Patchesny <strielok11+github@gmail.com>
2023-03-28 23:43:11 +0200

    split AgeRange value object to columns

--- src/datingApp.Core/Entities/UserSettings.cs
+++ src/datingApp.Core/Entities/UserSettings.cs
@@ -11,14 +11,20 @@ public class UserSettings
 {
     public int UserId { get; private set; }
     public Sex DiscoverSex { get; private set; }
-    public AgeRange DiscoverAgeRange { get; private set; }
+    public int DiscoverAgeFrom { get; private set; }
+    public int DiscoverAgeTo { get; private set; }
     public int DiscoverRange { get; private set; }
 
-    public UserSettings(int userId, Sex discoverSex, AgeRange discoverAgeRange, int discoverRange)
+    private UserSettings()
+    {
+        // EF
+    }
+
+    public UserSettings(int userId, Sex discoverSex, int discoverAgeFrom, int discoverAgeTo, int discoverRange)
     {
         UserId = userId;
         SetDiscoverSex(discoverSex);
-        SetDiscoverAge(discoverAgeRange);
+        SetDiscoverAge(discoverAgeFrom, discoverAgeTo);
         SetDiscoverRange(discoverRange);
     }
 
@@ -27,9 +33,9 @@ public class UserSettings
         SetDiscoverSex(sex);
     }
 
-    public void ChangeDiscoverAge(AgeRange discoverAgeRange)
+    public void ChangeDiscoverAge(int discoverAgeFrom, int discoverAgeTo)
     {
-        SetDiscoverAge(discoverAgeRange);
+        SetDiscoverAge(discoverAgeFrom, discoverAgeTo);
     }
 
     public void ChangeDiscoverRange(int discoverRange)
@@ -43,10 +49,22 @@ public class UserSettings
         DiscoverSex = sex;
     }
 
-    private void SetDiscoverAge(AgeRange discoverAgeRange)
+    private void SetDiscoverAge(int discoverAgeFrom, int discoverAgeTo)
     {
-        if (DiscoverAgeRange == discoverAgeRange) return;
-        DiscoverAgeRange = discoverAgeRange;
+        if (discoverAgeFrom < 18 | discoverAgeFrom > 100)
+        {
+            throw new InvalidDiscoveryAgeException("discover min age must be between 18 and 100");
+        }
+        else if (discoverAgeTo < 18 | discoverAgeTo > 100)
+        {
+            throw new InvalidDiscoveryAgeException("discover max age must be between 18 and 100");
+        }
+        else if (discoverAgeFrom > discoverAgeTo)
+        {
+            throw new InvalidDiscoveryAgeException("discover min age cannot be larger than max age");
+        }
+        DiscoverAgeFrom = discoverAgeFrom;
+        DiscoverAgeTo = discoverAgeTo;
     }
 
     private void SetDiscoverRange(int discoverRange)

58b7325ffcd30539b2c00d632adf216288221e55
M. Patchesny <strielok11+github@gmail.com>
2023-03-28 23:30:11 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Configurations/UserSettingsConfiguration.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace datingApp.Infrastructure.DAL.Configurations;
+
+internal sealed class UserSettingsConfiguration : IEntityTypeConfiguration<UserSettings>
+{
+    public void Configure(EntityTypeBuilder<UserSettings> builder)
+    {
+        builder.HasKey(e => e.UserId);
+        builder.Property(x => x.UserId)
+            .IsRequired();
+        builder.Property(x => x.DiscoverRange)
+            .IsRequired();
+        builder.Property(x => x.DiscoverSex)
+            .IsRequired();
+    }
+}

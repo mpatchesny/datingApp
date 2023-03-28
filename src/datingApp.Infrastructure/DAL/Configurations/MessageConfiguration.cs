0aa588c2579416166baad77372ea1ff75233969d
M. Patchesny <strielok11+github@gmail.com>
2023-03-28 22:36:04 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Configurations/MessageConfiguration.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace datingApp.Infrastructure.DAL.Configurations;
+
+internal sealed class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .IsRequired();
+        builder.Property(x => x.MatchId)
+            .IsRequired();
+        builder.Property(x => x.SendFromId)
+            .IsRequired();
+        builder.Property(x => x.SendToId)
+            .IsRequired();
+        builder.Property(x => x.IsDisplayed)
+            .IsRequired()
+            .HasDefaultValue(false);
+        builder.Property(x => x.Text)
+            .IsRequired()
+            .HasMaxLength(255);
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+    }
+}

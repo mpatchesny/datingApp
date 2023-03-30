873afc945285d743a62be8a1d4a1bf1cdc076859
M. Patchesny <strielok11+github@gmail.com>
2023-03-30 21:07:12 +0200

    add config & options for test db

--- tests/datingApp.Tests.Integration/TestDatabase.cs
+++ tests/datingApp.Tests.Integration/TestDatabase.cs
@@ -1,17 +1,24 @@
     using System;
 using datingApp.Infrastructure;
+using datingApp.Infrastructure.DAL;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace datingApp.Tests.Integration;
 
 internal sealed class TestDatabase : IDisposable
 {
     public DatingAppDbContext DbContext { get; }
-
     public TestDatabase()
     {
-        var options = new OptionsProvider().Get<PostgresOptions>("postgres");
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Test.json")
+            .Build();
+        string optionsSectionName = "postgres";
+        var options = configuration.GetOptions<PostgresOptions>(optionsSectionName);
         DbContext = new DatingAppDbContext(new DbContextOptionsBuilder<DatingAppDbContext>().UseNpgsql(options.ConnectionString).Options);
+        DbContext.Database.EnsureCreated();
     }
 
     public void Dispose()

110e7b51ef4948660786d32f429a84d8a725a611
M. Patchesny <strielok11+github@gmail.com>
2023-03-30 21:24:55 +0200

    make tests async

--- tests/datingApp.Tests.Integration/Repositories/UserRepositoryTests.cs
+++ tests/datingApp.Tests.Integration/Repositories/UserRepositoryTests.cs
@@ -11,135 +11,108 @@ namespace datingApp.Tests.Integration;
 public class UserRepositoryTests : IDisposable
 {
     [Fact]
-    public void get_user_by_id_should_succeed()
+    public async Task get_user_by_id_should_succeedAsync()
     {
-        var task = _userRepository.GetByIdAsync(1);
-        task.Wait();
-        var user = task.Result;
+        var user = await _userRepository.GetByIdAsync(1);
         Assert.NotNull(user);
         Assert.Equal(1, user.Id);
     }
 
     [Fact]
-    public void get_user_by_existing_phone_should_succeed()
+    public async Task get_user_by_existing_phone_should_succeedAsync()
     {
         var phone = "123456789";
-        var task = _userRepository.GetByPhoneAsync(phone);
-        task.Wait();
-        var user = task.Result;
+        var user = await _userRepository.GetByPhoneAsync(phone);
         Assert.NotNull(user);
         Assert.Equal(phone, user.Phone);
     }
 
     [Fact]
-    public void get_user_by_existing_email_should_succeed()
+    public async Task get_user_by_existing_email_should_succeedAsync()
     {
         var email = "test@test.com";
-        var task = _userRepository.GetByEmailAsync(email);
-        task.Wait();
-        var user = task.Result;
+        var user = await _userRepository.GetByEmailAsync(email);
         Assert.NotNull(user);
         Assert.Equal(email, user.Email);
     }
 
     [Fact]
-    public void update_existing_user_should_succeed()
+    public async Task update_existing_user_should_succeedAsync()
     {
         var userId = 1;
-        var task = _userRepository.GetByIdAsync(userId);
-        task.Wait();
-        var user = task.Result;
+        var user = await _userRepository.GetByIdAsync(userId);
         user.ChangeBio("new bio");
+        await _userRepository.UpdateAsync(user);
+        _testDb.DbContext.SaveChanges();
 
-        var updateTask = _userRepository.UpdateAsync(user);
-        updateTask.Wait();
-
-        task = _userRepository.GetByIdAsync(userId);
-        task.Wait();
-        user = task.Result;
-
-        Assert.Equal("new bio", user.Bio);
+        var user2 = await _userRepository.GetByIdAsync(userId);
+        Assert.Equal("new bio", user2.Bio);
     }
 
     [Fact]
-    public void update_nonexisting_user_should_fail()
+    public async Task update_nonexisting_user_should_failAsync()
     {
         var settings = new UserSettings(0, Sex.Female, 18, 21, 20, 45.5, 45.5);
         var user = new User(0, "111111111", "bademail@test.com", "Janusz", new DateOnly(2000,1,1), Sex.Male, null, settings);
-        var updateTask = _userRepository.UpdateAsync(user);
-        updateTask.Wait();
+        await _userRepository.UpdateAsync(user);
+        _testDb.DbContext.SaveChanges();
         // FIXME
-        Assert.Equal(true, updateTask.IsCompletedSuccessfully);
+        // Assert.Equal(true, updateTask.IsCompletedSuccessfully);
     }
 
     [Fact]
-    public void delete_existing_user_should_succeed()
+    public async Task delete_existing_user_should_succeedAsync()
     {
         var userId = 1;
-        var task = _userRepository.GetByIdAsync(userId);
-        task.Wait();
-        var user = task.Result;
-        var deleteTask =  _userRepository.DeleteAsync(user);
-        deleteTask.Wait();
-
-        task = _userRepository.GetByIdAsync(userId);
-        task.Wait();
-        user = task.Result;
-        Assert.Null(user);
+        var user = await _userRepository.GetByIdAsync(userId);
+        await _userRepository.DeleteAsync(user);
+        var user2 = await _userRepository.GetByIdAsync(userId);
+        Assert.Null(user2);
     }
 
     [Fact]
-    public void delete_nonexisting_user_should_fail()
+    public async Task delete_nonexisting_user_should_failAsync()
     {
         var settings = new UserSettings(0, Sex.Female, 18, 21, 20, 45.5, 45.5);
         var user = new User(0, "111111111", "bademail@test.com", "Janusz", new DateOnly(2000,1,1), Sex.Male, null, settings);
-        var deleteTask =  _userRepository.DeleteAsync(user);
-        deleteTask.Wait();
+        await _userRepository.DeleteAsync(user);
         // FIXME
-        Assert.Equal(true, deleteTask.IsCompletedSuccessfully);
+        // Assert.Equal(true, deleteTask.IsCompletedSuccessfully);
     }
 
     [Fact]
-    public void add_user_should_succeed()
+    public async Task add_user_should_succeedAsync()
     {
         var settings = new UserSettings(0, Sex.Female, 18, 20, 50, 45.5, 45.5);
         var user = new User(0, "000000000", "test2@test.com", "Klaudiusz", new DateOnly(2000,1,1), Sex.Male, null, settings);
-        var addTask =  _userRepository.AddAsync(user);
-        addTask.Wait();
+        await  _userRepository.AddAsync(user);
+        _testDb.DbContext.SaveChanges();
 
-        var task = _userRepository.GetByIdAsync(user.Id);
-        task.Wait();
-        user = task.Result;
-        Assert.NotNull(user);
+        var user2 = await _userRepository.GetByIdAsync(user.Id);
+        Assert.NotNull(user2);
     }
 
     [Fact]
-    public void get_nonexistsing_user_by_id_should_return_null()
+    public async Task get_nonexistsing_user_by_id_should_return_nullAsync()
     {
         var userId = 0;
-        var task = _userRepository.GetByIdAsync(userId);
-        task.Wait();
-        var user = task.Result;
+        var user = await _userRepository.GetByIdAsync(userId);
         Assert.Null(user);
     }
 
     [Fact]
-    public void get_nonexistsing_user_by_phone_should_return_null()
+    public async Task get_nonexistsing_user_by_phone_should_return_nullAsync()
     {
         var phone = "000555000";
-        var task = _userRepository.GetByPhoneAsync(phone);
-        task.Wait();
-        var user = task.Result;
+        var user = await _userRepository.GetByPhoneAsync(phone);
         Assert.Null(user);
     }
 
     [Fact]
-    public void get_nonexistsing_user_by_email_should_return_null()
+    public async Task get_nonexistsing_user_by_email_should_return_nullAsync()
     {
         var email = "nonexistingemail@test.com";
-        var task = _userRepository.GetByEmailAsync(email);
-        task.Wait();
-        var user = task.Result;
+        var user = await _userRepository.GetByEmailAsync(email);
         Assert.Null(user);
     }
 

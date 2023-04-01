2151c97aab75a6e62681bdacaba184e619729725
M. Patchesny <strielok11+github@gmail.com>
2023-04-01 12:09:21 +0200

    add

---
+++ tests/datingApp.Tests.Unit/QueryHandlers/GetPrivateUserHandlerTests.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.DTO;
+using datingApp.Application.Queries;
+using datingApp.Core.Entities;
+using datingApp.Core.Repositories;
+using datingApp.Infrastructure.DAL.Handlers;
+using Moq;
+using Xunit;
+
+namespace datingApp.Tests.Unit.QueryHandlers;
+
+public class GetPrivateUserHandlerTests
+{
+    [Fact]
+    public async Task query_existing_user_should_return_public_user_dto()
+    {
+        var query = new GetPrivateUser();
+        query.UserId = 1;
+        var user = await _handler.HandleAsync(query);
+        Assert.NotNull(user);
+        Assert.IsType<PrivateUserDto>(user);
+    }
+
+    [Fact]
+    public async Task query_nonexisting_user_should_return_null()
+    {
+        var query = new GetPrivateUser();
+        query.UserId = 2;
+        var user = await _handler.HandleAsync(query);
+        Assert.Null(user);
+    }
+
+    // Arrange
+    private readonly GetPrivateUserHandler _handler;
+    public GetPrivateUserHandlerTests()
+    {
+        var settings = new UserSettings(1, Sex.Female, 18, 21, 20, 45.5, 45.5);
+        var user = new User(1, "111111111", "bademail@test.com", "Janusz", new DateOnly(2000,1,1), Sex.Male, null, settings);
+
+        var mockUserRepository = new Mock<IUserRepository>();
+        mockUserRepository
+            .Setup(x => x.GetByIdAsync(1))
+            .ReturnsAsync(user);
+        _handler = new GetPrivateUserHandler(mockUserRepository.Object);
+    }
+}

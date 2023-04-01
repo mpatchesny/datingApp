51a3ba5177b5e5c29912a0a8109ba3b5c7690a41
M. Patchesny <strielok11+github@gmail.com>
2023-04-01 12:01:55 +0200

    add

---
+++ src/datingApp.Application/DTO/PrivateUserDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Application.DTO;
+
+public class PrivateUserDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Sex { get; set; }
+    public string Job { get; set; }
+    public string Bio { get; set; }
+    public IEnumerable<PhotoDto> Photos { get; set; }
+    public UserSettingsDto Settings { get; set; }
+}

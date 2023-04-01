df3dc611f170358f8eac4abe4e446492d3de610e
M. Patchesny <strielok11+github@gmail.com>
2023-04-01 10:51:18 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Handlers/GetMatchesHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using datingApp.Application.DTO;
+using datingApp.Application.Queries;
+using datingApp.Core.Repositories;
+
+namespace datingApp.Infrastructure.DAL.Handlers;
+
+public class GetMatchesHandler : IQueryHandler<GetMatches, IEnumerable<MatchDto>>
+{
+    private readonly IMatchRepository _matchRepository;
+    public GetMatchesHandler(IMatchRepository matchRepository)
+    {
+        _matchRepository = matchRepository;
+    }
+    
+    public async Task<IEnumerable<MatchDto>> HandleAsync(GetMatches query)
+    {
+        var matches = await _matchRepository.GetByUserIdAsync(query.UserId);
+        return matches.Select(x => x.AsDto()).ToList();
+    }
+}

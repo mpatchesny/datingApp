13a976e4a502bad20e22f7b327f39de99a1250b2
M. Patchesny <strielok11+github@gmail.com>
2023-04-02 11:38:22 +0200

    add more query properties

--- src/datingApp.Application/Queries/GetSwipeCandidates.cs
+++ src/datingApp.Application/Queries/GetSwipeCandidates.cs
@@ -10,4 +10,10 @@ namespace datingApp.Application.Queries;
 public class GetSwipeCandidates : IQuery<IEnumerable<PublicUserDto>>
 {
     public int UserId { get; set; }
+    public int AgeFrom { get; set; }
+    public int AgeTo { get; set; }
+    public int Range { get; set; }
+    public int Sex { get; set; }
+    public double Lat { get; set; }
+    public double Lon { get; set; }
 }

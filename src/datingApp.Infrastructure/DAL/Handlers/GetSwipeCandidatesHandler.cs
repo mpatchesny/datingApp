baf74ea157e143ab5e92cb8867bfc46e084aeae3
M. Patchesny <strielok11+github@gmail.com>
2023-04-02 16:13:11 +0200

    fix ingoring swipped candidates; add approximate locaiton square to initially ignore users outside range

--- src/datingApp.Infrastructure/DAL/Handlers/GetSwipeCandidatesHandler.cs
+++ src/datingApp.Infrastructure/DAL/Handlers/GetSwipeCandidatesHandler.cs
@@ -28,21 +28,41 @@ internal sealed class GetSwipeCandidatesHandler : IQueryHandler<GetSwipeCandidat
         DateOnly minDob = new DateOnly(now.Year - query.AgeTo, now.Month, now.Day);
         DateOnly maxDob = new DateOnly(now.Year - query.AgeFrom, now.Month, now.Day);
 
-        // we want only candidates who haven't been swiped
-        // and are different from user who requests candidates
-        var notSwippedCandidates = 
-            from u in _dbContext.Users.Where(x => x.Id != query.UserId)
-            from s in _dbContext.Swipes.Where(s => s.SwippedById != query.UserId).DefaultIfEmpty()
-            select new { u.Id };
+        double xne = query.Lat + query.Range * 0.009;
+        double yne = query.Lon + query.Range * 0.009;
 
-        // we want candidates that matches sex, age and range
+        double xnw = query.Lat - query.Range * 0.009;
+        double ynw = query.Lon + query.Range * 0.009;
+
+        double xse = query.Lat + query.Range * 0.009;
+        double yse = query.Lon - query.Range * 0.009;
+
+        double xsw = query.Lat - query.Range * 0.009;
+        double ysw = query.Lon - query.Range * 0.009;
+
+        // we want only candidates who haven't been swiped by user who requests
+        // and are different from user who requests candidates
+        var swippedCandidates = 
+            _dbContext.Swipes.Where(s => s.SwippedById == query.UserId).Select(x => x.SwippedWhoId);
+
+        // we want candidates that matches sex, age and range of user who requests
         // we want candidates sorted by number of likes descending
-        return _dbContext.Users
-                    .Where(x => notSwippedCandidates.Select(u => u.Id).Contains(x.Id))
+        var candidates = _dbContext.Users
+                    .Where(x => x.Id != query.UserId)
+                    .Where(x => !swippedCandidates.Contains(x.Id))
                     .Where(x => ((int) x.Sex & query.Sex) > 0)
+                    .Where(x => x.Settings.Lat <= yne)
+                    .Where(x => x.Settings.Lat <= ynw)
+                    .Where(x => x.Settings.Lat >= ysw)
+                    .Where(x => x.Settings.Lat >= yse)
+                    .Where(x => x.Settings.Lon <= xne)
+                    .Where(x => x.Settings.Lon <= xse)
+                    .Where(x => x.Settings.Lon >= xnw)
+                    .Where(x => x.Settings.Lon >= xsw)
                     .Where(x => x.DateOfBirth >= minDob && x.DateOfBirth <= maxDob)
                     .Include(x => x.Photos)
-                    .AsNoTracking()
-                    .Select(x => x.AsPublicDto()).ToList();
+                    .AsNoTracking();
+
+        return candidates.Select(x => x.AsPublicDto()).ToList();
     }
 }

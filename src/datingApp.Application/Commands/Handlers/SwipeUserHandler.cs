7bf40d22184442581ee768c89aecf9260301f52e
M. Patchesny <strielok11+github@gmail.com>
2023-04-03 00:01:47 +0200

    add check if swipped user exists

--- src/datingApp.Application/Commands/Handlers/SwipeUserHandler.cs
+++ src/datingApp.Application/Commands/Handlers/SwipeUserHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
+using datingApp.Application.Exceptions;
 using datingApp.Core.Entities;
 using datingApp.Core.Repositories;
 
@@ -11,13 +12,21 @@ namespace datingApp.Application.Commands.Handlers;
 public sealed class SwipeUserHandler : ICommandHandler<SwipeUser>
 {
     private readonly ISwipeRepository _swipeRepository;
-    public SwipeUserHandler(ISwipeRepository swipeRepository)
+    private readonly IUserRepository _userRepository;
+    public SwipeUserHandler(ISwipeRepository swipeRepository, IUserRepository userRepository)
     {
         _swipeRepository = swipeRepository;
+        _userRepository = userRepository;
     }
 
     public async Task HandleAsync(SwipeUser command)
     {
+        var user = await _userRepository.GetByIdAsync(command.SwipedWhoId);
+        if (user == null)
+        {
+            throw new UserNotExists(command.SwipedWhoId);
+        }
+
         var swipe = new Swipe(0, command.SwipedById, command.SwipedWhoId, (Like) command.Like, DateTime.UtcNow);
         await _swipeRepository.AddAsync(swipe);
     }

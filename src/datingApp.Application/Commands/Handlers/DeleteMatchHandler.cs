64f1f83b58748ef30a731a8c21630495f2721c70
M. Patchesny <strielok11+github@gmail.com>
2023-04-03 21:46:57 +0200

    add

---
+++ src/datingApp.Application/Commands/Handlers/DeleteMatchHandler.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using datingApp.Application.Exceptions;
+using datingApp.Core.Repositories;
+
+namespace datingApp.Application.Commands.Handlers;
+
+public class DeleteMatchHandler : ICommandHandler<DeleteMatch>
+{
+    private readonly IMatchRepository _matchRepository;
+    public DeleteMatchHandler(IMatchRepository matchRepository)
+    {
+        _matchRepository = matchRepository;
+    }
+    public async Task HandleAsync(DeleteMatch command)
+    {
+        var match = _matchRepository.GetByIdAsync(command.MatchId);
+        if (match == null)
+        {
+            throw new MatchNotExistsException(command.MatchId);
+        }
+
+        await _matchRepository.DeleteAsync(command.MatchId);
+    }
+}

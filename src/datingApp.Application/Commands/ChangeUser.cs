d4146d5e5b30c2daabb97eefc2b9a6d595bec293
M. Patchesny <strielok11+github@gmail.com>
2023-04-04 21:48:26 +0200

    change command

--- src/datingApp.Application/Commands/ChangeUser.cs
+++ src/datingApp.Application/Commands/ChangeUser.cs
@@ -6,4 +6,4 @@ using datingApp.Application.Abstractions;
 
 namespace datingApp.Application.Commands;
 
-public sealed record ChangeUser(int UserId, DateOnly DateOfBirth, string Bio="", string Job="") : ICommand;
+public sealed record ChangeUser(int UserId, DateOnly? DateOfBirth=null, string Bio=null, string Job=null) : ICommand;

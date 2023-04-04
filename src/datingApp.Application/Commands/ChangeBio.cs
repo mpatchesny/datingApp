ff56e52b1eda825875d6c61d29d70724eb4f5365
M. Patchesny <strielok11+github@gmail.com>
2023-04-04 19:15:59 +0200

    remove

--- src/datingApp.Application/Commands/ChangeBio.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using datingApp.Application.Abstractions;
-
-namespace datingApp.Application.Commands;
-
-public sealed record ChangeBio(int userId, string bio) : ICommand;
--- src/datingApp.Application/Commands/ChangeJob.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using datingApp.Application.Abstractions;
-
-namespace datingApp.Application.Commands;
-
-public sealed record ChangeJob(int userId, string job) : ICommand;

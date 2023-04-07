27f80d4d094359e41cc4dcde4a581693df572c3e
M. Patchesny <strielok11+github@gmail.com>
2023-04-07 23:33:22 +0200

    add

---
+++ src/datingApp.Api/Controllers/MatchesController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using datingApp.Application.Commands;
+using datingApp.Application.DTO;
+using datingApp.Application.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datingApp.Api.Controllers;
+
+[ApiController]
+[Route("matches")]
+public class MatchesController : ControllerBase
+{
+    private readonly IQueryHandler<GetMatches, IEnumerable<MatchDto>> _getMatchesHandler;
+    private readonly IQueryHandler<GetMessages, IEnumerable<MessageDto>> _getMessagesHandler;
+    private readonly ICommandHandler<DeleteMatch> _deleteMatchHandler;
+    public MatchesController(IQueryHandler<GetMatches, IEnumerable<MatchDto>> getMatchesHandler,
+                             ICommandHandler<DeleteMatch> deleteMatchHandler,
+                             IQueryHandler<GetMessages, IEnumerable<MessageDto>> getMessagesHandler)
+    {
+        _getMatchesHandler = getMatchesHandler;
+        _deleteMatchHandler = deleteMatchHandler;
+        _getMessagesHandler = getMessagesHandler;
+    }
+
+    [HttpGet("{userId:int}")]
+    public async Task<ActionResult<IEnumerable<MatchDto>>> Get(int userId)
+    {
+        return Ok(await _getMatchesHandler.HandleAsync(new GetMatches { UserId = userId }));
+    }
+
+    [HttpGet("{matchId:int}/messages")]
+    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessages(int matchId)
+    {
+        return Ok(await _getMessagesHandler.HandleAsync(new GetMessages { MatchId = matchId }));
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> Delete(int matchId)
+    {
+        await _deleteMatchHandler.HandleAsync(new DeleteMatch(matchId));
+        return Ok();
+    }
+
+}

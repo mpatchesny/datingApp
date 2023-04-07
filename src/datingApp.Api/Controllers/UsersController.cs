a3f8018f0bf2dc87c8575fcdd463d9bee34655df
M. Patchesny <strielok11+github@gmail.com>
2023-04-07 21:30:22 +0200

    add Post, Patch methods

--- src/datingApp.Api/Controllers/UsersController.cs
+++ src/datingApp.Api/Controllers/UsersController.cs
@@ -17,14 +17,28 @@ public class UserController : ControllerBase
     private readonly IQueryHandler<GetPublicUser, PublicUserDto> _getPublicUserHandler;
     private readonly IQueryHandler<GetPrivateUser, PrivateUserDto> _getPrivateUserHandler;
     private readonly ICommandHandler<SignUp> _signUpHandler;
+    private readonly ICommandHandler<ChangeUser> _changeUserHandler;
 
     public UserController(IQueryHandler<GetPublicUser, PublicUserDto> getUserHandler,
                             ICommandHandler<SignUp> signUpHandler,
-                            IQueryHandler<GetPrivateUser, PrivateUserDto> getPrivateUserHandler)
+                            IQueryHandler<GetPrivateUser, PrivateUserDto> getPrivateUserHandler,
+                            ICommandHandler<ChangeUser> changeUserHandler)
     {
         _getPublicUserHandler = getUserHandler;
         _signUpHandler = signUpHandler;
         _getPrivateUserHandler = getPrivateUserHandler;
+        _changeUserHandler = changeUserHandler;
+    }
+
+    [HttpGet("me")]
+    public async Task<ActionResult<PrivateUserDto>> Get()
+    {
+        var user = await _getPrivateUserHandler.HandleAsync(new GetPrivateUser { UserId = 1 });
+        if (user is null)
+        {
+            return NotFound();
+        }
+        return user;
     }
 
     [HttpGet("{userId:int}")]
@@ -39,10 +53,22 @@ public class UserController : ControllerBase
     }
 
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> Post(SignUp command)
     {
         await _signUpHandler.HandleAsync(command);
         // var user = await _getPublicUserHandler.HandleAsync(new GetPrivateUser { UserId });
-        return CreatedAtAction(nameof(Get), null, null);
+        return CreatedAtAction(nameof(Get), new { userId = 1 });
+    }
+
+    [HttpPatch]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> Patch(ChangeUser command)
+    {
+        await _changeUserHandler.HandleAsync(command);
+        // var user = await _getPublicUserHandler.HandleAsync(new GetPrivateUser { UserId });
+        return CreatedAtAction(nameof(Get), new { userId = 1 });
     }
 }

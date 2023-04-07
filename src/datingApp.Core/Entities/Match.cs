15334ed9d5be33ba90b7e2aee713fa33d2801c47
M. Patchesny <strielok11+github@gmail.com>
2023-04-07 22:13:12 +0200

    fix Messages is null when object initialized yb EF framework

--- src/datingApp.Core/Entities/Match.cs
+++ src/datingApp.Core/Entities/Match.cs
@@ -10,7 +10,7 @@ public class Match
     public int Id { get; }
     public int UserId1 { get; private set; }
     public int UserId2 { get; private set; }
-    public IEnumerable<Message> Messages { get; private set; }
+    public IEnumerable<Message> Messages { get; private set; } = new List<Message>();
     public DateTime CreatedAt { get; private set; }
 
     private Match()
@@ -24,7 +24,6 @@ public class Match
         UserId1 = userId1;
         UserId2 = userId2;
         Messages = messages;
-        if (messages == null) Messages = new List<Message>();
         CreatedAt = createdAt;
     }
 }

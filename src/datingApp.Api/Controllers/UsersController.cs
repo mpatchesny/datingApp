58788f840a639ee3acb22cebe88f4a829e74c1b7
M. Patchesny <strielok11+github@gmail.com>
2023-04-07 19:25:08 +0200

    add Post method

--- src/datingApp.Api/Controllers/UsersController.cs
+++ src/datingApp.Api/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
+using datingApp.Application.Commands;
 using datingApp.Application.DTO;
 using datingApp.Application.Queries;
 using Microsoft.AspNetCore.Mvc;
@@ -13,21 +14,35 @@ namespace datingApp.Api.Controllers;
 [Route("users")]
 public class UserController : ControllerBase
 {
-    private readonly IQueryHandler<GetPublicUser, PublicUserDto> _getUserHandler;
+    private readonly IQueryHandler<GetPublicUser, PublicUserDto> _getPublicUserHandler;
+    private readonly IQueryHandler<GetPrivateUser, PrivateUserDto> _getPrivateUserHandler;
+    private readonly ICommandHandler<SignUp> _signUpHandler;
 
-    public UserController(IQueryHandler<GetPublicUser, PublicUserDto> getUserHandler)
+    public UserController(IQueryHandler<GetPublicUser, PublicUserDto> getUserHandler,
+                            ICommandHandler<SignUp> signUpHandler,
+                            IQueryHandler<GetPrivateUser, PrivateUserDto> getPrivateUserHandler)
     {
-        _getUserHandler = getUserHandler;
+        _getPublicUserHandler = getUserHandler;
+        _signUpHandler = signUpHandler;
+        _getPrivateUserHandler = getPrivateUserHandler;
     }
 
     [HttpGet("{userId:int}")]
     public async Task<ActionResult<PublicUserDto>> Get(int userId)
     {
-        var user = await _getUserHandler.HandleAsync(new GetPublicUser { UserId = userId });
+        var user = await _getPublicUserHandler.HandleAsync(new GetPublicUser { UserId = userId });
         if (user is null)
         {
             return NotFound();
         }
         return user;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post(SignUp command)
+    {
+        await _signUpHandler.HandleAsync(command);
+        // var user = await _getPublicUserHandler.HandleAsync(new GetPrivateUser { UserId });
+        return CreatedAtAction(nameof(Get), null, null);
     }
 }

be534d5669728ff1b02b1f865eb7f5f148c283ac
M. Patchesny <strielok11+github@gmail.com>
2023-04-07 21:43:56 +0200

    add

---
+++ src/datingApp.Application/Exceptions/FailToConvertBase64StringToArrayOfBytes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Core.Exceptions;
+
+namespace datingApp.Application.Exceptions;
+
+public class FailToConvertBase64StringToArrayOfBytes : CustomException
+{
+    public FailToConvertBase64StringToArrayOfBytes() : base("Failed to convert base 64 string to array of bytes.")
+    {
+    }
+}

8ec5c642ee983f2363c754a0ded489397fb8162c
M. Patchesny <strielok11+github@gmail.com>
2023-04-09 20:57:41 +0200

    fix test due to changes in spatial

--- tests/datingApp.Tests.Integration/QueryHandlers/GetSwipeCandidatesHandlerTests.cs
+++ tests/datingApp.Tests.Integration/QueryHandlers/GetSwipeCandidatesHandlerTests.cs
@@ -333,15 +333,15 @@ public class GetSwipeCandidatesHandlerTests : IDisposable
         mockedSpatial.Setup(m => m.CalculateDistance(0.0, 0.0, 0.0, 0.0)).Returns(25);
         mockedSpatial.Setup(m => m.CalculateDistance(1.0, 1.0, 1.0, 1.0)).Returns(26);
 
-        var list = new List<(double lat, double lon)>();
-        (double lat, double lon) ne = (2.0, 2.0);
-        (double lat, double lon) nw = (2.0, -1.0);
-        (double lat, double lon) se = (-1.0, 2.0);
-        (double lat, double lon) sw = (-1.0, -1.0);
-        list.Add(ne);
-        list.Add(nw);
-        list.Add(se);
-        list.Add(sw);
+        var list = new List<double>();
+        double north = (2.0);
+        double south = (-1.0);
+        double east = ( 2.0);
+        double west = (-1.0);
+        list.Add(north);
+        list.Add(south);
+        list.Add(east);
+        list.Add(west);
 
         mockedSpatial.Setup(m => m.GetApproxSquareAroundPoint(It.IsAny<double>(), It.IsAny<double>(), It.IsAny<int>()))
             .Returns((double x, double y, int z) => list);

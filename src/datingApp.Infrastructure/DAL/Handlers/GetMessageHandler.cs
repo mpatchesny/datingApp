76a051db591f1ccfefc1b3d14d45e09e5faaeca2
M. Patchesny <strielok11+github@gmail.com>
2023-04-11 23:22:21 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Handlers/GetMessageHandler.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using datingApp.Application.DTO;
+using datingApp.Application.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace datingApp.Infrastructure.DAL.Handlers;
+
+internal sealed class GetMessageHandler : IQueryHandler<GetMessage, MessageDto>
+{
+    private readonly DatingAppDbContext _dbContext;
+    public GetMessageHandler(DatingAppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<MessageDto> HandleAsync(GetMessage query)
+    {
+        var message = await _dbContext.Messages
+                            .AsNoTracking()
+                            .SingleOrDefaultAsync(x => x.Id == query.MessageId);
+        return message?.AsDto();
+    }
+}

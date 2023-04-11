287c341546e1c415f7a6cf13d9321a0ccdc83df7
M. Patchesny <strielok11+github@gmail.com>
2023-04-11 23:42:57 +0200

    add like, pass Get endpoints and required backend

---
+++ src/datingApp.Api/Controllers/LikeController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using datingApp.Application.Commands;
+using datingApp.Application.DTO;
+using datingApp.Application.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datingApp.Api.Controllers;
+
+[ApiController]
+[Route("like")]
+public class LikeController : ControllerBase
+{
+    private readonly ICommandHandler<SwipeUser> _swipeUserHandler;
+    private readonly IQueryHandler<GetMatch, IsMatchDto> _getMatchHandler;
+    public LikeController(ICommandHandler<SwipeUser> swipeUserHandler,
+                        IQueryHandler<GetMatch, IsMatchDto> getMatchHandler)
+    {
+        _swipeUserHandler = swipeUserHandler;
+        _getMatchHandler = getMatchHandler;
+    }
+
+    [HttpGet("{userId:guid}")]
+    public async Task<ActionResult> Get(Guid userId)
+    {
+        var swippedUserId = Guid.Parse("00000000-0000-0000-0000-000000000001");
+        var command = new SwipeUser(Guid.NewGuid(), swippedUserId, userId, 2);
+        await _swipeUserHandler.HandleAsync(command);
+        return Ok(await _getMatchHandler.HandleAsync(new GetMatch { SwipedById = command.SwipedById, SwipedWhoId = command.SwipedWhoId }));
+    }
+}
---
+++ src/datingApp.Api/Controllers/PassController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using datingApp.Application.Abstractions;
+using datingApp.Application.Commands;
+using datingApp.Application.DTO;
+using datingApp.Application.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datingApp.Api.Controllers;
+
+[ApiController]
+[Route("like")]
+public class PassController : ControllerBase
+{
+    private readonly ICommandHandler<SwipeUser> _swipeUserHandler;
+    private readonly IQueryHandler<GetMatch, IsMatchDto> _getMatchHandler;
+    public PassController(ICommandHandler<SwipeUser> swipeUserHandler,
+                        IQueryHandler<GetMatch, IsMatchDto> getMatchHandler)
+    {
+        _swipeUserHandler = swipeUserHandler;
+        _getMatchHandler = getMatchHandler;
+    }
+
+    [HttpGet("{userId:guid}")]
+    public async Task<ActionResult> Get(Guid userId)
+    {
+        var swippedUserId = Guid.Parse("00000000-0000-0000-0000-000000000001");
+        var command = new SwipeUser(Guid.NewGuid(), swippedUserId, userId, 1);
+        await _swipeUserHandler.HandleAsync(command);
+        return Ok(await _getMatchHandler.HandleAsync(new GetMatch { SwipedById = command.SwipedById, SwipedWhoId = command.SwipedWhoId }));
+    }
+}

d79a62ce45e12534f0993fac1884872f934ea6ed
M. Patchesny <strielok11+github@gmail.com>
2023-04-12 23:04:49 +0200

    add

---
+++ src/datingApp.Application/Commands/Handlers/ChangeMessageHandler.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using datingApp.Application.Exceptions;
+using datingApp.Core.Repositories;
+
+namespace datingApp.Application.Commands.Handlers;
+
+public class ChangeMessageHandler : ICommandHandler<ChangeMessage>
+{
+    private readonly IMessageRepository _messageRepository;
+    public ChangeMessageHandler(IMessageRepository messageRepository)
+    {
+        _messageRepository = messageRepository;
+    }
+
+    public async Task HandleAsync(ChangeMessage command)
+    {
+        var message = await _messageRepository.GetByIdAsync(command.MessageId);
+        if (message == null)
+        {
+            throw new MessageNotExistsException(command.MessageId);
+        }
+        
+        if (command.IsDisplayed)
+        {
+            message.SetDisplayed();
+            await _messageRepository.UpdateAsync(message);
+        }
+    }
+}

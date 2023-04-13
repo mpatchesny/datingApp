f91f06ad1f83a7c159a780c814bcff0abfd6f997
M. Patchesny <strielok11+github@gmail.com>
2023-04-13 22:29:59 +0200

    make setters private; add public methods to change Page, PageSize with validation

--- src/datingApp.Application/Queries/PaginatedQuery.cs
+++ src/datingApp.Application/Queries/PaginatedQuery.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
+using datingApp.Application.Exceptions;
 
 namespace datingApp.Application.Queries;
 
 public class PaginatedQuery
 {
-    public int Page { get; set; } = 1;
-    public int PageSize { get; set; } = 15;
+    public int Page { get; private set; } = 1;
+    public int PageSize { get; private set; } = 15;
+
+    private const int MaxPageSize = 100;
+
+    public void SetPage(int? page)
+    {
+        if (page == null) return;
+        if (page < 0)
+        {
+            throw new InvalidPageNumberException();
+        }
+        Page = (int) page;
+    }
+
+    public void SetPageSize(int? pageSize)
+    {
+        if (pageSize == null) return;
+        if (pageSize < 0 || pageSize > MaxPageSize)
+        {
+            throw new InvalidPageSizeException(0, MaxPageSize);
+        }
+        PageSize = (int) pageSize;
+    }
 }

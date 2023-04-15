ab0e7e3c8efb80c30cf56db038cf386a48ec6156
M. Patchesny <strielok11+github@gmail.com>
2023-04-15 23:01:05 +0200

    add

---
+++ src/datingApp.Application/Commands/Handlers/SignInByEmailHandler.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using datingApp.Application.Exceptions;
+using datingApp.Core.Repositories;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace datingApp.Application.Commands.Handlers;
+
+public class SignInByEmailHandler : ICommandHandler<SignInByEmail>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMemoryCache _cache;
+    public SignInByEmailHandler(IUserRepository userRepository, IMemoryCache cache)
+    {
+        _userRepository = userRepository;
+        _cache = cache;
+    }
+    
+    public async Task HandleAsync(SignInByEmail command)
+    {
+        var user = await _userRepository.GetByEmailAsync(command.Email);
+        if (user == null)
+        {
+            throw new UserNotExistsException();
+        }
+        
+        var code = _cache.GetCode(command.Email);
+        if (code == null)
+        {
+            throw new InvalidCredentialsException();
+        }
+        else if (code.Code != command.Code || code.EmailOrPhone != command.Email.ToLowerInvariant())
+        {
+            throw new InvalidCredentialsException();
+        }
+
+        // Generowanie tokenu
+    }
+}

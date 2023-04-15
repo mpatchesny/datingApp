fc1231e51cd11b3dc4c049d9173ef4cb30caacb6
M. Patchesny <strielok11+github@gmail.com>
2023-04-15 22:25:12 +0200

    add exception message without userid

--- src/datingApp.Application/Exceptions/UserNotExistsException.cs
+++ src/datingApp.Application/Exceptions/UserNotExistsException.cs
@@ -8,7 +8,11 @@ namespace datingApp.Application.Exceptions;
 
 public class UserNotExistsException : CustomException
 {
+    public UserNotExistsException() : base($"User with given phone/email does not exist.")
+    {
+    }
     public UserNotExistsException(Guid userId) : base($"User with id {userId} does not exist.")
     {
     }
+
 }

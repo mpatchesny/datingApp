77b74e74cdabb2a5de9db3ffef4d400905d50abe
M. Patchesny <strielok11+github@gmail.com>
2023-04-17 00:11:23 +0200

    add

---
+++ src/datingApp.Infrastructure/Security/AccessCodeGenerator.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.DTO;
+using datingApp.Application.Security;
+
+namespace datingApp.Infrastructure.Security;
+
+internal sealed class AccessCodeGenerator : IAccessCodeGenerator
+{
+    private readonly static Random rand = new Random();
+    private readonly string seed = "0123456789QAZXSWEDCVFRTGBNHYUJMKILOP";
+    public AccessCodeGenerator()
+    {
+        // todo: options
+    }
+    public AccessCodeDto GenerateCode(string emailOrPhone)
+    {
+        char[] code = {};
+        for (int i = 0; i < 6; i++)
+        {
+            var randInt = rand.Next(0, seed.Length);
+            code[i] = seed[randInt];
+        }
+
+        return new AccessCodeDto
+        { 
+            AccessCode = code.ToString(),
+            EmailOrPhone = emailOrPhone,
+            Expiry = TimeSpan.FromMinutes(15)
+        };
+    }
+
+    public bool Verify(string code, string EmailOrPhone)
+    {
+        throw new NotImplementedException();
+    }
+}

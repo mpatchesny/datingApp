d482804d46839850e1ce976961250b42bfdaef8c
M. Patchesny <strielok11+github@gmail.com>
2023-04-16 23:51:12 +0200

    add

---
+++ src/datingApp.Infrastructure/Security/AuthOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Infrastructure.Security;
+
+public sealed class AuthOptions
+{
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string SigningKey { get; set; }
+    public TimeSpan? Expiry { get; set; }
+}

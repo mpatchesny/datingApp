fd3935c4a4771532102d95dee0c530027f6d3279
M. Patchesny <strielok11+github@gmail.com>
2023-04-17 22:51:24 +0200

    fix no case sensitive

--- src/datingApp.Application/Security/AccessCodeVerificator.cs
+++ src/datingApp.Application/Security/AccessCodeVerificator.cs
@@ -11,7 +11,7 @@ public class AccessCodeVerificator
     public bool Verify(AccessCodeDto accessCode, string userGivenAccessCode, string userEmail)
     {
         bool isCodeValid = accessCode.AccessCode.ToLowerInvariant() == userGivenAccessCode.ToLowerInvariant();
-        isCodeValid = isCodeValid && accessCode.EmailOrPhone == userEmail.ToLowerInvariant();
+        isCodeValid = isCodeValid && accessCode.EmailOrPhone.ToLowerInvariant() == userEmail.ToLowerInvariant();
         isCodeValid = isCodeValid && accessCode.ExpirationTime >= DateTime.UtcNow;
         return isCodeValid;
     }

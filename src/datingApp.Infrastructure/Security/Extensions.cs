19f960c309408a0ce535b103a7e6666eec818fae
M. Patchesny <strielok11+github@gmail.com>
2023-04-17 22:20:01 +0200

    add

---
+++ src/datingApp.Infrastructure/Security/Extensions.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using datingApp.Application.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace datingApp.Infrastructure.Security;
+
+internal static class Extensions
+{
+    private const string OptionsSectionName = "auth";
+    
+    public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        var options = configuration.GetOptions<AuthOptions>(OptionsSectionName);
+
+        services.AddMemoryCache();
+        services.AddSingleton<IAccessCodeStorage, InMemoryAccessCodeStorage>();
+        services.Configure<AuthOptions>(configuration.GetRequiredSection(OptionsSectionName));
+        services.AddSingleton<IAuthenticator, Authenticator>();
+        // services.AddSingleton<ITokenStorage, HttpContextTokenStorage>();
+        services.AddAuthentication(o =>
+                {
+                    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(o =>
+                {
+                    o.Audience = options.Audience;
+                    o.IncludeErrorDetails = true;
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = options.Issuer,
+                        ClockSkew = TimeSpan.Zero,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.SigningKey))
+                    };
+                });
+        return services;
+    }
+}

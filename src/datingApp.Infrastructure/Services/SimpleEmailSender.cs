3e7ac3c91a8ad4b747645457b8d4308f9733d00e
M. Patchesny <strielok11+github@gmail.com>
2023-04-19 22:19:30 +0200

    add sendasync method body

--- src/datingApp.Infrastructure/Services/SimpleEmailSender.cs
+++ src/datingApp.Infrastructure/Services/SimpleEmailSender.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using datingApp.Application.Services;
 using Microsoft.Extensions.Options;
@@ -15,9 +16,40 @@ internal sealed class SimpleEmailSender : IEmailSender
         _options = options;
     }
 
-    public Task SendAsync(string receiver, string subject, string body)
+    public async Task SendAsync(string receiver, string subject, string body)
     {
-        return Task.CompletedTask;
-        // throw new NotImplementedException();
+        MailMessage message = new MailMessage(
+            _options.Value.SendFrom,
+            receiver,
+            subject,
+            body);
+
+        int port = 0;
+        if (!int.TryParse(_options.Value.ServerPort, out port))
+        {
+            // TODO: add logging
+            new InvalidCastException($"Port {_options.Value.ServerPort} cannot be cast to integer.");
+        }
+
+        using (var client = new SmtpClient(_options.Value.ServerAddress, port))
+        {
+            try
+            {
+                client.EnableSsl = _options.Value.EnableSsl;
+                client.Credentials = new System.Net.NetworkCredential(_options.Value.Username, _options.Value.Password);
+                client.Timeout = 5000;
+                client.UseDefaultCredentials = false;
+                await client.SendMailAsync(message);
+            }
+            catch
+            {
+                // TODO: add logging
+                throw;
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
     }
 }

a711c45d8bad80192898e314750a94127cd130b4
M. Patchesny <strielok11+github@gmail.com>
2023-04-21 00:12:45 +0200

    match is added when get match handler returns true; otherwise match is not added

--- src/datingApp.Api/Controllers/LikeController.cs
+++ src/datingApp.Api/Controllers/LikeController.cs
@@ -35,8 +35,8 @@ public class LikeController : ControllerBase
         var swippedByUserId = Guid.Parse(User.Identity?.Name);
         var command = new SwipeUser(Guid.NewGuid(), swippedByUserId, userId, 2);
         await _swipeUserHandler.HandleAsync(command);
-        _addMatchHandler.HandleAsync(new AddMatch(swippedByUserId));
         var isMatch = await _getMatchHandler.HandleAsync(new GetMatch { SwipedById = command.SwippedById, SwipedWhoId = command.SwippedWhoId });
+        if (isMatch.Match == true) await _addMatchHandler.HandleAsync(new AddMatch(swippedByUserId, userId));
         return isMatch;
     }
 }

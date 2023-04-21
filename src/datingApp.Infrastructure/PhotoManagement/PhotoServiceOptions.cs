0170b0c602ce7aa1f2ea642658a859cf0bbaf402
M. Patchesny <strielok11+github@gmail.com>
2023-04-21 18:46:43 +0200

    add

---
+++ src/datingApp.Infrastructure/PhotoManagement/PhotoServiceOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Infrastructure.PhotoManagement;
+
+public sealed class PhotoServiceOptions
+{
+    public string StoragePath { get; set; }
+}

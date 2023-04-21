1b3e4efbf801c19614118ea4e201de5dbb8e3d97
M. Patchesny <strielok11+github@gmail.com>
2023-04-21 23:34:47 +0200

    fix tests

--- tests/datingApp.Tests.Unit/PhotoManagement/PhotoServiceTests.cs
+++ tests/datingApp.Tests.Unit/PhotoManagement/PhotoServiceTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using datingApp.Infrastructure;
 using datingApp.Infrastructure.Exceptions;
 using datingApp.Infrastructure.PhotoManagement;
 using Microsoft.Extensions.Options;
@@ -15,7 +16,8 @@ public class PhotoServiceTests
     public void get_image_file_format_returns_null_if_unkown_format()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        var service = new PhotoService(options, storageOptions);
         byte[] photo = new byte[] {0x74, 0x65, 0x73, 0x74};
         var ext = service.GetImageFileFormat(photo);
         Assert.Null(ext);
@@ -25,7 +27,8 @@ public class PhotoServiceTests
     public void given_invalid_unknown_file_format_validate_should_throw_exception()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        var service = new PhotoService(options, storageOptions);
         byte[] photo = new byte[] {0x74, 0x65, 0x73, 0x74};
         options.Value.MaxPhotoSizeBytes=10000;
         options.Value.MinPhotoSizeBytes=1;
@@ -38,7 +41,8 @@ public class PhotoServiceTests
     public void get_image_file_format_returns_jpg_if_given_jpg_file_header()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        var service = new PhotoService(options, storageOptions);
         byte[] photo = new byte[] {0xFF, 0xD8, 0xFF};
         var ext = service.GetImageFileFormat(photo);
         Assert.Equal("jpg", ext);
@@ -48,7 +52,8 @@ public class PhotoServiceTests
     public void get_image_file_format_returns_bmp_if_given_bmp_file_header()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        var service = new PhotoService(options, storageOptions);
         options.Value.MaxPhotoSizeBytes=10000;
         options.Value.MinPhotoSizeBytes=100;
         byte[] photo = new byte[] {0x42, 0x4D};
@@ -60,7 +65,8 @@ public class PhotoServiceTests
     public void get_image_file_format_returns_bmp_if_given_png_file_header()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        var service = new PhotoService(options, storageOptions);
         byte[] photo = new byte[] {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
         var ext = service.GetImageFileFormat(photo);
         Assert.Equal("png", ext);
@@ -70,7 +76,8 @@ public class PhotoServiceTests
     public void given_proper_base64_encoded_string_convert_to_array_of_bytes_should_succeed()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        var service = new PhotoService(options, storageOptions);
         string Base64Bytes = "dGVzdA==";
         var exception = Record.Exception(() => service.ConvertToArrayOfBytes(Base64Bytes));
         Assert.Null(exception);
@@ -80,7 +87,8 @@ public class PhotoServiceTests
     public void given_invalid_base64_encoded_string_convert_to_array_of_bytes_should_throw_exception()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        var service = new PhotoService(options, storageOptions);
         string Base64Bytes = "sd";
         var exception = Record.Exception(() => service.ConvertToArrayOfBytes(Base64Bytes));
         Assert.NotNull(exception);
@@ -91,9 +99,10 @@ public class PhotoServiceTests
     public void too_small_photo_should_throw_exception()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
         options.Value.MaxPhotoSizeBytes=10000;
         options.Value.MinPhotoSizeBytes=100;
-        var service = new PhotoService(options);
+        var service = new PhotoService(options, storageOptions);
         byte[] photo = new byte[99];
         var exception = Record.Exception(() => service.ValidatePhoto(photo));
         Assert.NotNull(exception);
@@ -104,9 +113,10 @@ public class PhotoServiceTests
     public void too_big_photo_should_throw_exception()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
         options.Value.MaxPhotoSizeBytes=100;
         options.Value.MinPhotoSizeBytes=99;
-        var service = new PhotoService(options);
+        var service = new PhotoService(options, storageOptions);
         byte[] photo = new byte[101];
         var exception = Record.Exception(() => service.ValidatePhoto(photo));
         Assert.NotNull(exception);
@@ -117,8 +127,9 @@ public class PhotoServiceTests
     public void file_is_saved_to_storage_path()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        options.Value.StoragePath = "./";
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        storageOptions.Value.StoragePath = "./";
+        var service = new PhotoService(options, storageOptions);
         byte[] photo = new byte[] {0x74, 0x65, 0x73, 0x74};
         var path = service.SavePhoto(photo, "test", "txt");
         var fileExists = System.IO.File.Exists(path);
@@ -130,8 +141,9 @@ public class PhotoServiceTests
     public void directory_is_created_if_needed_upon_save_photo()
     {
         IOptions<PhotoServiceOptions> options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        options.Value.StoragePath = testDirectoryPath;
-        var service = new PhotoService(options);
+        IOptions<StorageOptions> storageOptions = Options.Create<StorageOptions>(new StorageOptions());
+        storageOptions.Value.StoragePath = testDirectoryPath;
+        var service = new PhotoService(options, storageOptions);
         byte[] photo = new byte[] {0x74, 0x65, 0x73, 0x74};
         var path = service.SavePhoto(photo, "test", "txt");
         var folderExists = System.IO.Directory.Exists(testDirectoryPath);

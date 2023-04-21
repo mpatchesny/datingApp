60756b79196c5acb3b52c357ec80f3012edf6e5d
M. Patchesny <strielok11+github@gmail.com>
2023-04-21 18:46:56 +0200

    add extension argument to SavePhoto method

--- src/datingApp.Application/PhotoManagement/IPhotoService.cs
+++ src/datingApp.Application/PhotoManagement/IPhotoService.cs
@@ -7,5 +7,5 @@ namespace datingApp.Application.PhotoManagement;
 
 public interface IPhotoService
 {
-    public string SavePhoto(byte[] photo);
+    public string SavePhoto(byte[] photo, string extension);
 }

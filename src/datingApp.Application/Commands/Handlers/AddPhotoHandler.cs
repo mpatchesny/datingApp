83c7568c123bf8f4a9b3b47d50719ac11b74b1e1
M. Patchesny <strielok11+github@gmail.com>
2023-04-21 18:56:40 +0200

    move conversion photo logic, validation photo logic to photo service class

--- src/datingApp.Application/Commands/Handlers/AddPhotoHandler.cs
+++ src/datingApp.Application/Commands/Handlers/AddPhotoHandler.cs
@@ -24,32 +24,6 @@ public sealed class AddPhotoHandler : ICommandHandler<AddPhoto>
 
     public async Task HandleAsync(AddPhoto command)
     {
-        // https://stackoverflow.com/questions/51300523/how-to-use-span-in-convert-tryfrombase64string
-        byte[] bytes = new byte[((command.Base64Bytes.Length * 3) + 3) / 4 -
-            (command.Base64Bytes.Length > 0 && command.Base64Bytes[command.Base64Bytes.Length - 1] == '=' ?
-                command.Base64Bytes.Length > 1 && command.Base64Bytes[command.Base64Bytes.Length - 2] == '=' ?
-                    2 : 1 : 0)];
-
-        if (!Convert.TryFromBase64String(command.Base64Bytes, bytes, out int bytesWritten))
-        {
-            throw new FailToConvertBase64StringToArrayOfBytes();
-        }
-
-        // FIXME: move min, maxPhotoSize to app settings
-        // // 2MB
-        // const int maxPhotoSizeBytes = 2*1024*1024;
-        // const int maxPhotoSizeMB = (int) maxPhotoSizeBytes / (1024*1024);
-        // const int minPhotoSizeBytes = 100*1024;
-        // const int minPhotoSizeKB = (int) minPhotoSizeBytes / 1024;
-        // if (bytes.Count() > maxPhotoSizeBytes)
-        // {
-        //     throw new InvalidPhotoSizeException(minPhotoSizeKB, maxPhotoSizeMB);
-        // }
-        // if (bytes.Count() < minPhotoSizeBytes)
-        // {
-        //     throw new InvalidPhotoSizeException(minPhotoSizeKB, maxPhotoSizeMB);
-        // }
-
         var user = await _userRepository.GetByIdAsync(command.UserId);
         if (user == null)
         {
@@ -61,6 +35,8 @@ public sealed class AddPhotoHandler : ICommandHandler<AddPhoto>
             throw new UserPhotoLimitException();
         }
 
+        byte[] bytes = _photoService.ConvertToArrayOfBytes(command.Base64Bytes);
+        _photoService.ValidatePhoto(bytes);
         int oridinal = user.Photos.Count();
         var photoPath = _photoService.SavePhoto(bytes, "jpg");
         var photo = new Photo(command.PhotoId, command.UserId, photoPath, oridinal);

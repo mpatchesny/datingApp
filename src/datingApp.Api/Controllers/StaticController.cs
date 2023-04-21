d5b3fcc54366876bd219090d161b75cdaff6c7aa
M. Patchesny <strielok11+github@gmail.com>
2023-04-21 23:35:02 +0200

    storage path taken from StorageOptions section from appsettings

--- src/datingApp.Api/Controllers/StaticController.cs
+++ src/datingApp.Api/Controllers/StaticController.cs
@@ -2,25 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using datingApp.Infrastructure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace datingApp.Api.Controllers;
 
 [Route("storage")]
 public class StaticController : ControllerBase
 {
-    private readonly IWebHostEnvironment _env;
-    public StaticController(IWebHostEnvironment env)
+    private readonly IOptions<StorageOptions> _storageOptions;
+    public StaticController(IOptions<StorageOptions> storageOptions)
     {
-        _env = env;
+        _storageOptions = storageOptions;
     }
 
     [Authorize]
     [HttpGet("{filename}")]
     public ActionResult Get(string filename)
     {
-        var filePath = Path.Combine(_env.ContentRootPath, "storage", filename);
+        var filePath = Path.Combine(_storageOptions.Value.StoragePath, filename);
         if (System.IO.File.Exists(filePath))
         {
             return PhysicalFile(filePath, "image/jpeg");

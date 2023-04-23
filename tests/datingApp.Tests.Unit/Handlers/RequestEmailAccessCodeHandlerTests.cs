b31a50d75f645ed2a4b03c1698ec2d073649531a
M. Patchesny <strielok11+github@gmail.com>
2023-04-23 22:27:08 +0200

    add

---
+++ tests/datingApp.Tests.Unit/Handlers/RequestEmailAccessCodeHandlerTests.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Commands;
+using datingApp.Application.Commands.Handlers;
+using datingApp.Application.DTO;
+using datingApp.Application.Security;
+using datingApp.Application.Services;
+using Moq;
+using Xunit;
+
+namespace datingApp.Tests.Unit.Handlers;
+
+public class RequestEmailAccessCodeHandlerTests
+{
+    [Fact]
+    public async Task code_generator_generate_code_method_should_be_called_once()
+    {
+        var code = new AccessCodeDto() {
+            AccessCode ="12345",
+            EmailOrPhone = "test@test.com",
+            ExpirationTime = DateTime.UtcNow,
+            Expiry = TimeSpan.FromMinutes(15)
+        };
+
+        var codeStorage = new Mock<IAccessCodeStorage>();
+        codeStorage.Setup(m => m.Get(It.IsAny<string>())).Returns(code);
+        codeStorage.Setup(m => m.Set(code));
+
+        var codeGenerator = new Mock<IAccessCodeGenerator>();
+        codeGenerator.Setup(m => m.GenerateCode(It.IsAny<string>())).Returns(code);
+
+        var emailSender = new Mock<IEmailSender>();
+        emailSender.Setup(m => m.SendAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+
+        string email = "test@test.com";
+        var command = new RequestEmailAccessCode(email);
+        var handler = new RequestEmailAccessCodeHandler(codeGenerator.Object, codeStorage.Object, emailSender.Object);
+        await handler.HandleAsync(command);
+
+        codeGenerator.Verify(mock => mock.GenerateCode(email), Times.Once());
+    }
+
+    [Fact]
+    public async Task code_storage_set_method_should_be_called_once()
+    {
+        var code = new AccessCodeDto() {
+            AccessCode ="12345",
+            EmailOrPhone = "test@test.com",
+            ExpirationTime = DateTime.UtcNow,
+            Expiry = TimeSpan.FromMinutes(15)
+        };
+
+        var codeStorage = new Mock<IAccessCodeStorage>();
+        codeStorage.Setup(m => m.Get(It.IsAny<string>())).Returns(code);
+        codeStorage.Setup(m => m.Set(code));
+
+        var codeGenerator = new Mock<IAccessCodeGenerator>();
+        codeGenerator.Setup(m => m.GenerateCode(It.IsAny<string>())).Returns(code);
+
+        var emailSender = new Mock<IEmailSender>();
+        emailSender.Setup(m => m.SendAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+
+        string email = "test@test.com";
+        var command = new RequestEmailAccessCode(email);
+        var handler = new RequestEmailAccessCodeHandler(codeGenerator.Object, codeStorage.Object, emailSender.Object);
+        await handler.HandleAsync(command);
+
+        codeStorage.Verify(mock => mock.Set(code), Times.Once());
+    }
+
+    [Fact]
+    public async Task email_sender_sendasync_method_should_be_called_once()
+    {
+        var code = new AccessCodeDto() {
+            AccessCode ="12345",
+            EmailOrPhone = "test@test.com",
+            ExpirationTime = DateTime.UtcNow,
+            Expiry = TimeSpan.FromMinutes(15)
+        };
+
+        var codeStorage = new Mock<IAccessCodeStorage>();
+        codeStorage.Setup(m => m.Get(It.IsAny<string>())).Returns(code);
+        codeStorage.Setup(m => m.Set(code));
+
+        var codeGenerator = new Mock<IAccessCodeGenerator>();
+        codeGenerator.Setup(m => m.GenerateCode(It.IsAny<string>())).Returns(code);
+
+        var emailSender = new Mock<IEmailSender>();
+        emailSender.Setup(m => m.SendAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+
+        string email = "test@test.com";
+        var command = new RequestEmailAccessCode(email);
+        var handler = new RequestEmailAccessCodeHandler(codeGenerator.Object, codeStorage.Object, emailSender.Object);
+        await handler.HandleAsync(command);
+
+        emailSender.Verify(mock => mock.SendAsync(email, It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+    }
+}

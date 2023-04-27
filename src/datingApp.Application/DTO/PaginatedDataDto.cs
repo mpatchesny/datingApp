c6176847c0c07fe549544145e6c9cf79ae5ca326
M. Paczesny <paczesnymaciej+atlassian@gmail.com>
2023-04-27 14:27:30 +0200

    add

---
+++ src/datingApp.Application/DTO/PaginatedDataDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Application.DTO;
+
+public class PaginatedDataDto
+{
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int PageCount { get; set; }
+    public List<T> Data { get; set; }
+}

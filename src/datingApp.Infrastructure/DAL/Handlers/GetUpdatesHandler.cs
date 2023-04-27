8ae40ccf77df8ebf20bc7e8e6f84fbecbefcbc26
M. Paczesny <paczesnymaciej+atlassian@gmail.com>
2023-04-27 14:52:52 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Handlers/GetUpdatesHandler.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using datingApp.Application.DTO;
+using datingApp.Application.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace datingApp.Infrastructure.DAL.Handlers;
+
+internal sealed class GetUpdatesHandler : IQueryHandler<GetUpdates, IEnumerable<MatchDto>>
+{
+    private readonly DatingAppDbContext _dbContext;
+    public GetMatchesHandler(DatingAppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<MatchDto>> HandleAsync(GetUpdates query)
+    {
+        var newMatches = await _dbContext.Matches
+                        .AsNoTracking()
+                        .Where(x => x.UserId1 == query.UserId || x.UserId2 == query.UserId)
+                        .Where(x => x.CreatedAt >= query.LastActivityTime)
+                        .Select(x => x.Id)
+                        .ToListAsync();
+
+        var newMessages = await _dbContext.Messages
+                        .AsNoTracking()
+                        .Where(x => x.MatchId.Contains(newMatches))
+                        .Where(x => x.CreatedAt >= query.LastActivityTime)
+                        .Select(x => x.MatchId)
+                        .ToListAsync();
+
+        var newMatchesWithNewMessages = newMatches.Union(newMessages);
+
+        return await _dbContext.Matches
+                        .AsNoTracking()
+                        .Where(x => x.UserId1 == query.UserId || x.UserId2 == query.UserId)
+                        .Where(x => x.MatchId.Contains(newMatchesWithNewMessages))
+                        .Select(x => new
+                            {
+                                Match = x,
+                                User = _dbContext.Users.Where(u => u.Id == ((x.UserId1 != query.UserId) ? x.UserId1 : x.UserId2)).FirstOrDefault(),
+                                Photo = _dbContext.Photos.Where(p => p.UserId == ((x.UserId1 != query.UserId) ? x.UserId1 : x.UserId2))
+                                            .FirstOrDefault(p => p.Oridinal == 0)
+                            })
+                        .Select(x =>
+                            new MatchDto()
+                            {
+                                Id = x.Match.Id,
+                                UserId = x.User.Id,
+                                Name = x.User.Name,
+                                IsDisplayed = ((x.Match.UserId1 == query.UserId) ? x.Match.IsDisplayedByUser1 : x.Match.IsDisplayedByUser2),
+                                ProfilePicture = (x.Photo != null) ? x.Photo.AsDto() : null,
+                                Messages = x.Match.Messages.OrderByDescending(m => m.CreatedAt).Take(1).Select(x => x.AsDto()).ToList(),
+                                CreatedAt = DateTime.UtcNow
+                            })
+                        .Skip((query.Page - 1) * query.PageSize)
+                        .Take(query.PageSize)
+                        .ToListAsync();
+    }
+}

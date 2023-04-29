a8a47c0571bbd0559b5b2ab706ad4fd02cd56c90
M. Patchesny <strielok11+github@gmail.com>
2023-04-29 23:37:06 +0200

    add

---
+++ src/datingApp.Application/Services/QueueItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Application.Services
+{
+    public class QueueItem
+    {
+        public int Id { get; set; }
+        public string Data { get; set; }
+    }
+}

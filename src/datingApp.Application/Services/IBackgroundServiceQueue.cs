0d4f981e2bd314b05f5cee551f2b9522d5822a3a
M. Patchesny <strielok11+github@gmail.com>
2023-04-29 23:10:52 +0200

    add

---
+++ src/datingApp.Application/Services/IBackgroundServiceQueue.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Application.Services;
+
+public interface IBackgroundServiceQueue
+{
+    public void Enqueue(dynamic item);
+    public dynamic Dequeue();
+}
---
+++ src/datingApp.Infrastructure/Services/BackgroundServiceQueue.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Services;
+
+namespace datingApp.Infrastructure.Services;
+
+public class BackgroundServiceQueue : IBackgroundServiceQueue
+{
+    private readonly List<dynamic> queue = new List<dynamic>();
+
+    public BackgroundServiceQueue()
+    {
+    }
+
+    public void Enqueue(dynamic item)
+    {
+        queue.Add(item);
+    }
+
+    public dynamic Dequeue()
+    {
+        var item = queue.FirstOrDefault();
+        queue.RemoveAt(0);
+        return item;
+    }
+
+}

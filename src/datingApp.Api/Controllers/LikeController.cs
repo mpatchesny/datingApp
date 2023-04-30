d3ff583cbcd3aae3533cfbd8d5b567e38d295853
M. Patchesny <strielok11+github@gmail.com>
2023-04-30 17:28:32 +0200

    more readable code formatting

--- src/datingApp.Api/Controllers/LikeController.cs
+++ src/datingApp.Api/Controllers/LikeController.cs
@@ -34,10 +34,9 @@ public class LikeController : ControllerBase
         var swippedByUserId = Guid.Parse(User.Identity?.Name);
         var command = new SwipeUser(Guid.NewGuid(), swippedByUserId, userId, 2);
         await _swipeUserHandler.HandleAsync(command);
-        
-        var isMatch = await _getMatchHandler.HandleAsync(new GetMatch { SwipedById = command.SwippedById, SwipedWhoId = command.SwippedWhoId });
         await _addMatchHandler.HandleAsync(new AddMatch(swippedByUserId, userId));
         
+        var isMatch = await _getMatchHandler.HandleAsync(new GetMatch { SwipedById = command.SwippedById, SwipedWhoId = command.SwippedWhoId });
         return isMatch;
     }
 }

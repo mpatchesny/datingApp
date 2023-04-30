1f3402447aa408ca87a56f643182063bbb5f987b
M. Patchesny <strielok11+github@gmail.com>
2023-04-30 22:06:51 +0200

    add

---
+++ src/datingApp.Application/Services/Email.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Application.Services;
+
+public class Email
+{
+    public string Receiver { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}

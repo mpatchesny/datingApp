2b7ab7fbf777fb9766a6c3d3df9834a53eb7737b
M. Patchesny <strielok11+github@gmail.com>
2023-05-02 20:16:21 +0200

    add

---
+++ src/datingApp.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datingApp.Api.Controllers;
+
+[Route("[controller]")]
+public abstract class ApiControllerBase : ControllerBase
+{
+    protected Guid AuthenticatedUserId => 
+        !string.IsNullOrWhiteSpace(User.Identity?.Name) ? 
+            Guid.Parse(User.Identity.Name) :
+            Guid.Empty;
+
+    protected dynamic Authenticate(dynamic query)
+    {
+        query.AuthenticatedUserId = this.AuthenticatedUserId;
+        return query;
+    }
+}

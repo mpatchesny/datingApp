d95a6496baac8189fb7318b1b1e93464e514e7e8
M. Patchesny <strielok11+github@gmail.com>
2023-05-02 17:59:00 +0200

    add

---
+++ src/datingApp.Application/Abstractions/IAuthenticatedCommand.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Application.Abstractions;
+
+public interface IAuthenticatedCommand
+{
+    public Guid AuthenticatedUserId { get; set; }
+}

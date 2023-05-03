b91a351c7afbc377a91806fd956d3c4a44e64fea
M. Patchesny <strielok11+github@gmail.com>
2023-05-03 11:55:33 +0200

    add reference to app.Exceptions

--- src/datingApp.Infrastructure/Exceptions/ExceptionMiddleware.cs
+++ src/datingApp.Infrastructure/Exceptions/ExceptionMiddleware.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using datingApp.Application.Exceptions;
 using datingApp.Core.Exceptions;
 
 namespace datingApp.Infrastructure.Exceptions;

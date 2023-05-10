667c0aef71b46b5276fda2919eb1d0d7717c90f3
M. Patchesny <strielok11+github@gmail.com>
2023-05-10 19:27:17 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Handlers/GetMatchHandler.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.DTO;
+using datingApp.Application.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace datingApp.Infrastructure.DAL.Handlers;
+
+internal sealed class GetMatchHandler
+{
+    private readonly DatingAppDbContext _dbContext;
+    public GetMatchHandler(DatingAppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<MatchDto> HandleAsync(GetMatch query)
+    {
+        var match = await _dbContext.Matches
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(x => x.Id == query.MatchId);
+        
+        return new MatchDto
+            {
+                Id = match.Id,
+                UserId = ((match.UserId1 == query.UserId) ? match.UserId2 : match.UserId1),
+                IsDisplayed = ((match.UserId1 == query.UserId) ? match.IsDisplayedByUser1 : match.IsDisplayedByUser2),
+                // FIXME: magic string
+                Messages = _dbContext.Messages.Where(m => m.MatchId == match.Id).OrderByDescending(m => m.CreatedAt).Take(10).Select(x => x.AsDto()),
+                CreatedAt = match.CreatedAt
+            }; 
+    }
+}

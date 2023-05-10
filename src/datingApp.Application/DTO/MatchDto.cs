eb745e9d34ea3b30a5d3ce005f9b314fce3611e7
M. Patchesny <strielok11+github@gmail.com>
2023-05-10 19:55:58 +0200

    change MatchDto

--- src/datingApp.Application/DTO/MatchDto.cs
+++ src/datingApp.Application/DTO/MatchDto.cs
@@ -8,10 +8,8 @@ namespace datingApp.Application.DTO;
 public class MatchDto
 {
     public Guid Id { get; set; }
-    public Guid UserId { get; set; }
-    public string Name { get; set; }
+    public PublicUserDto User { get; set; }
     public bool IsDisplayed { get; set; }
-    public PhotoDto ProfilePicture { get; set; }
     public IEnumerable<MessageDto> Messages { get; set; }
     public DateTime CreatedAt { get; set; }
 }

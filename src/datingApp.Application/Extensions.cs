79d9fcab021ff234ca6fc5fadd4cfe31885dbab8
M. Patchesny <strielok11+github@gmail.com>
2023-05-10 20:08:07 +0200

    add PaginatedDefaultsOptions to DI

--- src/datingApp.Application/Extensions.cs
+++ src/datingApp.Application/Extensions.cs
@@ -6,6 +6,7 @@ using datingApp.Application.Abstractions;
 using datingApp.Application.Commands;
 using datingApp.Application.Commands.Handlers;
 using datingApp.Application.PhotoManagement;
+using datingApp.Application.Queries;
 using datingApp.Application.Security;
 using datingApp.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,10 +16,12 @@ namespace datingApp.Application
     public static class Extensions
     {
         private const string EmailGeneratorOptionsName = "AccessCodeEmail";
+        private const string PaginatedDefaultsOptionsName = "PaginatedDefaults";
         public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
         {
             var applicationAssembly = typeof(ICommandHandler<>).Assembly;
             services.Configure<EmailGeneratorOptions>(configuration.GetRequiredSection(EmailGeneratorOptionsName));
+            services.Configure<PaginatedDefaultsOptions>(configuration.GetRequiredSection(PaginatedDefaultsOptionsName));
             services.AddSingleton<IPhotoOrderer, PhotoOrderer>();
             services.AddScoped<ICommandHandler<SignUp>, SignUpHandler>();
             services.AddScoped<ICommandHandler<ChangeUser>, ChangeUserHandler>();

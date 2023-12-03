46f2967f954fc5c41d41e43c60748a7e89ac30d2
M. Patchesny <strielok11+github@gmail.com>
2023-12-03 20:14:50 +0100

    add

---
+++ src/datingApp.Infrastructure/DAL/Configurations/AccessCodeConfiguration.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.DTO;
+using datingApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace datingApp.Infrastructure.DAL.Configurations;
+
+internal sealed class AccessCodeConfiguration : IEntityTypeConfiguration<AccessCodeDto>
+{
+    public void Configure(EntityTypeBuilder<AccessCodeDto> builder)
+    {
+        builder.Property(x => x.EmailOrPhone)
+            .IsRequired();
+        builder.Property(x => x.AccessCode)
+            .IsRequired();
+        builder.Property(x => x.Expiry)
+            .IsRequired();
+        builder.Property(x => x.ExpirationTime)
+            .IsRequired();
+        builder.HasIndex(x => new {x.EmailOrPhone});
+    }
+}

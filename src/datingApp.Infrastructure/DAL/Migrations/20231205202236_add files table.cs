f8a3c644e4bd8e96f79e587c5df9131dba060671
M. Patchesny <strielok11+github@gmail.com>
2023-12-05 21:22:52 +0100

    add migration

---
+++ src/datingApp.Infrastructure/DAL/Migrations/20231205202236_add files table.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace datingApp.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfilestable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Extension = table.Column<string>(type: "text", nullable: false),
+                    Binary = table.Column<byte[]>(type: "bytea", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_Id",
+                table: "Files",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+        }
+    }
+}

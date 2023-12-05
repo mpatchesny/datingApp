eea2cdb6200a05422954e72e89736775e11de671
M. Patchesny <strielok11+github@gmail.com>
2023-12-05 22:26:55 +0100

    add delete all user photos from storage

--- src/datingApp.Application/Commands/Handlers/DeleteUserHandler.cs
+++ src/datingApp.Application/Commands/Handlers/DeleteUserHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
 using datingApp.Application.Exceptions;
+using datingApp.Application.Services;
 using datingApp.Core.Repositories;
 
 namespace datingApp.Application.Commands.Handlers;
@@ -11,9 +12,11 @@ namespace datingApp.Application.Commands.Handlers;
 public sealed class DeleteUserHandler : ICommandHandler<DeleteUser>
 {
     private readonly IUserRepository _userRepository;
-    public DeleteUserHandler(IUserRepository userRepository)
+    private readonly IFileStorage _fileStorage;
+    public DeleteUserHandler(IUserRepository userRepository, IFileStorage fileStorage)
     {
         _userRepository = userRepository;
+        _fileStorage = fileStorage;
     }
 
     public async Task HandleAsync(DeleteUser command)
@@ -22,6 +25,10 @@ public sealed class DeleteUserHandler : ICommandHandler<DeleteUser>
         if (user == null)
         {
             throw new UserNotExistsException(command.UserId);
+        }
+        foreach (var photo in user.Photos)
+        {
+            await _fileStorage.DeleteFileAsync(photo.Id.ToString());
         }
         await _userRepository.DeleteAsync(user);
     }

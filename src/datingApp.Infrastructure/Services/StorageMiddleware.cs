6b91eeb2cc097a02ee85226a8bd683be63a9a37a
M. Patchesny <strielok11+github@gmail.com>
2023-12-05 21:17:28 +0100

    add logic

--- src/datingApp.Infrastructure/Services/StorageMiddleware.cs
+++ src/datingApp.Infrastructure/Services/StorageMiddleware.cs
@@ -5,14 +5,41 @@ using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using datingApp.Application.Exceptions;
+using datingApp.Application.Services;
 using datingApp.Core.Exceptions;
 
 namespace datingApp.Infrastructure.Exceptions;
 
 internal sealed class StorageMiddleware : IMiddleware
 {
+    private readonly ILogger<IMiddleware> _logger;
+    public StorageMiddleware(ILogger<IMiddleware> logger)
+    {
+        _logger = logger;
+    }
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
+        if (context.Request.Path.ToString().StartsWith("/storage"))
+        {
+            string id = "";
+            string ext = "";
+            try
+            {
+                var s = context.Request.Path.ToString().Replace("/storage/", "").Split(".");
+                id = s[0];
+                ext = s[1];
+            }
+            catch (System.Exception)
+            {
+                // pass
+            }
+            if (!string.IsNullOrEmpty(id))
+            {
+                // pobieranie danych z bazy i zapisywanie na dysku
+                // storageService.GetFile(id);
+                _logger.LogInformation($"Storage access: {id}, {ext}");
+            }
+        }
         await next(context);
     }
 }

cfce5aea840d4d0cab536676904748200c43415f
M. Patchesny <strielok11+github@gmail.com>
2023-12-06 17:59:53 +0100

    remove unecessary parenthesis

--- src/datingApp.Core/Entities/Extensions.cs
+++ src/datingApp.Core/Entities/Extensions.cs
@@ -9,26 +9,26 @@ public static class Extensions
 {
     public static bool IsOwner(this User entity, Guid userId)
     {
-        return (entity.Id == userId);
+        return entity.Id == userId;
     }
     public static bool IsOwner(this UserSettings entity, Guid userId)
     {
-        return (entity.UserId == userId);
+        return entity.UserId == userId;
     }
     public static bool IsOwner(this Match entity, Guid userId)
     {
-        return (entity.UserId1 == userId || entity.UserId2 == userId);
+        return entity.UserId1 == userId || entity.UserId2 == userId;
     }
     public static bool IsOwner(this Message entity, Guid userId)
     {
-        return (entity.SendFromId == userId);
+        return entity.SendFromId == userId;
     }
     public static bool IsOwner(this Photo entity, Guid userId)
     {
-        return (entity.UserId == userId);
+        return entity.UserId == userId;
     }
     public static bool IsOwner(this Swipe entity, Guid userId)
     {
-        return (entity.SwipedById == userId);
+        return entity.SwipedById == userId;
     }
 }

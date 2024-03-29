6ae5e1753974ca55a7c6e20e1682a34845f2b144
M. Patchesny <strielok11+github@gmail.com>
2024-03-29 09:34:29 +0100

    add optionsProvider

--- tests/datingApp.Tests.Integration/ControllerTestBase.cs
+++ tests/datingApp.Tests.Integration/ControllerTestBase.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace datingApp.Tests.Integration
 {
-    public abstract class ControllerTestBase
+    public abstract class ControllerTestBase : IClassFixture<OptionsProvider>
     {
         protected HttpClient Client { get; }
 
-        public ControllerTestBase()
+        public ControllerTestBase(OptionsProvider optionsProvider)
         {
             var app = new DatingAppTestApp();
             Client = app.Client;

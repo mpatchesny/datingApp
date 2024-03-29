90c9a6a9c2377f96cd321695ea097b2fafed5b33
M. Patchesny <strielok11+github@gmail.com>
2024-03-29 11:32:09 +0100

    add test, change tests names, add xUnit collection

--- tests/datingApp.Tests.Integration/Controllers/UsersControllerTests.cs
+++ tests/datingApp.Tests.Integration/Controllers/UsersControllerTests.cs
@@ -5,14 +5,16 @@ using System.Net;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using datingApp.Application.Commands;
+using datingApp.Core.Entities;
 using Xunit;
 
 namespace datingApp.Tests.Integration.Controllers;
 
+[Collection("Integration tests")]
 public class UsersControllerTests : ControllerTestBase, IDisposable
 {
     [Fact]
-    public async Task given_valid_sign_up_request_post_should_return_201_created()
+    public async Task given_valid_sign_up_post_request_should_return_201_created()
     {
         var command = new SignUp(Guid.Empty, "123456789", "test@test.com", "Janusz", "2000-01-01", 1, 1);
         var response = await Client.PostAsJsonAsync("users", command);
@@ -21,7 +23,19 @@ public class UsersControllerTests : ControllerTestBase, IDisposable
     }
 
     [Fact]
-    public async Task get_public_user_endpoint_when_not_logged_in_should_return_401_unauthorized()
+    public async Task given_email_already_exists_sign_up_post_request_should_return_400_bad_request()
+    {
+        var settings = new UserSettings(Guid.Parse("00000000-0000-0000-0000-000000000002"), Sex.Female, 18, 20, 50, 45.5, 45.5);
+        var user = new User(Guid.NewGuid(), "123456789", "test@test.com", "Janusz", new DateOnly(2000,1,1), Sex.Male, null, settings);
+        _testDb.DbContext.Users.Add(user);
+        await _testDb.DbContext.SaveChangesAsync();
+        var command = new SignUp(Guid.Empty, "123456789", "test@test.com", "Janusz", "2000-01-01", 1, 1);
+        var response = await Client.PostAsJsonAsync("users", command);
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task get_public_user_when_not_logged_in_should_return_401_unauthorized()
     {
         var guid = new Guid("00000000-0000-0000-0000-000000000001");
         var response = await Client.GetAsync($"users/{guid}");

a4be46b925d524c73d52b588b1922247e9a97bfb
M. Patchesny <strielok11+github@gmail.com>
2024-04-01 11:04:30 +0200

    add authenticator, Authorize method

--- tests/datingApp.Tests.Integration/ControllerTestBase.cs
+++ tests/datingApp.Tests.Integration/ControllerTestBase.cs
@@ -1,19 +1,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using datingApp.Application.DTO;
+using datingApp.Application.Security;
+using datingApp.Infrastructure.Exceptions;
+using datingApp.Infrastructure.Security;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace datingApp.Tests.Integration
 {
     public abstract class ControllerTestBase : IClassFixture<OptionsProvider>
     {
+        private readonly IAuthenticator _authenticator;
         protected HttpClient Client { get; }
+
+        protected JwtDto Authorize(Guid userId)
+        {
+            var token = _authenticator.CreateToken(userId);
+            return token;
+        }
 
         public ControllerTestBase(OptionsProvider optionsProvider)
         {
-            var app = new DatingAppTestApp();
+            var authOptions = optionsProvider.Get<AuthOptions>("auth");
+            _authenticator = new Authenticator(new OptionsWrapper<AuthOptions>(authOptions));
+
+            var app = new DatingAppTestApp(ConfigureServices);
             Client = app.Client;
+        }
+
+        protected virtual void ConfigureServices(IServiceCollection services)
+        {
         }
     }
 }

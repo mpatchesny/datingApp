7d8185752f8865c467a0bf93ce5f192f3d4839e2
M. Patchesny <strielok11+github@gmail.com>
2024-04-01 11:04:01 +0200

    add configusersrvies is servies is not null

--- tests/datingApp.Tests.Integration/DatingAppTestApp.cs
+++ tests/datingApp.Tests.Integration/DatingAppTestApp.cs
@@ -5,16 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace datingApp.Tests.Integration;
 
 internal sealed class DatingAppTestApp : WebApplicationFactory<Program>
 {
     public HttpClient Client { get; }
-    public DatingAppTestApp()
+    public DatingAppTestApp(Action<IServiceCollection> services = null)
     {
         Client = base.WithWebHostBuilder(builder => 
         {
+            if (services is not null)
+            {
+                builder.ConfigureServices(services);
+            }
             builder.UseEnvironment("test");
         }).CreateClient();
     }

2552c32929b18256b2784ee9bd780858c15d12a5
M. Patchesny <strielok11+github@gmail.com>
2024-04-02 22:43:17 +0200

    add case for PhotoNotExistsException

--- src/datingApp.Infrastructure/Exceptions/ExceptionMiddleware.cs
+++ src/datingApp.Infrastructure/Exceptions/ExceptionMiddleware.cs
@@ -28,6 +28,7 @@ internal sealed class ExceptionMiddleware : IMiddleware
         var (statusCode, error) = exception switch 
         {
             UnauthorizedException => (StatusCodes.Status403Forbidden, new Error(GetPrettyExeptionName(exception.GetType().Name), exception.Message)),
+            PhotoNotExistsException => (StatusCodes.Status404NotFound, new Error(GetPrettyExeptionName(exception.GetType().Name), exception.Message)),
             CustomException => (StatusCodes.Status400BadRequest, new Error(GetPrettyExeptionName(exception.GetType().Name), exception.Message)),
             _ => (StatusCodes.Status500InternalServerError, new Error("error", "Something went wrong.")),
         };

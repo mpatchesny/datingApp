90ad6deb99010459ab40f89d523b2e58433a2314
M. Patchesny <strielok11+github@gmail.com>
2024-04-03 23:25:06 +0200

    add tests

--- tests/datingApp.Tests.Integration/Controllers/MatchesControllerTests.cs
+++ tests/datingApp.Tests.Integration/Controllers/MatchesControllerTests.cs
@@ -27,6 +27,19 @@ public class MatchesControllerTests : ControllerTestBase, IDisposable
         var response = await Client.GetFromJsonAsync<MatchDto>($"matches/{match.Id}");
         Assert.Equal(match.Id, response.Id);
     }
+
+    [Fact]
+    public async void given_match_not_exists_get_match_returns_404_not_found()
+    {
+        var user1 = await CreateUserAsync("test1@test.com");
+        var user2 = await CreateUserAsync("test2@test.com");
+
+        var token = Authorize(user1.Id);
+        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
+
+        var response = await Client.GetAsync($"matches/{Guid.NewGuid()}");
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
     
     [Fact]
     public async void get_matches_returns_200_ok_and_list_of_match_dto_in_paginated_data_dto()
@@ -42,6 +55,152 @@ public class MatchesControllerTests : ControllerTestBase, IDisposable
         Assert.Single(response.Data);
         var matchDto = JsonConvert.DeserializeObject<MatchDto>(response.Data[0].ToString());
         Assert.Equal(match.Id, matchDto.Id);
+    }
+
+    [Fact]
+    public async void get_matches_respects_provided_page_size()
+    {
+        var user1 = await CreateUserAsync($"test@test.com");
+        for (int i=0; i<20; i++)
+        {
+            var user2 = await CreateUserAsync($"test{i}@test.com");
+            await CreateMatchAsync(user1, user2);
+        }
+
+        var token = Authorize(user1.Id);
+        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
+
+        var response = await Client.GetFromJsonAsync<PaginatedDataDto>($"matches?pageSize={5}");
+        Assert.Equal(5, response.Data.Count);
+    }
+
+    [Fact]
+    public async void get_matches_respects_provided_page()
+    {
+        var user1 = await CreateUserAsync($"test@test.com");
+        var allMatchesGuid = new List<Guid>();
+        for (int i=0; i<45; i++)
+        {
+            var user2 = await CreateUserAsync($"test{i}@test.com");
+            var match = await CreateMatchAsync(user1, user2);
+            allMatchesGuid.Add(match.Id);
+        }
+
+        var token = Authorize(user1.Id);
+        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
+
+        var allMatchesDtoIds = new List<Guid>();
+        for (int i=1; i<=3; i++)
+        {
+            var response = await Client.GetFromJsonAsync<PaginatedDataDto>($"matches?page={i}");
+            foreach (var item in response.Data)
+            {
+                var matchDto = JsonConvert.DeserializeObject<MatchDto>(item.ToString());
+                allMatchesDtoIds.Add(matchDto.Id);
+            }
+        }
+
+        foreach (var matchId in allMatchesGuid)
+        {
+            Assert.Contains(matchId, allMatchesDtoIds);
+        }
+    }
+
+    [Fact]
+    public async void get_message_returns_200_ok_and_message_dto()
+    {
+        var user1 = await CreateUserAsync("test1@test.com");
+        var user2 = await CreateUserAsync("test2@test.com");
+        var match = await CreateMatchAsync(user1, user2);
+        var message = await CreateMessageAsync(match);
+
+        var token = Authorize(user1.Id);
+        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
+
+        var response = await Client.GetFromJsonAsync<MessageDto>($"matches/{match.Id}/messages/{message.Id}");
+        Assert.Equal(message.Id, response.Id);
+    }
+
+    [Fact]
+    public async void given_message_not_exists_get_message_returns_404_not_found()
+    {
+        var user1 = await CreateUserAsync("test1@test.com");
+        var user2 = await CreateUserAsync("test2@test.com");
+        var match = await CreateMatchAsync(user1, user2);
+
+        var token = Authorize(user1.Id);
+        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
+
+        var response = await Client.GetAsync($"matches/{match.Id}/messages/{Guid.NewGuid()}");
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async void get_messages_returns_200_ok_and_list_of_message_dto_in_paginated_data_dto()
+    {
+        var user1 = await CreateUserAsync("test1@test.com");
+        var user2 = await CreateUserAsync("test2@test.com");
+        var match = await CreateMatchAsync(user1, user2);
+        var message = await CreateMessageAsync(match);
+
+        var token = Authorize(user1.Id);
+        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
+
+        var response = await Client.GetFromJsonAsync<PaginatedDataDto>($"matches/{match.Id}/messages");
+        Assert.Single(response.Data);
+        var messageDto = JsonConvert.DeserializeObject<MessageDto>(response.Data[0].ToString());
+        Assert.Equal(message.Id, messageDto.Id);
+    }
+
+    [Fact]
+    public async void get_messages_respects_provided_page_size()
+    {
+        var user1 = await CreateUserAsync("test1@test.com");
+        var user2 = await CreateUserAsync("test2@test.com");
+        var match = await CreateMatchAsync(user1, user2);
+        for (int i=0; i<15; i++)
+        {
+            await CreateMessageAsync(match);
+        }
+
+        var token = Authorize(user1.Id);
+        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
+
+        var response = await Client.GetFromJsonAsync<PaginatedDataDto>($"matches/{match.Id}/messages?pageSize=5");
+        Assert.Equal(5, response.Data.Count);
+    }
+
+    [Fact]
+    public async void get_messages_respects_provided_page()
+    {
+        var user1 = await CreateUserAsync("test1@test.com");
+        var user2 = await CreateUserAsync("test2@test.com");
+        var match = await CreateMatchAsync(user1, user2);
+        var allMessagesGuid = new List<Guid>();
+        for (int i=0; i<45; i++)
+        {
+            var message = await CreateMessageAsync(match);
+            allMessagesGuid.Add(message.Id);
+        }
+
+        var token = Authorize(user1.Id);
+        Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token.AccessToken}");
+
+        var allMessagesDtoIds = new List<Guid>();
+        for (int i=1; i<=3; i++)
+        {
+            var response = await Client.GetFromJsonAsync<PaginatedDataDto>($"matches/{match.Id}/messages?page={i}");
+            foreach (var item in response.Data)
+            {
+                var messageDto = JsonConvert.DeserializeObject<MessageDto>(item.ToString());
+                allMessagesDtoIds.Add(messageDto.Id);
+            }
+        }
+
+        foreach (var messageId in allMessagesDtoIds)
+        {
+            Assert.Contains(messageId, allMessagesGuid);
+        }
     }
 
     private async Task<User> CreateUserAsync(string email, string phone = null)

d5b49102eefaa49d31cdb790c23265e2d35f0484
M. Patchesny <strielok11+github@gmail.com>
2024-04-12 13:52:21 +0200

    add reference to App.Repositories

--- tests/datingApp.Tests.Integration/Repositories/FileRepositoryTests.cs
+++ tests/datingApp.Tests.Integration/Repositories/FileRepositoryTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using datingApp.Application.Repositories;
 using datingApp.Application.Services;
 using datingApp.Infrastructure.DAL.Repositories;
 using datingApp.Infrastructure.Services;

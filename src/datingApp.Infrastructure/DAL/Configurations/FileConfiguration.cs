45692c06e14f246e3d9451ec7882d4de8a470caf
M. Patchesny <strielok11+github@gmail.com>
2024-04-12 22:09:08 +0200

    Add reference to Photos table from Files table

--- src/datingApp.Infrastructure/DAL/Configurations/FileConfiguration.cs
+++ src/datingApp.Infrastructure/DAL/Configurations/FileConfiguration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.DTO;
+using datingApp.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -20,5 +21,8 @@ internal sealed class FileConfiguration : IEntityTypeConfiguration<FileDto>
             .IsRequired();
         builder.HasKey(x => x.Id);
         builder.HasIndex(x => new {x.Id});
+        builder.HasOne<Photo>()
+               .WithOne()
+               .HasForeignKey<FileDto>(x => x.Id);
     }
 }
---
+++ src/datingApp.Infrastructure/DAL/Migrations/20240412200713_Add reference to Photo in FileDto.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace datingApp.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddreferencetoPhotoinFileDto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Files_Id",
+                table: "Photos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Files_Photos_Id",
+                table: "Files",
+                column: "Id",
+                principalTable: "Photos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Files_Photos_Id",
+                table: "Files");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Files_Id",
+                table: "Photos",
+                column: "Id",
+                principalTable: "Files",
+                principalColumn: "Id");
+        }
+    }
+}

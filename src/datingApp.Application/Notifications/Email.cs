d34da3d1205cb0a5a4d7bddab928a9c8d22ff77e
M. Patchesny <strielok11+github@gmail.com>
2024-04-15 22:43:26 +0200

    make as record

--- src/datingApp.Application/Notifications/Email.cs
+++ src/datingApp.Application/Notifications/Email.cs
@@ -5,9 +5,4 @@ using System.Threading.Tasks;
 
 namespace datingApp.Application.Notifications;
 
-public class Email : INotificationMessage
-{
-    public string Receiver { get; set; }
-    public string Subject { get; set; }
-    public string Body { get; set; }
-}
+public sealed record Email(string Receiver, string Subject, string Body) : INotificationMessage;

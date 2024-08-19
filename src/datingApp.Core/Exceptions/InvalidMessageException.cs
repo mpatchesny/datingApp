ef3093c22105c5f3d0ca640297a8fc5167c115cc
M. Patchesny <strielok11+github@gmail.com>
2024-08-19 23:17:22 +0200

    derived from CustomException

--- src/datingApp.Core/Exceptions/InvalidMessageException.cs
+++ src/datingApp.Core/Exceptions/InvalidMessageException.cs
@@ -1,8 +1,9 @@
 using System.Runtime.Serialization;
+using datingApp.Core.Exceptions;
 
 namespace datingApp.Core.Entities;
 
-public sealed class InvalidMessageException : Exception
+public sealed class InvalidMessageException : CustomException
 {
     public InvalidMessageException(string details) : base($"Message is invalid: {details}.")
     {

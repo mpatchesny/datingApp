68549092de6adc9f46ceabb0d5729370ed21afa1
M. Patchesny <strielok11+github@gmail.com>
2024-08-20 20:24:12 +0200

    add tests

--- tests/datingApp.Tests.Unit/Handlers/RequestEmailAccessCodeHandlerTests.cs
+++ tests/datingApp.Tests.Unit/Handlers/RequestEmailAccessCodeHandlerTests.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using datingApp.Application.Commands;
 using datingApp.Application.Commands.Handlers;
 using datingApp.Application.DTO;
+using datingApp.Application.Exceptions;
 using datingApp.Application.Notifications;
 using datingApp.Application.Security;
 using datingApp.Application.Services;
@@ -77,6 +78,38 @@ public class RequestEmailAccessCodeHandlerTests
         await handler.HandleAsync(command);
 
         codeStorage.Verify(mock => mock.Set(code), Times.Once());
+    }
+
+    [Fact]
+    public async Task given_provided_email_is_null_no_email_provided_exception_is_raised()
+    {
+        var code = new AccessCodeDto() {
+            AccessCode ="12345",
+            EmailOrPhone = "test@test.com",
+            ExpirationTime = DateTime.UtcNow,
+            Expiry = TimeSpan.FromMinutes(15)
+        };
+
+        var codeStorage = new Mock<IAccessCodeStorage>();
+        codeStorage.Setup(m => m.Get(It.IsAny<string>())).Returns(code);
+        codeStorage.Setup(m => m.Set(code));
+
+        var codeGenerator = new Mock<IAccessCodeGenerator>();
+        codeGenerator.Setup(m => m.GenerateCode(It.IsAny<string>())).Returns(code);
+
+        var emailSender = new Mock<INotificationSender<Email>>();
+        emailSender.Setup(m => m.SendAsync(It.IsAny<Email>()));
+
+        var emailMsg = new Email("receiver", "subject", "body");
+        var emailGenerator = new Mock<INotificationMessageGenerator<Email>>();
+        emailGenerator.Setup(m => m.Generate(It.IsAny<string>(), It.IsAny<Dictionary<string,string>>())).Returns(emailMsg);
+
+        string email = null;
+        var command = new RequestEmailAccessCode(email);
+        var handler = new RequestEmailAccessCodeHandler(codeGenerator.Object, codeStorage.Object, emailSender.Object, emailGenerator.Object);
+        var exception = await Record.ExceptionAsync(async () => await handler.HandleAsync(command));
+        Assert.NotNull(exception);
+        Assert.IsType<NoEmailProvidedException>(exception);
     }
 
     [Fact]

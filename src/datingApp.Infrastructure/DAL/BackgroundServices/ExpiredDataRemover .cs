c530076855b128e9daf3623e2e9c804bbabfbc74
M. Patchesny <strielok11+github@gmail.com>
2024-08-22 22:35:22 +0200

    add remove expired, revoked tokens

--- src/datingApp.Infrastructure/DAL/BackgroundServices/ExpiredDataRemover .cs
+++ src/datingApp.Infrastructure/DAL/BackgroundServices/ExpiredDataRemover .cs
@@ -31,6 +31,7 @@ internal sealed class ExpiredDataRemover : BackgroundService
         while (!stoppingToken.IsCancellationRequested)
         {
             dbContext.AccessCodes.RemoveRange(dbContext.AccessCodes.Where(x => x.ExpirationTime < DateTime.UtcNow));
+            dbContext.RevokedTokens.RemoveRange(dbContext.RevokedTokens.Where(x => x.ExpirationTime < DateTime.UtcNow));
             _ = await dbContext.SaveChangesAsync(stoppingToken);
             await Task.Delay(loopDelayInMilliseconds, stoppingToken);
         }

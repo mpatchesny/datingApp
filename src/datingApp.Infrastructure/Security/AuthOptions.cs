9b1f4721c3bab7395fb111fb39f7d955451bf598
M. Patchesny <strielok11+github@gmail.com>
2024-08-23 22:41:13 +0200

    remove nullable

--- src/datingApp.Infrastructure/Security/AuthOptions.cs
+++ src/datingApp.Infrastructure/Security/AuthOptions.cs
@@ -9,7 +9,7 @@ public sealed class Token
     public string Issuer { get; set; }
     public string Audience { get; set; }
     public string SigningKey { get; set; }
-    public TimeSpan? Expiry { get; set; }
+    public TimeSpan Expiry { get; set; }
 }
 
 public sealed class AuthOptions

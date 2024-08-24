c6819164fb903d811167558f84feeb0d2bee757a
M. Patchesny <strielok11+github@gmail.com>
2024-08-24 09:44:02 +0200

    refactor

--- src/datingApp.Infrastructure/Security/Authenticator.cs
+++ src/datingApp.Infrastructure/Security/Authenticator.cs
@@ -21,58 +21,63 @@ internal sealed class Authenticator : IAuthenticator
         SigningCredentials Credentials
     );
 
-    private readonly List<TokenOptions> _tokenOptionsList;
+    private TokenOptions _accessTokenOptions;
+    private TokenOptions _refreshTokenOptions;
     private readonly JwtSecurityTokenHandler _jwtSecurityToken = new JwtSecurityTokenHandler();
 
     public Authenticator(IOptions<AuthOptions> options)
     {
-        _tokenOptionsList = new List<TokenOptions>();
-        foreach (var tokenOption in new [] { options.Value.AccessToken, options.Value.RefreshToken })
-        {
-            var opts = new TokenOptions(
-                tokenOption.Issuer,
-                tokenOption.Expiry,
-                tokenOption.Audience,
-                new SigningCredentials(
-                    new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(tokenOption.SigningKey)
-                    ),
-                    SecurityAlgorithms.HmacSha256
-                    )
-                );
-            _tokenOptionsList.Add(opts);
-        }
+        _accessTokenOptions = CrateTokenOptions(options.Value.AccessToken);
+        _refreshTokenOptions = CrateTokenOptions(options.Value.RefreshToken);
     }
 
     public JwtDto CreateToken(Guid userId)
     {
-        var tokens = new List<Tuple<string, DateTime>>();
-        foreach (var tokenOption in _tokenOptionsList)
-        {
-            var now = DateTime.UtcNow;
-            var claims = new List<Claim>
-            {
-                new(JwtRegisteredClaimNames.Sub, userId.ToString()),
-                new(JwtRegisteredClaimNames.UniqueName, userId.ToString()),
-            };
-
-            var expires = now.Add(tokenOption.Expirty);
-            var jwt = new JwtSecurityToken(
-                tokenOption.Issuer,
-                tokenOption.Audience,
-                claims,
-                now,
-                expires,
-                tokenOption.Credentials
-            );
-            var token = _jwtSecurityToken.WriteToken(jwt);
-            tokens.Add(new Tuple<string, DateTime> (token, expires));
-        }
+        var accessToken = CreateToken(userId, _accessTokenOptions);
+        var refreshToken = CreateToken(userId, _refreshTokenOptions);
 
         return new JwtDto
         {
-            AccessToken = new TokenDto(tokens[0].Item1, tokens[0].Item2),
-            RefreshToken = new TokenDto(tokens[1].Item1, tokens[1].Item2)
+            AccessToken = accessToken,
+            RefreshToken = refreshToken
         };
+    }
+
+    private static TokenOptions CrateTokenOptions(AuthOptions.Token tokenOptions)
+    {
+        return new TokenOptions(
+            tokenOptions.Issuer,
+            tokenOptions.Expiry,
+            tokenOptions.Audience,
+            new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(tokenOptions.SigningKey)
+                    ),
+                SecurityAlgorithms.HmacSha256
+                )
+            );
+    }
+
+    private TokenDto CreateToken(Guid userId, TokenOptions options)
+    {
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new(JwtRegisteredClaimNames.UniqueName, userId.ToString()),
+        };
+
+        var now = DateTime.UtcNow;
+        var expires = now.Add(options.Expirty);
+        var jwt = new JwtSecurityToken(
+            options.Issuer,
+            options.Audience,
+            claims,
+            now,
+            expires,
+            options.Credentials
+        );
+
+        var token = _jwtSecurityToken.WriteToken(jwt);
+        return new TokenDto(token, expires);
     }
 }

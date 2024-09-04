9119c6e7e14d33c45aa7ca9eb2adebf85d14e6ba
M. Patchesny <strielok11+github@gmail.com>
2024-09-04 21:56:30 +0200

    add _revokedRefreshTokensRepository property

--- src/datingApp.Application/Commands/Handlers/RefreshTokenHandler.cs
+++ src/datingApp.Application/Commands/Handlers/RefreshTokenHandler.cs
@@ -11,15 +11,20 @@ public sealed class RefreshTokenHandler : ICommandHandler<RefreshToken>
 {
     private readonly IAuthenticator _authenticator;
     private readonly ITokenStorage _tokenStorage;
+    private readonly object _revokedRefreshTokensRepository;
+
     public RefreshTokenHandler(IAuthenticator authenticator,
-                                ITokenStorage tokenStorage)
+                                ITokenStorage tokenStorage,
+                                object revokedRefreshTokensRepository)
     {
         _authenticator = authenticator;
         _tokenStorage = tokenStorage;
+        _revokedRefreshTokensRepository = revokedRefreshTokensRepository;
     }
     public async Task HandleAsync(RefreshToken command)
     {
         var jwt = _authenticator.CreateToken(command.AuthenticatedUserId);
         _tokenStorage.Set(jwt);
+        await _revokedRefreshTokensRepository.DeleteAsync(command.Token);
     }
 }

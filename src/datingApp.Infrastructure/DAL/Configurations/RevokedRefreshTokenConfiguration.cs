b1045d16f6361f46441d3cc48df5b6b9267ab8e7
M. Patchesny <strielok11+github@gmail.com>
2024-09-04 21:56:09 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Configurations/RevokedRefreshTokenConfiguration.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.DTO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace datingApp.Infrastructure.DAL.Configurations;
+
+internal sealed class RevokedRefreshTokenConfiguration : IEntityTypeConfiguration<TokenDto>
+{
+    public void Configure(EntityTypeBuilder<TokenDto> builder)
+    {
+        builder.Property(x => x.Token)
+            .IsRequired();
+        builder.Property(x => x.ExpirationTime)
+            .IsRequired();
+        builder.HasKey(x => x.Token);
+        builder.HasIndex(x => new {x.Token});
+    }
+}

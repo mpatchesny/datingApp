d49b7c252e9ea7da1ae12bc30e9ada58774f78f3
M. Patchesny <strielok11+github@gmail.com>
2024-09-09 23:27:57 +0200

    remove commented out code; change comment

--- src/datingApp.Infrastructure/Security/Authorization/PermissionHandler.cs
+++ src/datingApp.Infrastructure/Security/Authorization/PermissionHandler.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 
 namespace datingApp.Infrastructure.Security.Authorization;
 
+// https://learn.microsoft.com/en-us/aspnet/core/security/authorization/policies?view=aspnetcore-8.0
 internal sealed class PermissionHandler : IAuthorizationHandler
 {
     public Task HandleAsync(AuthorizationHandlerContext context)
     {
+        // FIXME: przejrzeć, spr czy coś jeszcze potrzeba tutaj
+        // żeby działało
         var pendingRequirements = context.PendingRequirements.ToList();
 
         foreach (var requirement in pendingRequirements)
@@ -17,29 +21,20 @@ internal sealed class PermissionHandler : IAuthorizationHandler
             switch (requirement)
             {
                 case IsOwnerRequirement:
+                    if (IsOwner(context.User, context.Resource)) 
+                    {
+                        context.Succeed(requirement);
+                    }
                     break;
                 default:
                     break;
             }
-
-            /*
-            if (requirement is ReadPermission)
-            {
-                if (IsOwner(context.User, context.Resource)
-                    || IsSponsor(context.User, context.Resource))
-                {
-                    context.Succeed(requirement);
-                }
-            }
-            else if (requirement is EditPermission || requirement is DeletePermission)
-            {
-                if (IsOwner(context.User, context.Resource))
-                {
-                    context.Succeed(requirement);
-                }
-            }
-            */
         }
         return Task.CompletedTask;
+    }
+    private static bool IsOwner(ClaimsPrincipal user, object? resource)
+    {
+        // TODO
+        return true;
     }
 }

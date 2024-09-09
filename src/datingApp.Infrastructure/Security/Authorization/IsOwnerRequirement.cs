99905f73507b2c735dfd369cca8f4c22d29403b9
M. Patchesny <strielok11+github@gmail.com>
2024-09-09 23:24:11 +0200

    add sample custom requirement

---
+++ src/datingApp.Infrastructure/Security/Authorization/IsOwnerRequirement.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace datingApp.Infrastructure.Security.Authorization;
+
+internal sealed class IsOwnerRequirement : IAuthorizationRequirement
+{
+    public IsOwnerRequirement()
+    {
+        // TODO
+    }
+}

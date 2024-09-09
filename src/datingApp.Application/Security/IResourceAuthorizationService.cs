7e5b0dff7567c12e121aa55dbb49449526f9b080
M. Patchesny <strielok11+github@gmail.com>
2024-09-09 23:12:46 +0200

    change return type

--- src/datingApp.Application/Security/IResourceAuthorizationService.cs
+++ src/datingApp.Application/Security/IResourceAuthorizationService.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace datingApp.Application.Security;
 
 public interface IResourceAuthorizationService
 {
-    public Task<bool> AuthorizeAsync(Guid userId, object resource);
+    public Task<AuthorizationResult> AuthorizeAsync(Guid userId, object resource, string policyName);
 }

2cf2866da12a6291b7dfb985bc953f2278d26b07
M. Patchesny <strielok11+github@gmail.com>
2024-09-17 22:08:47 +0200

    change folder/namespace

--- src/datingApp.Application/Security/Authorization/IsOwnerRequirement.cs
+++ src/datingApp.Application/Security/Authorization/IsOwnerRequirement.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 
-namespace datingApp.Infrastructure.Security.Authorization;
+namespace datingApp.Application.Security.Authorization;
 
 internal sealed class IsOwnerRequirement : IAuthorizationRequirement
 {

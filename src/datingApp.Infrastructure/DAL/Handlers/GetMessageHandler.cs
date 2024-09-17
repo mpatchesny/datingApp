576b1d8b711d2b26a1f46df989e471b3376bc336
M. Patchesny <strielok11+github@gmail.com>
2024-09-17 22:03:06 +0200

    add IsOwner policy authorization

--- src/datingApp.Infrastructure/DAL/Handlers/GetMessageHandler.cs
+++ src/datingApp.Infrastructure/DAL/Handlers/GetMessageHandler.cs
@@ -6,6 +6,7 @@ using datingApp.Application.Abstractions;
 using datingApp.Application.DTO;
 using datingApp.Application.Exceptions;
 using datingApp.Application.Queries;
+using datingApp.Application.Security;
 using Microsoft.EntityFrameworkCore;
 
 namespace datingApp.Infrastructure.DAL.Handlers;
@@ -13,22 +14,42 @@ namespace datingApp.Infrastructure.DAL.Handlers;
 internal sealed class GetMessageHandler : IQueryHandler<GetMessage, MessageDto>
 {
     private readonly DatingAppDbContext _dbContext;
-    public GetMessageHandler(DatingAppDbContext dbContext)
+    private readonly IDatingAppAuthorizationService _authorizationService;
+
+    public GetMessageHandler(DatingAppDbContext dbContext, IDatingAppAuthorizationService authorizationService)
     {
         _dbContext = dbContext;
+        _authorizationService = authorizationService;
     }
 
     public async Task<MessageDto> HandleAsync(GetMessage query)
     {
-        var message = await _dbContext.Messages
-                                    .AsNoTracking()
-                                    .SingleOrDefaultAsync(x => x.Id == query.MessageId);
+        var dbQuery = 
+            from match in _dbContext.Matches.Include(m => m.Messages)
+            from message in _dbContext.Messages
+            where match.Id == message.MatchId
+            where message.Id == query.MessageId
+            select new
+            {
+                Match = match,
+                Message = message
+            };
 
-        if (message == null) 
+        var data = await dbQuery
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync();
+
+        if (data.Message == null) 
         {
             throw new MessageNotExistsException(query.MessageId);
         }
 
-        return message.AsDto();
+        var authorizationResult = await _authorizationService.AuthorizeAsync(query.AuthenticatedUserId, data.Match, "OwnerPolicy");
+        if (!authorizationResult.Succeeded)
+        {
+            throw new UnauthorizedException();
+        }
+
+        return data.Message.AsDto();
     }
 }

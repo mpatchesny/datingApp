0bcf0591f42bef85b8d93916df00ebfeec19b9b6
M. Patchesny <strielok11+github@gmail.com>
2024-09-19 21:01:31 +0200

    refactor

--- tests/datingApp.Tests.Integration/CommandHandlers/RefreshTokenHandlerTests.cs
+++ tests/datingApp.Tests.Integration/CommandHandlers/RefreshTokenHandlerTests.cs
@@ -23,18 +23,15 @@ public class RefreshTokenHandlerTests : IDisposable
     [Fact]
     public async Task given_refresh_token_exists_in_revoked_repository_ResfreshTokenHandler_should_throw_RefreshTokenRevokedException()
     {
-        var authenticator = new Mock<IAuthenticator>();
-        authenticator.Setup(m => m.CreateToken(It.IsAny<Guid>())).Returns(new JwtDto());
-        var tokenStorage = new Mock<ITokenStorage>();
-        tokenStorage.Setup(m => m.Set(It.IsAny<JwtDto>()));
-        var handler = new RefreshJWTHandler(authenticator.Object, tokenStorage.Object, _revokedRefreshTokensRepository);
+        _authenticator.Setup(m => m.CreateToken(It.IsAny<Guid>())).Returns(new JwtDto());
+        _tokenStorage.Setup(m => m.Set(It.IsAny<JwtDto>()));
 
         TokenDto refreshToken = new TokenDto("abc", DateTime.UtcNow + TimeSpan.FromDays(1));
         await _testDb.DbContext.RevokedRefreshTokens.AddAsync(refreshToken);
         await _testDb.DbContext.SaveChangesAsync();
 
         var command = new RefreshJWT(refreshToken.Token);
-        var exception = await Record.ExceptionAsync(async () => await handler.HandleAsync(command));
+        var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.NotNull(exception);
         Assert.IsType<RefreshTokenRevokedException>(exception);
     }
@@ -42,17 +39,13 @@ public class RefreshTokenHandlerTests : IDisposable
     [Fact]
     public async Task given_refresh_token_validation_failed_ResfreshTokenHandler_should_throw_InvalidRefreshTokenException()
     {
-        var authenticator = new Mock<IAuthenticator>();
-        authenticator.Setup(m => m.CreateToken(It.IsAny<Guid>())).Returns(new JwtDto());
-        authenticator.Setup(m => m.ValidateRefreshToken(It.IsAny<string>())).Returns(new ClaimsPrincipal());
-        var tokenStorage = new Mock<ITokenStorage>();
-        tokenStorage.Setup(m => m.Set(It.IsAny<JwtDto>()));
-        var handler = new RefreshJWTHandler(authenticator.Object, tokenStorage.Object, _revokedRefreshTokensRepository);
+        _authenticator.Setup(m => m.CreateToken(It.IsAny<Guid>())).Returns(new JwtDto());
+        _authenticator.Setup(m => m.ValidateRefreshToken(It.IsAny<string>())).Returns(new ClaimsPrincipal());
+        _tokenStorage.Setup(m => m.Set(It.IsAny<JwtDto>()));
 
         TokenDto refreshToken = new TokenDto("abc", DateTime.UtcNow + TimeSpan.FromDays(1));
-
         var command = new RefreshJWT(refreshToken.Token);
-        var exception = await Record.ExceptionAsync(async () => await handler.HandleAsync(command));
+        var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.NotNull(exception);
         Assert.IsType<InvalidRefreshTokenException>(exception);
     }
@@ -61,45 +54,62 @@ public class RefreshTokenHandlerTests : IDisposable
     public async Task given_refresh_token_succeed_ResfreshTokenHandler_should_add_new_JwtDto_to_token_storage()
     {
         var expirationTime = DateTime.UtcNow + TimeSpan.FromHours(1);
-        var newToken =  new JwtDto
-            {
-                AccessToken = new TokenDto("access token", expirationTime),
-                RefreshToken = new TokenDto("refresh token", expirationTime)
-            };
-        var authenticator = new Mock<IAuthenticator>();
-        authenticator.Setup(m => m.CreateToken(It.IsAny<Guid>())).Returns(newToken);
-        var userId = Guid.NewGuid();
-        var claims = new List<Claim>
-            {
-                new(JwtRegisteredClaimNames.Sub, userId.ToString()),
-                new(JwtRegisteredClaimNames.UniqueName, userId.ToString()),
-            };
-        var identity = new ClaimsIdentity(claims, authenticationType: string.Empty, nameType: JwtRegisteredClaimNames.Sub, roleType: string.Empty);
-        var claimPrincipal = new ClaimsPrincipal(identity);
-        authenticator.Setup(m => m.ValidateRefreshToken(It.IsAny<string>())).Returns(claimPrincipal);
-        
-        var tokenStorage = new Mock<ITokenStorage>();
-        tokenStorage.Setup(m => m.Set(It.IsAny<JwtDto>()));
-        var handler = new RefreshJWTHandler(authenticator.Object, tokenStorage.Object, _revokedRefreshTokensRepository);
+        var newToken = CreateToken(expirationTime);
+        _authenticator.Setup(m => m.CreateToken(It.IsAny<Guid>())).Returns(newToken);
+
+        var claimPrincipal = CreateClaimsPrincipal();
+        _authenticator.Setup(m => m.ValidateRefreshToken(It.IsAny<string>())).Returns(claimPrincipal);
+        _tokenStorage.Setup(m => m.Set(It.IsAny<JwtDto>()));
 
         TokenDto refreshToken = new TokenDto("abc", DateTime.UtcNow + TimeSpan.FromDays(1));
         var command = new RefreshJWT(refreshToken.Token);
-        var exception = await Record.ExceptionAsync(async () => await handler.HandleAsync(command));
+        var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
-        tokenStorage.Verify(x => x.Set(newToken), Times.Once);
+        _tokenStorage.Verify(x => x.Set(newToken), Times.Once);
     }
 
     [Fact]
     public async Task given_refresh_token_succeed_ResfreshTokenHandler_should_add_refresh_token_to_revoked_tokens_repository()
     {
         var expirationTime = DateTime.UtcNow + TimeSpan.FromHours(1);
-        var newToken =  new JwtDto
-            {
-                AccessToken = new TokenDto("access token", expirationTime),
-                RefreshToken = new TokenDto("refresh token", expirationTime)
-            };
-        var authenticator = new Mock<IAuthenticator>();
-        authenticator.Setup(m => m.CreateToken(It.IsAny<Guid>())).Returns(newToken);
+        var newToken = CreateToken(expirationTime);
+        _authenticator.Setup(m => m.CreateToken(It.IsAny<Guid>())).Returns(newToken);
+
+        var claimPrincipal = CreateClaimsPrincipal();
+        _authenticator.Setup(m => m.ValidateRefreshToken(It.IsAny<string>())).Returns(claimPrincipal);
+        _tokenStorage.Setup(m => m.Set(It.IsAny<JwtDto>()));
+
+        var refreshToken = new TokenDto("abc", DateTime.UtcNow + TimeSpan.FromDays(1));
+        var command = new RefreshJWT(refreshToken.Token);
+        var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
+        var tokenExistsInRepository = await _testDb.DbContext.RevokedRefreshTokens.AnyAsync(x => x.Token == refreshToken.Token);
+        Assert.Null(exception);
+        Assert.True(tokenExistsInRepository);
+    }
+
+    // Arrange
+    private readonly TestDatabase _testDb;
+    private readonly RefreshJWTHandler _handler;
+    private readonly Mock<ITokenStorage> _tokenStorage;
+    private readonly Mock<IAuthenticator> _authenticator;
+    private readonly DbRevokedRefreshTokensRepository _revokedRefreshTokensRepository;
+    public RefreshTokenHandlerTests()
+    {
+        _testDb = new TestDatabase();
+        _revokedRefreshTokensRepository = new DbRevokedRefreshTokensRepository(_testDb.DbContext);
+        _tokenStorage = new Mock<ITokenStorage>();
+        _authenticator = new Mock<IAuthenticator>();
+        _handler = new RefreshJWTHandler(_authenticator.Object, _tokenStorage.Object, _revokedRefreshTokensRepository);
+    }
+
+    // Teardown
+    public void Dispose()
+    {
+        _testDb.Dispose();
+    }
+
+    private static ClaimsPrincipal CreateClaimsPrincipal()
+    {
         var userId = Guid.NewGuid();
         var claims = new List<Claim>
             {
@@ -107,33 +117,15 @@ public class RefreshTokenHandlerTests : IDisposable
                 new(JwtRegisteredClaimNames.UniqueName, userId.ToString()),
             };
         var identity = new ClaimsIdentity(claims, authenticationType: string.Empty, nameType: JwtRegisteredClaimNames.Sub, roleType: string.Empty);
-        var claimPrincipal = new ClaimsPrincipal(identity);
-        authenticator.Setup(m => m.ValidateRefreshToken(It.IsAny<string>())).Returns(claimPrincipal);
-        
-        var tokenStorage = new Mock<ITokenStorage>();
-        tokenStorage.Setup(m => m.Set(It.IsAny<JwtDto>()));
-        var handler = new RefreshJWTHandler(authenticator.Object, tokenStorage.Object, _revokedRefreshTokensRepository);
-
-        var refreshToken = new TokenDto("abc", DateTime.UtcNow + TimeSpan.FromDays(1));
-        var command = new RefreshJWT(refreshToken.Token);
-        var exception = await Record.ExceptionAsync(async () => await handler.HandleAsync(command));
-        var tokenExistsInRepository = await _testDb.DbContext.RevokedRefreshTokens.AnyAsync(x => x.Token == refreshToken.Token);
-        Assert.Null(exception);
-        Assert.True(tokenExistsInRepository);
+        return new ClaimsPrincipal(identity);
     }
 
-    // Arrange
-    private readonly DbRevokedRefreshTokensRepository _revokedRefreshTokensRepository;
-    private readonly TestDatabase _testDb;
-    public RefreshTokenHandlerTests()
+    private static JwtDto CreateToken(DateTime expirationTime)
     {
-        _testDb = new TestDatabase();
-        _revokedRefreshTokensRepository = new DbRevokedRefreshTokensRepository(_testDb.DbContext);
-    }
-
-    // Teardown
-    public void Dispose()
-    {
-        _testDb.Dispose();
+        return new JwtDto
+            {
+                AccessToken = new TokenDto("access token", expirationTime),
+                RefreshToken = new TokenDto("refresh token", expirationTime)
+            };
     }
 }

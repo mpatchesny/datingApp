ab3d0db91c506fb1038ebb52fef824ba020f1ee7
M. Patchesny <strielok11+github@gmail.com>
2024-09-19 20:23:58 +0200

    refactor, add authorization mock

--- tests/datingApp.Tests.Integration/CommandHandlers/ChangeUserHandlerTests.cs
+++ tests/datingApp.Tests.Integration/CommandHandlers/ChangeUserHandlerTests.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using datingApp.Application.Commands;
 using datingApp.Application.Commands.Handlers;
 using datingApp.Application.Exceptions;
+using datingApp.Application.Security;
 using datingApp.Core.Entities;
 using datingApp.Infrastructure.DAL.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Moq;
 using Xunit;
 
 namespace datingApp.Tests.Integration.CommandHandlers;
@@ -17,15 +20,33 @@ public class ChangeUserHandlerTests : IDisposable
     [Fact]
     public async Task change_existing_user_should_succeed()
     {
-        var command = new ChangeUser(Guid.Parse("00000000-0000-0000-0000-000000000001"), "1998-01-01");
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<User>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new ChangeUser(user.Id, "1998-01-01");
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
 
     [Fact]
+    public async Task given_authorization_fail_change_existing_user_should_return_UnauthorizedException()
+    {
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<User>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Failed()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new ChangeUser(user.Id, "1998-01-01");
+        var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
+        Assert.NotNull(exception);
+        Assert.IsType<UnauthorizedException>(exception);
+    }
+
+    [Fact]
     public async Task change_nothing_should_succeed()
     {
-        var command = new ChangeUser(Guid.Parse("00000000-0000-0000-0000-000000000001"));
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<User>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new ChangeUser(user.Id);
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
@@ -33,7 +54,10 @@ public class ChangeUserHandlerTests : IDisposable
     [Fact]
     public async Task change_existing_user_without_changing_date_of_birth_should_succeed()
     {
-        var command = new ChangeUser(Guid.Parse("00000000-0000-0000-0000-000000000001"), null, "new bio");
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<User>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new ChangeUser(user.Id, null, "new bio");
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
@@ -41,7 +65,9 @@ public class ChangeUserHandlerTests : IDisposable
     [Fact]
     public async Task change_nonexisting_user_should_throw_exception()
     {
-        var command = new ChangeUser(Guid.Parse("00000000-0000-0000-0000-000000000002"), "1998-01-01");
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<User>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+
+        var command = new ChangeUser(Guid.NewGuid(), "1998-01-01");
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.NotNull(exception);
         Assert.IsType<UserNotExistsException>(exception);
@@ -50,7 +76,10 @@ public class ChangeUserHandlerTests : IDisposable
     [Fact]
     public async Task given_invalid_date_change_user_should_throw_exception()
     {
-        var command = new ChangeUser(Guid.Parse("00000000-0000-0000-0000-000000000001"), "01.01.1998");
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<User>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new ChangeUser(user.Id, "01.01.1998");
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.NotNull(exception);
         Assert.IsType<InvalidDateOfBirthFormatException>(exception);
@@ -59,7 +88,10 @@ public class ChangeUserHandlerTests : IDisposable
     [Fact]
     public async Task change_existing_user_settings_should_succeed()
     {
-        var command = new ChangeUser(Guid.Parse("00000000-0000-0000-0000-000000000001"), DiscoverAgeFrom: 18, DiscoverAgeTo: 20, DiscoverRange: 20, DiscoverSex: 1);
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<User>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new ChangeUser(user.Id, DiscoverAgeFrom: 18, DiscoverAgeTo: 20, DiscoverRange: 20, DiscoverSex: 1);
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
@@ -67,25 +99,24 @@ public class ChangeUserHandlerTests : IDisposable
     [Fact]
     public async Task change_location_of_existing_user_should_succeed()
     {
-        var command = new ChangeUser(Guid.Parse("00000000-0000-0000-0000-000000000001"), Lat: 40.0, Lon: 40.0);
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<User>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new ChangeUser(user.Id, Lat: 40.0, Lon: 40.0);
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
 
     // Arrange
-    private readonly ChangeUserHandler _handler;
     private readonly TestDatabase _testDb;
+    private readonly ChangeUserHandler _handler;
+    private readonly Mock<IDatingAppAuthorizationService> _authService;
     public ChangeUserHandlerTests()
     {
-        var settings = new UserSettings(Guid.Parse("00000000-0000-0000-0000-000000000001"), Sex.Female, 18, 20, 50, 40.5, 40.5);
-        var user = new User(Guid.Parse("00000000-0000-0000-0000-000000000001"), "123456789", "test@test.com", "Janusz", new DateOnly(2000,1,1), Sex.Male, null, settings);
-
         _testDb = new TestDatabase();
-        _testDb.DbContext.Users.Add(user);
-        _testDb.DbContext.SaveChanges();
-
         var userRepository = new DbUserRepository(_testDb.DbContext);
-        _handler = new ChangeUserHandler(userRepository);
+        _authService = new Mock<IDatingAppAuthorizationService>();
+        _handler = new ChangeUserHandler(userRepository, _authService.Object);
     }
 
     // Teardown

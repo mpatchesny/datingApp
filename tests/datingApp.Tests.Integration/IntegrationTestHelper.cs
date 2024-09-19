f6ff4cfb74fc7d91cbe6d152755d7d5533579272
MPatchesny <strielok11+github@gmail.com>
2024-09-19 10:00:51 +0100

    fix random email generation

--- tests/datingApp.Tests.Integration/IntegrationTestHelper.cs
+++ tests/datingApp.Tests.Integration/IntegrationTestHelper.cs
@@ -12,8 +12,8 @@ internal static class IntegrationTestHelper
 {
     public static async Task<User> CreateUserAsync(TestDatabase database, string email = null, string phone = null)
     {
-        if (email == null) email = Guid.NewGuid().ToString().Replace("-", "") + "@test.com";
         Random random = new Random();
+        if (email == null) email = "test_" + random.Next(1, 9).ToString() + "@test.com";
         if (phone == null) phone = random.Next(100000000, 999999999).ToString();
 
         var settings = new UserSettings(Guid.NewGuid(), Sex.MaleAndFemale, 18, 100, 100, 45.5, 45.5);

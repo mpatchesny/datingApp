a903f904ed306a0aa7b1b66e3b30626cf5c3b81c
M. Patchesny <strielok11+github@gmail.com>
2024-09-19 20:16:14 +0200

    add authService mock, add test, refactor

--- tests/datingApp.Tests.Integration/CommandHandlers/ChangePhotoOridinalHandlerTests.cs
+++ tests/datingApp.Tests.Integration/CommandHandlers/ChangePhotoOridinalHandlerTests.cs
@@ -6,8 +6,10 @@ using datingApp.Application.Commands;
 using datingApp.Application.Commands.Handlers;
 using datingApp.Application.Exceptions;
 using datingApp.Application.PhotoManagement;
+using datingApp.Application.Security;
 using datingApp.Core.Entities;
 using datingApp.Infrastructure.DAL.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Moq;
 using Xunit;
 
@@ -18,7 +20,10 @@ public class ChangePhotoOridinalHandlerTests : IDisposable
     [Fact]
     public async Task change_oridinal_of_existing_photo_should_succeed()
     {
-        var command = new ChangePhotoOridinal(Guid.Parse("00000000-0000-0000-0000-000000000001"), 2);
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<Photo>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var photo = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id);
+        var command = new ChangePhotoOridinal(photo.Id, 2);
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
@@ -26,10 +31,11 @@ public class ChangePhotoOridinalHandlerTests : IDisposable
     [Fact]
     public async Task change_oridinal_of_existing_photo_when_other_photo_with_that_oridinal_exists_should_succeed()
     {
-        var photo = new Photo(Guid.Parse("00000000-0000-0000-0000-000000000002"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "abc", "abc", 1);
-        _testDb.DbContext.Photos.Add(photo);
-        _testDb.DbContext.SaveChanges();
-        var command = new ChangePhotoOridinal(Guid.Parse("00000000-0000-0000-0000-000000000001"), 2);
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<Photo>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var photo1 = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id);
+        var photo2 = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id, 2);
+        var command = new ChangePhotoOridinal(photo1.Id, 2);
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
@@ -37,34 +43,38 @@ public class ChangePhotoOridinalHandlerTests : IDisposable
     [Fact]
     public async Task change_oridinal_of_nonexisting_photo_should_throw_exception()
     {
-        var command = new ChangePhotoOridinal(Guid.Parse("00000000-0000-0000-0000-000000000002"), 1);
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<Photo>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Success()));
+        var command = new ChangePhotoOridinal(Guid.NewGuid(), 1);
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.NotNull(exception);
         Assert.IsType<PhotoNotExistsException>(exception);
     }
+
+    [Fact]
+    public async Task given_authorization_fail_change_oridinal_of_existing_photo_should_throw_UnauthorizedException()
+    {
+        _authService.Setup(m => m.AuthorizeAsync(It.IsAny<Guid>(), It.IsAny<Photo>(), "OwnerPolicy")).Returns(Task.FromResult(AuthorizationResult.Failed()));
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var photo = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id);
+        var command = new ChangePhotoOridinal(photo.Id, 2);
+        var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
+        Assert.NotNull(exception);
+        Assert.IsType<UnauthorizedException>(exception);
+    }
         
     // Arrange
-    private readonly ChangePhotoOridinalHandler _handler;
     private readonly TestDatabase _testDb;
+    private readonly ChangePhotoOridinalHandler _handler;
+    private readonly Mock<IDatingAppAuthorizationService> _authService;
     public ChangePhotoOridinalHandlerTests()
     {
-        var settings = new UserSettings(Guid.Parse("00000000-0000-0000-0000-000000000001"), Sex.Female, 18, 20, 50, 40.5, 40.5);
-        var user = new User(Guid.Parse("00000000-0000-0000-0000-000000000001"), "123456789", "test@test.com", "Janusz", new DateOnly(2000,1,1), Sex.Male, null, settings);
-
-        var photo = new Photo(Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "abc", "abc", 0);
-
         _testDb = new TestDatabase();
-        _testDb.DbContext.Users.Add(user);
-        _testDb.DbContext.SaveChanges();
-        _testDb.DbContext.Photos.Add(photo);
-        _testDb.DbContext.SaveChanges();
-
         var mockedPhotoOrderer = new Mock<IPhotoOrderer>();
         mockedPhotoOrderer.Setup(m => m.OrderPhotos(It.IsAny<List<Photo>>(), It.IsAny<Guid>(), It.IsAny<int>()))
             .Returns((List<Photo> x, Guid y, int z) => x);
-
         var photoRepository = new DbPhotoRepository(_testDb.DbContext);
-        _handler = new ChangePhotoOridinalHandler(photoRepository, mockedPhotoOrderer.Object);
+        _authService = new Mock<IDatingAppAuthorizationService>();
+        _handler = new ChangePhotoOridinalHandler(photoRepository, mockedPhotoOrderer.Object, _authService.Object);
     }
 
     // Teardown

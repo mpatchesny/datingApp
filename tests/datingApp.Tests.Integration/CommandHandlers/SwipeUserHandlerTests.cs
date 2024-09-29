4ddfe7a11a1505f6cd4b592890e2dab7b7bbe75e
M. Patchesny <strielok11+github@gmail.com>
2024-09-29 10:46:16 +0200

    fix and add tests

--- tests/datingApp.Tests.Integration/CommandHandlers/SwipeUserHandlerTests.cs
+++ tests/datingApp.Tests.Integration/CommandHandlers/SwipeUserHandlerTests.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Commands;
 using datingApp.Application.Commands.Handlers;
+using datingApp.Application.DTO;
 using datingApp.Application.Exceptions;
+using datingApp.Application.Storage;
 using datingApp.Core.Consts;
 using datingApp.Core.Entities;
 using datingApp.Infrastructure.DAL.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using Xunit;
 
 namespace datingApp.Tests.Integration.CommandHandlers;
@@ -15,39 +21,70 @@ namespace datingApp.Tests.Integration.CommandHandlers;
 
 public class SwipeUserHandlerTests : IDisposable
 {
-    [Fact]
-    public async Task add_swipe_should_succeed()
+    [Theory]
+    [InlineData(Like.Like)]
+    [InlineData(Like.Pass)]
+    public async Task given_users_exist_add_swipe_should_succeed(Like like)
     {
-        var command = new SwipeUser(Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000002"), 1);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new SwipeUser(user1.Id, user2.Id, (int) like);
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
 
-    [Fact]
-    public async Task add_swipe_for_nonexistsing_user_should_not_throw_exception()
+    [Theory]
+    [InlineData(Like.Like)]
+    [InlineData(Like.Pass)]
+    public async Task add_swipe_should_add_IsLikedByOtherUserDto_to_storage_only_once_with_propero_IsLikedByOtherUserValue(Like like)
     {
-        var command = new SwipeUser(Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000003"), 1);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new SwipeUser(user1.Id, user2.Id, (int) like);
+        var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
+        Assert.Null(exception);
+        _isLikedByOtherUserStorage.Verify(x => x.Set(It.IsAny<IsLikedByOtherUserDto>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task given_swipe_with_like_value_from_other_user_exists_add_swipe_with_like_value_should_create_new_match()
+    {
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        _ = await IntegrationTestHelper.CreateSwipeAsync(_testDb, user2.Id, user1.Id, Like.Like);
+
+        var command = new SwipeUser(user1.Id, user2.Id, 1);
+        await _handler.HandleAsync(command);
+        var match = await _testDb.DbContext.Matches.FirstOrDefaultAsync(m => (m.UserId1 == user1.Id && m.UserId2 == user2.Id) || 
+            (m.UserId1 == user2.Id && m.UserId2 == user1.Id) );
+        Assert.NotNull(match);
+        Assert.Equal(match.UserId1, user1.Id);
+        Assert.Equal(match.UserId2, user1.Id);
+    }
+
+    [Fact]
+    public async Task given_user_does_not_exists_add_swipe_should_not_throw_exception()
+    {
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+
+        var command = new SwipeUser(user1.Id, Guid.NewGuid(), 1);
         var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
         Assert.Null(exception);
     }
 
     // Arrange
-    private readonly SwipeUserHandler _handler;
     private readonly TestDatabase _testDb;
+    private readonly SwipeUserHandler _handler;
+    private readonly Mock<IIsLikedByOtherUserStorage> _isLikedByOtherUserStorage;
     public SwipeUserHandlerTests()
     {
-        var settings = new UserSettings(Guid.Parse("00000000-0000-0000-0000-000000000001"), PreferredSex.Female, 18, 20, 50, 40.5, 40.5);
-        var user = new User(Guid.Parse("00000000-0000-0000-0000-000000000001"), "123456789", "test@test.com", "Janusz", new DateOnly(2000,1,1), UserSex.Male, null, settings);
-
-        var settings2 = new UserSettings(Guid.Parse("00000000-0000-0000-0000-000000000002"), PreferredSex.Female, 18, 20, 50, 40.5, 40.5);
-        var user2 = new User(Guid.Parse("00000000-0000-0000-0000-000000000002"), "111111111", "test2@test.com", "Janusz", new DateOnly(2000,1,1), UserSex.Male, null, settings2);
         _testDb = new TestDatabase();
-        _testDb.DbContext.Users.Add(user);
-        _testDb.DbContext.Users.Add(user2);
-        _testDb.DbContext.SaveChanges();
-
         var swipeRepository = new DbSwipeRepository(_testDb.DbContext);
-        _handler = new SwipeUserHandler(swipeRepository);
+        var matchRepository = new DbMatchRepository(_testDb.DbContext);
+        _isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
+        _handler = new SwipeUserHandler(swipeRepository, matchRepository, _isLikedByOtherUserStorage.Object);
     }
 
     // Teardown

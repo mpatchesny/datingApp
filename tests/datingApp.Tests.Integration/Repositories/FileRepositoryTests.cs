b48c67eb59a2665df46266c72e1cf1706be3552b
M. Patchesny <strielok11+github@gmail.com>
2024-09-29 18:52:10 +0200

    refactor

--- tests/datingApp.Tests.Integration/Repositories/FileRepositoryTests.cs
+++ tests/datingApp.Tests.Integration/Repositories/FileRepositoryTests.cs
@@ -9,6 +9,7 @@ using datingApp.Core.Consts;
 using datingApp.Core.Entities;
 using datingApp.Infrastructure.DAL.Repositories;
 using datingApp.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 using Npgsql.Replication;
 using Xunit;
 
@@ -18,95 +19,96 @@ namespace datingApp.Tests.Integration.Repositories;
 public class FileRepositoryTests : IDisposable
 {
     [Fact]
-    public async Task given_file_exists_GetFileAsync_returns_file_binaryAsync()
+    public async Task given_file_exists_ExistsAsync_returns_true()
     {
-        var photo = await CreatePhotoAsync();
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var photo = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id);
+        _ = await CreateFileAsync(photo.Id);
+
+        var exists = await _storage.ExistsAsync(photo.Id);
+        Assert.True(exists);
+    }
+
+    [Fact]
+    public async Task given_file_not_exists_ExistsAsync_returns_false()
+    {
+        var exists = await _storage.ExistsAsync(Guid.NewGuid());
+        Assert.False(exists);
+    }
+
+    [Fact]
+    public async Task given_file_exists_GetFileAsync_returns_file_binary()
+    {
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var photo = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id);
         var file = await CreateFileAsync(photo.Id);
 
-        IFileRepository storage = new DbFileRepository(_testDb.DbContext);
-        var fileBinary = await storage.GetByIdAsync(file.Id);
+        var fileBinary = await _storage.GetByIdAsync(file.Id);
         Assert.Same(fileBinary, file.Binary);
     }
 
     [Fact]
     public async Task given_file_not_exists_GetFileAsync_returns_null()
     {
-        IFileRepository storage = new DbFileRepository(_testDb.DbContext);
-        var file = await storage.GetByIdAsync(Guid.NewGuid());
+        var file = await _storage.GetByIdAsync(Guid.NewGuid());
         Assert.Null(file);
     }
 
     [Fact]
     public async Task given_file_not_exists_SaveFileAsync_saves_file()
     {
-        var photo = await CreatePhotoAsync();
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var photo = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id);
         byte[] data = new byte[] { byte.MinValue, 0, byte.MaxValue };
         var fileId = photo.Id;
 
-        IFileRepository storage = new DbFileRepository(_testDb.DbContext);
-        var exception = await Record.ExceptionAsync(async () => await storage.AddAsync(data, fileId , "txt"));
+        var exception = await Record.ExceptionAsync(async () => await _storage.AddAsync(data, fileId , "txt"));
         Assert.Null(exception);
 
-        var file = await storage.GetByIdAsync(fileId);
+        var file = await _testDb.DbContext.Files.FirstOrDefaultAsync(x => x.Id == photo.Id);
         Assert.NotNull(file);
     }
 
     [Fact]
     public async Task given_file_exists_SaveFileAsync_updates_file()
     {
-        var photo = await CreatePhotoAsync();
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var photo = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id);
         var file = await CreateFileAsync(photo.Id);
 
         file.Binary[0] = byte.MaxValue;
         file.Binary[1] = byte.MaxValue;
         file.Binary[2] = byte.MaxValue;
 
-        IFileRepository storage = new DbFileRepository(_testDb.DbContext);
-        var exception = await Record.ExceptionAsync(async () => await storage.AddAsync(file.Binary, file.Id , "txt"));
+        var exception = await Record.ExceptionAsync(async () => await _storage.AddAsync(file.Binary, file.Id , "txt"));
         Assert.Null(exception);
 
-        var fileBinary = await storage.GetByIdAsync(file.Id);
+        var fileBinary = (await _testDb.DbContext.Files.FirstOrDefaultAsync(x => x.Id == photo.Id))?.Binary;
         Assert.Same(file.Binary, fileBinary);
     }
 
     [Fact]
     public async Task given_file_exists_DeleteFileAsync_deletes_files()
     {
-        var photo = await CreatePhotoAsync();
+        var user = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var photo = await IntegrationTestHelper.CreatePhotoAsync(_testDb, user.Id);
         var file = await CreateFileAsync(photo.Id);
         byte[] data = new byte[] { byte.MinValue, 0, byte.MaxValue };
 
-        IFileRepository storage = new DbFileRepository(_testDb.DbContext);
-        await storage.DeleteAsync(file.Id);
+        await _storage.DeleteAsync(file.Id);
 
-        file = _testDb.DbContext.Files.FirstOrDefault(x => x.Id == file.Id);
-        Assert.Null(file);
+        var exists = await _testDb.DbContext.Files.AnyAsync(x => x.Id == photo.Id);
+        Assert.False(exists);
     }
 
     [Fact]
-    public async Task given_file_notexists_DeleteFileAsync_not_throws_exepction()
+    public async Task given_file_not_exists_DeleteFileAsync_not_throws_exepction()
     {
-        IFileRepository storage = new DbFileRepository(_testDb.DbContext);
-        var exception = await Record.ExceptionAsync(async () => await storage.DeleteAsync(Guid.NewGuid()));
+        var exception = await Record.ExceptionAsync(async () => await _storage.DeleteAsync(Guid.NewGuid()));
         Assert.Null(exception);
     }
 
     // Arrange
-    private async Task<Photo> CreatePhotoAsync()
-    {
-        var userId = Guid.NewGuid();
-        var settings = new UserSettings(userId, PreferredSex.MaleAndFemale, 18, 100, 100, 45.5, 45.5);
-        var user = new User(userId, "123456798", "test@test.com", "Janusz", new DateOnly(2000,1,1), UserSex.Male, null, settings);
-        await _testDb.DbContext.Users.AddAsync(user);
-        await _testDb.DbContext.SaveChangesAsync();
-
-        var photo = new Photo(Guid.NewGuid(), userId, "path", "url", 1);
-        await _testDb.DbContext.Photos.AddAsync(photo);
-        await _testDb.DbContext.SaveChangesAsync();
-
-        return photo;
-    }
-
     private async Task<FileDto> CreateFileAsync(Guid photoId)
     {
         byte[] data = new byte[] { byte.MinValue, 0, byte.MaxValue };
@@ -124,9 +126,11 @@ public class FileRepositoryTests : IDisposable
     }
 
     private readonly TestDatabase _testDb;
+    private readonly IFileRepository _storage;
     public FileRepositoryTests()
     {
         _testDb = new TestDatabase();
+        _storage = new DbFileRepository(_testDb.DbContext);
     }
 
     // Teardown

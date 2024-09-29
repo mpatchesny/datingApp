33cecdb608f1ed0ab34967e4f94c1059191faa17
M. Patchesny <strielok11+github@gmail.com>
2024-09-29 22:29:45 +0200

    refactor

--- tests/datingApp.Tests.Integration/Repositories/MessagesRepositoryTests.cs
+++ tests/datingApp.Tests.Integration/Repositories/MessagesRepositoryTests.cs
@@ -17,121 +17,151 @@ public class MessageRepositoryTests : IDisposable
     [Fact]
     public async void get_existing_messages_by_match_id_should_return_nonempty_collection()
     {
-        var messages = await _repository.GetByMatchIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000001"));
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+        _ = await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id);
+        
+        var messages = await _repository.GetByMatchIdAsync(match.Id);
         Assert.Single(messages);
     }
 
     [Fact]
-    public async void get_previous_not_displayed_messages_should_return_previous_messages_with_same_match_that_are_not_displayed()
+    public async void get_previous_not_displayed_messages_returns_not_displayed_messages_within_one_match_that_were_sent_before_the_message()
     {
-        var messages = new List<Message>{
-            new Message(Guid.Parse("00000000-0000-0000-0000-000000000004"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", true, DateTime.UtcNow - TimeSpan.FromSeconds(1)),
-            new Message(Guid.Parse("00000000-0000-0000-0000-000000000002"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow),
-            new Message(Guid.Parse("00000000-0000-0000-0000-000000000003"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow + TimeSpan.FromSeconds(1)),
-            new Message(Guid.Parse("00000000-0000-0000-0000-000000000005"), Guid.Parse("00000000-0000-0000-0000-000000000002"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow - TimeSpan.FromSeconds(10))
-        };
-        await _testDb.DbContext.Messages.AddRangeAsync(messages);
-        await _testDb.DbContext.SaveChangesAsync();
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user3 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match1 = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+        var match2 = await IntegrationTestHelper.CreateMatchAsync(_testDb, user2.Id, user3.Id);
+        _ = await IntegrationTestHelper.CreateMessageAsync(_testDb, match1.Id, user1.Id);
+        var message = await IntegrationTestHelper.CreateMessageAsync(_testDb, match1.Id, user1.Id);
+        _ = await IntegrationTestHelper.CreateMessageAsync(_testDb, match1.Id, user1.Id);
+        _ = await IntegrationTestHelper.CreateMessageAsync(_testDb, match2.Id, user1.Id);
 
-        var result = await _repository.GetPreviousNotDisplayedMessages(Guid.Parse("00000000-0000-0000-0000-000000000002"));
+        var result = await _repository.GetPreviousNotDisplayedMessages(message.Id);
         Assert.Equal(2, result.Count());
     }
 
     [Fact]
-    public async void get_existing_message_by_id_should_return_nonempty_message()
+    public async void get_existing_message_by_id_should_return_succeed()
     {
-        var message = await _repository.GetByIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000001"));
-        Assert.NotNull(message);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+        var message = await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id);
+
+        var returnedMessage = await _repository.GetByIdAsync(message.Id);
+        Assert.Equal(message, returnedMessage);
     }
 
     [Fact]
-    public async void get_nonexisting_message_by_id_should_return_null()
+    public async void given_message_not_exists_get_message_by_id_should_return_null()
     {
-        var message = await _repository.GetByIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000000"));
+        var message = await _repository.GetByIdAsync(Guid.NewGuid());
         Assert.Null(message);
     }
 
     [Fact]
-    public async void get_nonexisting_message_by_match_id_should_return_empty_collection()
+    public async void given_message_not_exists_get_message_by_match_id_should_return_empty_collection()
     {
-        var messages = await _repository.GetByMatchIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000002"));
+        var messages = await _repository.GetByMatchIdAsync(Guid.NewGuid());
         Assert.Empty(messages);
     }
 
     [Fact]
     public async void delete_existing_message_by_id_should_succeed()
     {
-        var exception = await Record.ExceptionAsync(async () => await _repository.DeleteAsync(Guid.Parse("00000000-0000-0000-0000-000000000001")));
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+        var message = await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id);
+
+        var exception = await Record.ExceptionAsync(async () => await _repository.DeleteAsync(message.Id));
         Assert.Null(exception);
-        var deletedMessage = await _testDb.DbContext.Messages.FirstOrDefaultAsync(x => x.Id == Guid.Parse("00000000-0000-0000-0000-000000000001"));
+        var deletedMessage = await _testDb.DbContext.Messages.FirstOrDefaultAsync(x => x.Id == message.Id);
         Assert.Null(deletedMessage);
     }
 
     [Fact]
     public async void after_delete_message_get_messages_by_match_id_should_return_minus_one_elements()
     {
-        var messageId = Guid.Parse("00000000-0000-0000-0000-000000000001");
-        await _repository.DeleteAsync(messageId);
-        var messages = await _repository.GetByMatchIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000001"));
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+        var message = await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id);
+
+        await _repository.DeleteAsync(message.Id);
+        var messages = await _repository.GetByMatchIdAsync(match.Id);
         Assert.Empty(messages);
     }
 
     [Fact]
-    public async void delete_nonexisting_message_by_id_should_throw_exception()
+    public async void given_message_not_exists_delete_message_by_id_should_not_throw_exception()
     {
-        var exception = await Record.ExceptionAsync(async () => await _repository.DeleteAsync(Guid.Parse("00000000-0000-0000-0000-000000000002")));
+        var exception = await Record.ExceptionAsync(async () => await _repository.DeleteAsync(Guid.NewGuid()));
         Assert.NotNull(exception);
     }
 
     [Fact]
     public async void add_message_should_succeed()
     {
-        var message = new Message(Guid.Parse("00000000-0000-0000-0000-000000000002"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+
+        var message = new Message(Guid.NewGuid(), match.Id, user1.Id, "ahoj", false, DateTime.UtcNow);
         await _repository.AddAsync(message);
-        var messages = await _repository.GetByMatchIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000001"));
-        Assert.NotNull(messages);
-        Assert.Equal(2, messages.Count());
-        var addedMessage = _testDb.DbContext.Messages.FirstOrDefault(x => x.Id == Guid.Parse("00000000-0000-0000-0000-000000000002"));
+        var addedMessage = _testDb.DbContext.Messages.FirstOrDefault(x => x.Id == message.Id);
         Assert.Same(message, addedMessage);
     }
 
     [Fact]
     public async void update_message_should_succeed()
     {
-        var message = await _repository.GetByIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000001"));
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+        var message = await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id);
+
         message.SetDisplayed();
         var exception = await Record.ExceptionAsync(async () => await _repository.UpdateAsync(message));
         Assert.Null(exception);
-        var updatedMessage = _testDb.DbContext.Messages.FirstOrDefault(x => x.Id == Guid.Parse("00000000-0000-0000-0000-000000000001"));
+        var updatedMessage = _testDb.DbContext.Messages.FirstOrDefault(x => x.Id == message.Id);
         Assert.Same(message, updatedMessage);
     }
 
     [Fact]
     public async void update_range_should_succeed()
     {
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
         var messages = new List<Message> {
-            new Message(Guid.Parse("00000000-0000-0000-0000-000000000002"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow),
-            new Message(Guid.Parse("00000000-0000-0000-0000-000000000003"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow),
-            new Message(Guid.Parse("00000000-0000-0000-0000-000000000004"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow),
-            new Message(Guid.Parse("00000000-0000-0000-0000-000000000005"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow)
+            await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id),
+            await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id),
+            await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id),
+            await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id),
         };
-        await _testDb.DbContext.Messages.AddRangeAsync(messages);
-        _testDb.DbContext.SaveChanges();
 
-        var updatedMessages = await _repository.GetByMatchIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000001"));
-        foreach (var message in updatedMessages)
+        foreach (var message in messages)
         {
             message.SetDisplayed();
         }
 
-        var exception = await Record.ExceptionAsync(async () => await _repository.UpdateRangeAsync(updatedMessages.ToArray()));
+        var exception = await Record.ExceptionAsync(async () => await _repository.UpdateRangeAsync(messages.ToArray()));
         Assert.Null(exception);
     }
 
     [Fact]
     public async void add_message_with_existing_id_should_throw_exception()
     {
-        var message = new Message(Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+        var message = await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id);
+
+        var badMessage = new Message(message.Id, match.Id, user1.Id, "ahoj", false, DateTime.UtcNow);
         var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(message));
         Assert.NotNull(exception);
         Assert.IsType<InvalidOperationException>(exception);
@@ -140,34 +170,25 @@ public class MessageRepositoryTests : IDisposable
     [Fact]
     public async void after_add_message_get_messages_by_match_id_should_return_plus_one_elements()
     {
-        var message = new Message(Guid.Parse("00000000-0000-0000-0000-000000000003"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow);
-        await _repository.AddAsync(message);
-        var messages = await _repository.GetByMatchIdAsync(Guid.Parse("00000000-0000-0000-0000-000000000001"));
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb, user1.Id, user2.Id);
+        _ = await IntegrationTestHelper.CreateMessageAsync(_testDb, match.Id, user1.Id);
+        
+        var newMessage = new Message(Guid.NewGuid(), match.Id, user1.Id, "ahoj", false, DateTime.UtcNow);
+        await _repository.AddAsync(newMessage);
+        
+        var messages = await _repository.GetByMatchIdAsync(match.Id);
         Assert.NotNull(messages);
         Assert.Equal(2, messages.Count());
     }
 
     // Arrange
-    private readonly IMessageRepository _repository;
     private readonly TestDatabase _testDb;
+    private readonly IMessageRepository _repository;
     public MessageRepositoryTests()
     {
-        var settings = new UserSettings(Guid.Parse("00000000-0000-0000-0000-000000000001"), PreferredSex.Female, 18, 20, 50, 40.5, 40.5);
-        var user = new User(Guid.Parse("00000000-0000-0000-0000-000000000001"), "123456789", "test@test.com", "Janusz", new DateOnly(2000,1,1), UserSex.Male, null, settings);
         _testDb = new TestDatabase();
-        _testDb.DbContext.Users.Add(user);
-        _testDb.DbContext.SaveChanges();
-
-        var match = new Match(Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), false, false, null, DateTime.UtcNow);
-        var match2 = new Match(Guid.Parse("00000000-0000-0000-0000-000000000002"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), false, false, null, DateTime.UtcNow);
-        _testDb.DbContext.Matches.Add(match);
-        _testDb.DbContext.Matches.Add(match2);
-        _testDb.DbContext.SaveChanges();
-
-        var message = new Message(Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), Guid.Parse("00000000-0000-0000-0000-000000000001"), "ahoj", false, DateTime.UtcNow);
-        _testDb.DbContext.Messages.Add(message);
-        _testDb.DbContext.SaveChanges();
-
         _repository = new DbMessageRepository(_testDb.DbContext);
     }
 

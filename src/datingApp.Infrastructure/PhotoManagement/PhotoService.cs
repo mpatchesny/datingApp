05598cac9a266177390e22685cf5457b0b5c4aa0
M. Patchesny <strielok11+github@gmail.com>
2024-10-01 17:30:46 +0200

    add approximation calc of how much bigger file encoded as bas64 will be

--- src/datingApp.Infrastructure/PhotoManagement/PhotoService.cs
+++ src/datingApp.Infrastructure/PhotoManagement/PhotoService.cs
@@ -96,15 +96,15 @@ internal sealed class PhotoService : IPhotoService
 
         int maxPhotoSizeMB = _options.Value.MaxPhotoSizeBytes / (1024*1024);
         int minPhotoSizeKB = _options.Value.MinPhotoSizeBytes / 1024;
-        int maxPhotoSizeMBBytes64EncodedString = 0; // TODO
-        int minPhotoSizeKBBytes64EncodedString = 0; // TODO
+        int maxPhotoSizeMBBytes64EncodedStringApprox = (int) Math.Ceiling(1.5 * maxPhotoSizeMB);
+        int minPhotoSizeKBBytes64EncodedStringApprox = (int) Math.Ceiling(1.5 * maxPhotoSizeMB);
 
         // Initial photo validation: is base64 string length within range
-        if (_base64Bytes.Length < maxPhotoSizeMBBytes64EncodedString)
+        if (_base64Bytes.Length < maxPhotoSizeMBBytes64EncodedStringApprox)
         {
             throw new InvalidPhotoSizeException(minPhotoSizeKB, maxPhotoSizeMB);
         }
-        if (_base64Bytes.Length > minPhotoSizeKBBytes64EncodedString)
+        if (_base64Bytes.Length > minPhotoSizeKBBytes64EncodedStringApprox)
         {
             throw new InvalidPhotoSizeException(minPhotoSizeKB, maxPhotoSizeMB);
         }

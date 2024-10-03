b53cab5485b1b8f505b04dc4c449e730f74119c0
MPatchesny <strielok11+github@gmail.com>
2024-10-03 12:19:52 +0100

    fill tests

--- tests/datingApp.Tests.Unit/Entities/PhotoFileTests.cs
+++ tests/datingApp.Tests.Unit/Entities/PhotoFileTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using datingApp.Core.Entities;
+using datingApp.Core.Exceptions;
 using Xunit;
 
 namespace datingApp.Tests.Unit.Entities;
@@ -11,67 +13,116 @@ public class PhotoFileTests
     [Fact]
     public void given_bytes_content_is_null_throws_EmptyPhotoContentException()
     {
-        Assert.True(true);
+        byte[] bytes = null;
+
+        var exception = Record.Exception(() => new PhotoFile(Guid.NewGuid(), bytes));
+        Assert.NotNull(exception);
+        Assert.IsType<EmptyPhotoContentException>(exception);
     }
 
-    [Fact]
-    public void given_base64_content_is_null_or_emptystring_throws_EmptyPhotoContentException()
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void given_base64_content_is_null_or_emptystring_throws_EmptyPhotoContentException(string byte64Content)
     {
-        Assert.True(true);
+        var exception = Record.Exception(() => new PhotoFile(Guid.NewGuid(), byte64Content));
+        Assert.NotNull(exception);
+        Assert.IsType<EmptyPhotoContentException>(exception);
     }
 
     [Fact]
     public void given_not_recognized_file_format_throws_InvalidPhotoException()
     {
-        Assert.True(true);
+        // TODO
+        byte[] bytes = null;
+
+        var exception = Record.Exception(() => new PhotoFile(Guid.NewGuid(), bytes));
+        Assert.NotNull(exception);
+        Assert.IsType<FailToConvertBase64StringToArrayOfBytesException>(exception);
     }
 
     [Fact]
-    public void given_recognized_file_format_extension_has_proper_value()
+    public void given_recognized_file_format_Extension_has_proper_value()
     {
-        Assert.True(true);
+        // TODO
+        byte[] bytes = null;
+
+        PhotoFile photoFile = new PhotoFile(Guid.NewGuid(), bytes);
+        Assert.Equal("jpg", photoFile.Extension);
+        Assert.Equal(bytes, photoFile.Content);
     }
 
     [Fact]
-    public void given_bad_base64_content_throws_FailToConvertBase64StringToArrayOfBytes()
+    public void given_bad_base64_content_throws_FailToConvertBase64StringToArrayOfBytesException()
     {
-        Assert.True(true);
+        var base64Content = new string('a', 25000);
+
+        var exception = Record.Exception(() => new PhotoFile(Guid.NewGuid(), base64Content));
+        Assert.NotNull(exception);
+        Assert.IsType<FailToConvertBase64StringToArrayOfBytesException>(exception);
     }
 
     [Fact]
-    public void given_valid_base64_content_Content_and_Exception_is_set()
+    public void given_valid_base64_content_Content_and_Extension_is_set()
     {
-        Assert.True(true);
+        // TODO
+        byte[] bytes = null;
+        string byte64Content = "";
+
+        PhotoFile photoFile = new PhotoFile(Guid.NewGuid(), byte64Content);
+        Assert.Equal("jpg", photoFile.Extension);
+        Assert.Equal(bytes, photoFile.Content);
     }
 
     [Fact]
-    public void given_valid_bytes_content_Content_and_Exception_is_set()
+    public void given_valid_bytes_content_Content_and_Extension_is_set()
     {
-        Assert.True(true);
+        // TODO
+        byte[] bytes = null;
+
+        PhotoFile photoFile = new PhotoFile(Guid.NewGuid(), bytes);
+        Assert.Equal("jpg", photoFile.Extension);
+        Assert.Equal(bytes, photoFile.Content);
     }
 
     [Fact]
     public void given_bytes_content_is_too_small_throws_InvalidPhotoSizeException()
     {
-        Assert.True(true);
+        var byteContent = new byte[10240 - 1];
+
+        var exception = Record.Exception(() => new PhotoFile(Guid.NewGuid(), byteContent));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoSizeException>(exception);
     }
 
     [Fact]
     public void given_bytes_content_is_too_large_throws_InvalidPhotoSizeException()
     {
-        Assert.True(true);
+        var byteContent = new byte[2621440 + 1];
+
+        var exception = Record.Exception(() => new PhotoFile(Guid.NewGuid(), byteContent));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoSizeException>(exception);
     }
 
     [Fact]
     public void given_base64_content_is_too_small_throws_InvalidPhotoSizeException()
     {
-        Assert.True(true);
+        var base64Content = new string('a', 15359);
+
+        var exception = Record.Exception(() => new PhotoFile(Guid.NewGuid(), base64Content));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoSizeException>(exception);
     }
 
     [Fact]
     public void given_base64_content_is_too_large_throws_InvalidPhotoSizeException()
     {
-        Assert.True(true);
+        var base64Content = new string('a', 3932161);
+
+        var exception = Record.Exception(() => new PhotoFile(Guid.NewGuid(), base64Content));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoSizeException>(exception);
     }
 
     private static string ToBase64(byte[] file)

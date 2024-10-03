bed56975c95060328865b34c2cb8fc1b89a40a04
MPatchesny <strielok11+github@gmail.com>
2024-10-03 11:14:22 +0100

    add

---
+++ src/datingApp.Infrastructure/DAL/Configurations/PhotoFileConfiguration.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace datingApp.Infrastructure.DAL.Configurations;
+
+internal sealed class PhotoFileConfiguration
+{
+    public void Configure(EntityTypeBuilder<PhotoFile> builder)
+    {
+        builder.HasKey(x => x.PhotoId);
+        builder.Property(x => x.PhotoId)
+            .IsRequired();
+        builder.Property(x => x.Content)
+            .IsRequired();
+    }
+}

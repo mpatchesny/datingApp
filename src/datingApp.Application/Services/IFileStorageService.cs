8cc403d531fe581e839508902a99262f69dbf493
M. Patchesny <strielok11+github@gmail.com>
2024-10-08 17:20:39 +0200

    move to services

--- src/datingApp.Application/Services/IFileStorageService.cs
+++ src/datingApp.Application/Services/IFileStorageService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace datingApp.Application.Storage;
+namespace datingApp.Application.Services;
 
 public interface IFileStorageService
 {

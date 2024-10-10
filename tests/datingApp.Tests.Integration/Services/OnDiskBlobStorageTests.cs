effda5aefc453416a6543374f725d04925504252
M. Patchesny <strielok11+github@gmail.com>
2024-10-10 09:20:06 +0200

    add

---
+++ tests/datingApp.Tests.Integration/Services/OnDiskBlobStorageTests.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentStorage;
+using FluentStorage.Blobs;
+using Xunit;
+
+namespace datingApp.Tests.Integration.Services;
+
+public class OnDiskBlobStorageTests
+{
+    [Fact]
+    public async void given_valid_input_WriteAsync_saves_file()
+    {
+        byte[] data = new byte[] { byte.MinValue, 0, byte.MaxValue };
+        var filename = "test.txt";
+        await _storageService.WriteAsync(filename, new MemoryStream(data));
+
+        var filePath = System.IO.Path.Combine(_storagePath, filename);
+        var exists = System.IO.File.Exists(filePath);
+        Assert.True(exists);
+    }
+
+    [Fact]
+    public async void given_invalid_input_WriteAsync_not_throws_exception()
+    {
+        byte[] data = new byte[0];
+        var filename = "test.txt";
+        var exception = await Record.ExceptionAsync(() => _storageService.WriteAsync(filename, new MemoryStream(data)));
+        Assert.Null(exception);
+
+        var dir = new System.IO.DirectoryInfo(_storagePath);
+        var files = dir.GetFiles();
+        Assert.Empty(files);
+    }
+
+    [Fact]
+    public async void given_file_exists_DeleteAsync_deletes_file()
+    {
+        var filename = "test.txt";
+        var filePath = System.IO.Path.Combine(_storagePath, filename);
+        byte[] data = new byte[] { byte.MinValue, 0, byte.MaxValue };
+        System.IO.File.WriteAllBytes(filePath, data);
+
+        await _storageService.DeleteAsync(filename);
+        var exists = System.IO.File.Exists(filePath);
+        Assert.False(exists);
+    }
+
+    [Fact]
+    public async void given_file_exists_DeleteAsync_deletes_list_of_files()
+    {
+        var fileNames = new List<string>();
+        var filePaths = new List<string>();
+        for (int i = 0; i < 10; i++)
+        {
+            var guid = Guid.NewGuid().ToString().Replace("-", "");
+            var filename = $"test_{guid}.txt";
+            var filePath = System.IO.Path.Combine(_storagePath, filename);
+            byte[] data = new byte[] { byte.MinValue, 0, byte.MaxValue };
+            System.IO.File.WriteAllBytes(filePath, data);
+            fileNames.Append(filename);
+            filePaths.Append(filePath);
+        }
+
+        await _storageService.DeleteAsync(fileNames);
+        foreach (var file in filePaths)
+        {
+            var exists = System.IO.File.Exists(file);
+            Assert.False(exists);
+        }
+    }
+
+    [Fact]
+    public async void given_file_not_exists_DeleteAsync_not_throws_exception()
+    {
+        var exception = await Record.ExceptionAsync(() => _storageService.DeleteAsync("not_existing_file.txt"));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async Task given_files_not_exists_DeleteAsync_not_throws_exceptionAsync()
+    {
+        var notExistingFiles = new List<string>();
+        for (int i = 0; i < 10; i++)
+        {
+            var guid = Guid.NewGuid().ToString().Replace("-", "");
+            var filename = $"test_{guid}.txt";
+            notExistingFiles.Append(filename);
+        }
+
+        var exception = await Record.ExceptionAsync(() => _storageService.DeleteAsync(notExistingFiles));
+        Assert.Null(exception);
+    }
+
+    private readonly IBlobStorage _storageService;
+    private readonly string _storagePath;
+    public OnDiskBlobStorageTests()
+    {
+        _storagePath = System.IO.Path.Combine(
+                Path.GetTempPath(), $"datingapptest_{Guid.NewGuid()}"
+            );
+        var dir = new DirectoryInfo(_storagePath);
+        dir.Create();
+        _storageService = StorageFactory.Blobs.DirectoryFiles(_storagePath);
+    }
+
+    public async void Dispose()
+    {
+        await Task.Delay(1000);
+        var dir = new DirectoryInfo(_storagePath);
+        dir.Delete(true);
+    }
+}

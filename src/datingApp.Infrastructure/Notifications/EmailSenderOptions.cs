ade7797eb23aa0f9a9e826001a5bf51a367756c7
M. Patchesny <strielok11+github@gmail.com>
2024-10-17 21:39:26 +0200

    change EmailSenderOptions

--- src/datingApp.Infrastructure/Notifications/EmailSenderOptions.cs
+++ src/datingApp.Infrastructure/Notifications/EmailSenderOptions.cs
@@ -7,10 +7,11 @@ namespace datingApp.Infrastructure.Notifications;
 
 public sealed class EmailSenderOptions
 {
+    public string ClientId { get; set; }
+    public string TenantId { get; set; }
+    public string ClientSecret { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
     public string ServerAddress { get; set; }
     public int ServerPort { get; set; }
-    public bool EnableSsl { get; set; }
-    public TimeSpan EmailSendTimeout { get; set; }
 }

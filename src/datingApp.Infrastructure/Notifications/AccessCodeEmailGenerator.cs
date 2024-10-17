cdda6455e12975c4a209ff9477f457b71a14a32c
MPatchesny <strielok11+github@gmail.com>
2024-10-17 14:00:50 +0100

    get sender from EmailSenderOptions and add it to generated email

--- src/datingApp.Infrastructure/Notifications/AccessCodeEmailGenerator.cs
+++ src/datingApp.Infrastructure/Notifications/AccessCodeEmailGenerator.cs
@@ -10,12 +10,15 @@ namespace datingApp.Infrastructure.Notifications;
 
 public class AccessCodeEmailGenerator : INotificationMessageGenerator<Email>
 {
+    private readonly string _sender;
     private readonly string _bodyTemplate;
     private readonly string _subjectTemplate;
-    public AccessCodeEmailGenerator(IOptions<EmailGeneratorOptions> options)
+    public AccessCodeEmailGenerator(IOptions<EmailGeneratorOptions> emailGeneratorOptions,
+                                    IOptions<EmailSenderOptions> emailSenderOptions)
     {
-        _subjectTemplate = options.Value.SubjectTemplate;
-        _bodyTemplate = options.Value.BodyTemplate;
+        _sender = emailSenderOptions.Value.SendFrom;
+        _subjectTemplate = emailGeneratorOptions.Value.SubjectTemplate;
+        _bodyTemplate = emailGeneratorOptions.Value.BodyTemplate;
     }
    
     public Email Generate(string Recipient, Dictionary<string, string> kwargs)
@@ -29,6 +32,6 @@ public class AccessCodeEmailGenerator : INotificationMessageGenerator<Email>
             subject = subject.Replace($"{key}", kwargs[key]);
             body = body.Replace($"{key}", kwargs[key]);
         }
-        return new Email("TODO", recipient, subject, body);
+        return new Email(_sender, recipient, subject, body);
     }
 }

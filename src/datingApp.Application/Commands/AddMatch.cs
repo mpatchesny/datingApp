a3b9c78244fb170d570683acf9446271a68b411c
MPatchesny <strielok11+github@gmail.com>
2024-10-29 07:37:16 +0000

    make command properties PascalCase

--- src/datingApp.Application/Commands/AddMatch.cs
+++ src/datingApp.Application/Commands/AddMatch.cs
@@ -6,4 +6,4 @@ using datingApp.Application.Abstractions;
 
 namespace datingApp.Application.Commands;
 
-public sealed record AddMatch(Guid swipedById, Guid swipedWhoId) : AuthenticatedCommandBase;
+public sealed record AddMatch(Guid SwipedById, Guid SwipedWhoId) : AuthenticatedCommandBase;

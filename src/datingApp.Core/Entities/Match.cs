4f24916494ab9b7e9db571e559af7e5a6dd26cc7
M. Patchesny <strielok11+github@gmail.com>
2024-10-30 23:35:02 +0100

    add AddMessage, RemoveMessage methods; new List<messages> for null messages collection in constructor

--- src/datingApp.Core/Entities/Match.cs
+++ src/datingApp.Core/Entities/Match.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using datingApp.Core.ValueObjects;
 
@@ -13,7 +14,7 @@ public class Match
     public UserId UserId2 { get; private set; }
     public bool IsDisplayedByUser1 { get; private set; }
     public bool IsDisplayedByUser2 { get; private set; }
-    public IEnumerable<Message> Messages { get; private set; } = new List<Message>();
+    public ICollection<Message> Messages { get; private set; }
     public DateTime CreatedAt { get; private set; }
 
     private Match()
@@ -21,14 +22,14 @@ public class Match
         // EF
     }
 
-    public Match(MatchId id, UserId userId1, UserId userId2, bool isDisplayedByUser1, bool isDisplayedByUser2, IEnumerable<Message> messages, DateTime createdAt)
+    public Match(MatchId id, UserId userId1, UserId userId2, bool isDisplayedByUser1, bool isDisplayedByUser2, ICollection<Message> messages, DateTime createdAt)
     {
         Id = id;
         UserId1 = userId1;
         UserId2 = userId2;
         IsDisplayedByUser1 = isDisplayedByUser1;
         IsDisplayedByUser2 = isDisplayedByUser2;
-        Messages = messages;
+        Messages = messages ?? new List<Message>();
         CreatedAt = createdAt;
     }
 
@@ -41,6 +42,21 @@ public class Match
         else if (UserId2.Equals(userId))
         {
             IsDisplayedByUser2 = true;
+        }
+    }
+
+    public void AddMessage(Message message)
+    {
+        if (Messages.Any(m => m.Id == message.Id)) return;
+        Messages.Add(message);
+    }
+
+    public void RemoveMessage(MessageId messageId)
+    {
+        var message = Messages.FirstOrDefault(m => m.Id == messageId);
+        if (message != null) 
+        {
+            Messages.Remove(message);
         }
     }
 }

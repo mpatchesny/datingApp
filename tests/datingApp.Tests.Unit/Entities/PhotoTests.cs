43209866e26bccc14fecd82f0957d9f1662a61dc
M. Patchesny <strielok11+github@gmail.com>
2024-10-30 23:27:22 +0100

    fix error in logic; fix test

--- tests/datingApp.Tests.Unit/Entities/PhotoTests.cs
+++ tests/datingApp.Tests.Unit/Entities/PhotoTests.cs
@@ -29,10 +29,14 @@ public class PhotoTests
         Assert.IsType<EmptyPhotoUrlException>(exception);
     }
 
-    [Fact]
-    public void photo_oridinal_is_set_to_0_if_negative()
+    [Theory]
+    [InlineData(-100)]
+    [InlineData(-3)]
+    [InlineData(-2)]
+    [InlineData(-1)]
+    public void photo_oridinal_is_set_to_0_if_negative(int oridinal)
     {
-        var photo = new Photo(Guid.NewGuid(), Guid.NewGuid(), "abcdef", -1);
+        var photo = new Photo(Guid.NewGuid(), Guid.NewGuid(), "abcdef", oridinal);
         Assert.Equal(0, photo.Oridinal.Value);
     }
 

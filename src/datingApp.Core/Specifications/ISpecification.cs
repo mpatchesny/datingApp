a6d8cfdc99a58853c24c03cecfd4e9e5787bca3c
M. Patchesny <strielok11+github@gmail.com>
2024-11-01 14:42:38 +0100

    change

--- src/datingApp.Core/Specifications/ISpecification.cs
+++ src/datingApp.Core/Specifications/ISpecification.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace datingApp.Core.Specifications;
 
 public interface ISpecification<T>
 {
-    Expression<Func<T, bool>> Criteria { get; }
+    List<Expression<Func<T, bool>>> Criteria { get; }
     List<Expression<Func<T, object>>> Include { get; }
     int? Take { get; }
+
+    public IQueryable<T> Apply(IQueryable<T> query);
 }

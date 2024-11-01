ed9e349db44d919e97c9ef0abe886704864a4695
M. Patchesny <strielok11+github@gmail.com>
2024-11-01 20:16:34 +0100

    add CreateNewDbContext method

--- tests/datingApp.Tests.Integration/TestDatabase.cs
+++ tests/datingApp.Tests.Integration/TestDatabase.cs
@@ -11,14 +11,20 @@ namespace datingApp.Tests.Integration;
 internal sealed class TestDatabase : IDisposable
 {
     public DatingAppDbContext DbContext { get; }
+    private readonly string _connectionString;
     public TestDatabase(bool randomDbName = true)
     {
         var options = new OptionsProvider().Get<ConnectionStringsOptions>("ConnectionStrings");
-        var connString = options.datingApp;
-        if (randomDbName) connString = string.Format(connString, Guid.NewGuid().ToString());
-        DbContext = new DatingAppDbContext(new DbContextOptionsBuilder<DatingAppDbContext>().UseNpgsql(connString).Options);
+        _connectionString = options.datingApp;
+        if (randomDbName) _connectionString = string.Format(_connectionString, Guid.NewGuid().ToString());
+        DbContext = new DatingAppDbContext(new DbContextOptionsBuilder<DatingAppDbContext>().UseNpgsql(_connectionString).Options);
         DbContext.Database.EnsureDeleted();
         DbContext.Database.EnsureCreated();
+    }
+
+    public DatingAppDbContext CreateNewDbContext()
+    {
+        return new DatingAppDbContext(new DbContextOptionsBuilder<DatingAppDbContext>().UseNpgsql(_connectionString).Options);
     }
 
     public void Dispose()

fdfe5631a4f0b85cd0db97bea67adec27a42405d
M. Patchesny <strielok11+github@gmail.com>
2024-11-02 20:20:14 +0100

    fix

--- tests/datingApp.Tests.Integration/Repositories/MatchRepositoryTests.cs
+++ tests/datingApp.Tests.Integration/Repositories/MatchRepositoryTests.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 using datingApp.Core.Consts;
 using datingApp.Core.Entities;
 using datingApp.Core.Repositories;
+using datingApp.Infrastructure;
 using datingApp.Infrastructure.DAL.Repositories;
-using datingApp.Infrastructure.DAL.Specifications;
+using datingApp.Infrastructure.DAL.Repositories.Specifications;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
@@ -18,13 +19,14 @@ public class MatchRepositoryTests : IDisposable
     [Fact]
     public async void given_match_exists_get_match_by_user_id_should_return_users_matches()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user3 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user4 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        _ = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
-        _ = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user3.Id);
-        _ = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user4.Id);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user3 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user4 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        _ = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
+        _ = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user3.Id);
+        _ = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user4.Id);
+        _dbContext.ChangeTracker.Clear();
 
         var matches = await _repository.GetByUserIdAsync(user1.Id);
         Assert.Equal(3, matches.Count());
@@ -40,9 +42,10 @@ public class MatchRepositoryTests : IDisposable
     [Fact]
     public async void given_match_exists_get_match_by_id_should_succeed()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
+        _dbContext.ChangeTracker.Clear();
 
         var retrievedMatch = await _repository.GetByIdAsync(match.Id);
         Assert.NotNull(match);
@@ -59,9 +62,10 @@ public class MatchRepositoryTests : IDisposable
     [Fact]
     public async void given_match_exists_get_exsits_should_return_true()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        _ = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        _ = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
+        _dbContext.ChangeTracker.Clear();
 
         var exists = await _repository.ExistsAsync(user1.Id, user2.Id);
         Assert.True(exists);
@@ -77,37 +81,27 @@ public class MatchRepositoryTests : IDisposable
     [Fact]
     public async void delete_existing_match_by_id_should_succeed()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
 
         var exception = await Record.ExceptionAsync(async () => await _repository.DeleteAsync(match));
         Assert.Null(exception);
+        _dbContext.ChangeTracker.Clear();
         var deletedMatch = await _testDb.DbContext.Matches.FirstOrDefaultAsync(x => x.Id == match.Id);
         Assert.Null(deletedMatch);
     }
 
     [Fact]
-    public async void after_delete_match_get_matches_by_user_id_should_return_minus_one_elements()
-    {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
-
-        await _repository.DeleteAsync(match);
-        var matches = await _repository.GetByUserIdAsync(user1.Id);
-        Assert.Empty(matches);
-    }
-
-    [Fact]
     public async void add_match_should_succeed()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
         var match = new Match(Guid.NewGuid(), user1.Id, user2.Id, false, false, null, DateTime.UtcNow);
 
         var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(match));
         Assert.Null(exception);
+        _dbContext.ChangeTracker.Clear();
         var addedMatch = await _testDb.DbContext.Matches.FirstOrDefaultAsync(x => x.Id == match.Id);
         Assert.True(addedMatch.IsEqualTo(match));
     }
@@ -115,13 +109,14 @@ public class MatchRepositoryTests : IDisposable
     [Fact]
     public async void update_match_should_succeed()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
         match.SetDisplayed(match.UserId1);
 
         var exception = await Record.ExceptionAsync(async () => await _repository.UpdateAsync(match));
         Assert.Null(exception);
+        _dbContext.ChangeTracker.Clear();
         var updatedMatch = await _testDb.DbContext.Matches.FirstOrDefaultAsync(x => x.Id == match.Id);
         Assert.True(updatedMatch.IsEqualTo(match));
     }
@@ -129,50 +124,34 @@ public class MatchRepositoryTests : IDisposable
     [Fact]
     public async void add_match_with_existing_id_throws_exception()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user3 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user3 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
 
         var match2 = new Match(match.Id, user1.Id, user3.Id, false, false, null, DateTime.UtcNow);
         var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(match));
         Assert.NotNull(exception);
-        // Assert.IsType<InvalidOperationException>(exception);
     }
 
     [Fact]
     public async void add_match_with_existing_user_id_throws_exception()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
 
         var match2 = new Match(Guid.NewGuid(), user1.Id, user2.Id, false, false, null, DateTime.UtcNow);
         var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(match));
         Assert.NotNull(exception);
-        // Assert.IsType<InvalidOperationException>(exception);
-    }
-
-    [Fact]
-    public async void after_add_match_get_matches_by_user_id_should_return_plus_one_elements()
-    {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user3 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        _ = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
-
-        var match = new Match(Guid.NewGuid(), user1.Id, user3.Id, false, false, null, DateTime.UtcNow);
-        await _repository.AddAsync(match);
-        var matches = await _repository.GetByUserIdAsync(user1.Id);
-        Assert.Equal(2, matches.Count());
     }
 
     [Fact]
     public async void get_match_by_nonexisting_message_id_returns_null()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        _ = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        _ = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
 
         var match = await _repository.GetByMessageIdAsync(Guid.NewGuid());
         Assert.Null(match);
@@ -181,42 +160,48 @@ public class MatchRepositoryTests : IDisposable
     [Fact]
     public async void get_match_by_existing_message_id_returns_match()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
-        var message1 = await IntegrationTestHelper.CreateMessageAsync(_testDb.CreateNewDbContext(), match.Id, user1.Id, DateTime.UtcNow);
-        var message2 = await IntegrationTestHelper.CreateMessageAsync(_testDb.CreateNewDbContext(), match.Id, user2.Id, DateTime.UtcNow);
-        var message3 = await IntegrationTestHelper.CreateMessageAsync(_testDb.CreateNewDbContext(), match.Id, user1.Id, DateTime.UtcNow);
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
+        var message1 = await IntegrationTestHelper.CreateMessageAsync(_dbContext, match.Id, user1.Id, DateTime.UtcNow);
+        var message2 = await IntegrationTestHelper.CreateMessageAsync(_dbContext, match.Id, user2.Id, DateTime.UtcNow);
+        var message3 = await IntegrationTestHelper.CreateMessageAsync(_dbContext, match.Id, user1.Id, DateTime.UtcNow);
 
         foreach (var message in new Message[]{ message1, message2, message3 })
         {
+            _dbContext.ChangeTracker.Clear();
             var retrievedMatch = await _repository.GetByMessageIdAsync(message.Id);
-            Assert.True(match.IsEqualTo(retrievedMatch));
+            Assert.True(match.Id == retrievedMatch.Id);
         }
     }
 
     [Fact]
     public async void given_specification_is_not_null_get_match_includes_messages()
     {
-        var user1 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var user2 = await IntegrationTestHelper.CreateUserAsync(_testDb.CreateNewDbContext());
-        var match = await IntegrationTestHelper.CreateMatchAsync(_testDb.CreateNewDbContext(), user1.Id, user2.Id);
-        for (int i = 0; i < 50; i++)
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var user2 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        var match = await IntegrationTestHelper.CreateMatchAsync(_dbContext, user1.Id, user2.Id);
+        for (int i = 0; i < 10; i++)
         {
-            _ = await IntegrationTestHelper.CreateMessageAsync(_testDb.CreateNewDbContext(), match.Id, user1.Id, DateTime.UtcNow);
+            _ = await IntegrationTestHelper.CreateMessageAsync(_dbContext, match.Id, user1.Id, DateTime.UtcNow);
         }
 
-        var retrievedMatch = await _repository.GetByIdAsync(match.Id);
+        _dbContext.ChangeTracker.Clear();
+        var retrievedMatch = await _repository.GetByIdAsync(match.Id, new MatchWithMessagesSpecification());
         Assert.True(match.IsEqualTo(retrievedMatch));
     }
 
+    // TODO: dodać testy żeby sprawdzić, czy UpdateAsync łapie zmiany w wiadomościach (AddMessage, RemoveMEssage itd.)
+
     // Arrange
     private readonly TestDatabase _testDb;
+    private readonly DatingAppDbContext _dbContext;
     private readonly IMatchRepository _repository;
     public MatchRepositoryTests()
     {
         _testDb = new TestDatabase();
-        _repository = new DbMatchRepository(_testDb.CreateNewDbContext());
+        _dbContext = _testDb.DbContext;
+        _repository = new DbMatchRepository(_dbContext);
     }
 
     // Teardown
--- tests/datingApp.Tests.Integration/Repositories/RevokedRefreshTokensRepositoryTests.cs
+++ tests/datingApp.Tests.Integration/Repositories/RevokedRefreshTokensRepositoryTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.DTO;
 using datingApp.Application.Repositories;
+using datingApp.Infrastructure;
 using datingApp.Infrastructure.DAL.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
@@ -16,9 +17,9 @@ public class RevokedRefreshTokensRepositoryTests : IDisposable
     public async Task given_token_exists_ExistsAsync_returns_true()
     {
         var token = new TokenDto("abcdef", DateTime.UtcNow);
-        var dbContext = _testDb.CreateNewDbContext();
-        await dbContext.RevokedRefreshTokens.AddAsync(token);
-        await dbContext.SaveChangesAsync();
+        await _dbContext.RevokedRefreshTokens.AddAsync(token);
+        await _dbContext.SaveChangesAsync();
+        _dbContext.ChangeTracker.Clear();
 
         var exists = await _repository.ExistsAsync(token.Token);
         Assert.True(exists);
@@ -37,6 +38,8 @@ public class RevokedRefreshTokensRepositoryTests : IDisposable
     {
         var token = new TokenDto("abcdef", DateTime.UtcNow);
         await _repository.AddAsync(token);
+
+        _dbContext.ChangeTracker.Clear();
         var exists = await _testDb.CreateNewDbContext().RevokedRefreshTokens.AnyAsync(x => x.Token == token.Token);
         Assert.True(exists);
     }
@@ -46,9 +49,9 @@ public class RevokedRefreshTokensRepositoryTests : IDisposable
     {
         var tokenToken = "abcdef";
         var token = new TokenDto(tokenToken, DateTime.UtcNow);
-        var dbContext = _testDb.CreateNewDbContext();
-        await dbContext.RevokedRefreshTokens.AddAsync(token);
-        await dbContext.SaveChangesAsync();
+        await _dbContext.RevokedRefreshTokens.AddAsync(token);
+        await _dbContext.SaveChangesAsync();
+        _dbContext.ChangeTracker.Clear();
 
         var badToken = new TokenDto(tokenToken, DateTime.UtcNow);
         var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(badToken));
@@ -57,11 +60,13 @@ public class RevokedRefreshTokensRepositoryTests : IDisposable
 
     // Arrange
     private readonly TestDatabase _testDb;
+    private readonly DatingAppDbContext _dbContext;
     private readonly IRevokedRefreshTokensRepository _repository;
     public RevokedRefreshTokensRepositoryTests()
     {
         _testDb = new TestDatabase();
-        _repository = new DbRevokedRefreshTokensRepository(_testDb.DbContext);
+        _dbContext = _testDb.DbContext;
+        _repository = new DbRevokedRefreshTokensRepository(_dbContext);
     }
 
     // Teardown

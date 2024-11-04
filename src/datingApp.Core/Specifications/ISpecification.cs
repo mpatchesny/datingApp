089ece7b27b2f1d37e200a437bb85bb28265d2d0
M. Patchesny <strielok11+github@gmail.com>
2024-11-04 23:10:22 +0100

    remove not needed properties

--- src/datingApp.Core/Specifications/ISpecification.cs
+++ src/datingApp.Core/Specifications/ISpecification.cs
@@ -9,9 +9,5 @@ namespace datingApp.Core.Specifications;
 
 public interface ISpecification<T>
 {
-    List<Expression<Func<T, bool>>> Criteria { get; }
-    List<Expression<Func<T, object>>> Include { get; }
-    int? Take { get; }
-
     public IQueryable<T> Apply(IQueryable<T> query);
 }

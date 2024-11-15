fae4cb1d0bda49a678e91b35c8a9ea05456a1b6d
mpatchesny <strielok11+github@gmail.com>
2024-11-15 13:51:24 +0000

    playing around

--- src/datingApp.Infrastructure/DAL/Models/MatchReadModel.cs
+++ src/datingApp.Infrastructure/DAL/Models/MatchReadModel.cs
@@ -8,9 +8,10 @@ namespace datingApp.Infrastructure.DAL.Models;
 internal class MatchReadModel
 {
     public Guid Id { get; set; }
-    public UserReadModel Owner { get; set; }
-    public UserReadModel User { get; set; }
-    public bool IsDisplayed { get; set; }
+    public UserReadModel User1 { get; set; }
+    public UserReadModel User2 { get; set; }
+    public bool IsDisplayedByUser1 { get; set; }
+    public bool IsDisplayedByUser2 { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime LastChangeTime { get; set; }
     public IEnumerable<MessageReadModel> Messages { get; set; }

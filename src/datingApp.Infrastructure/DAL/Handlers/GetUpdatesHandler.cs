879b378131a714310bd1da5fb9dd8b3cd08393d5
mpatchesny <strielok11+github@gmail.com>
2024-11-18 14:28:28 +0000

    fix formatting

--- src/datingApp.Infrastructure/DAL/Handlers/GetUpdatesHandler.cs
+++ src/datingApp.Infrastructure/DAL/Handlers/GetUpdatesHandler.cs
@@ -21,21 +21,21 @@ internal sealed class GetUpdatesHandler : IQueryHandler<GetUpdates, IEnumerable<
     private async Task<IEnumerable<Guid>> GetMatchesByMessagesPastGivenActivityTimeAsync(Guid userId, DateTime lastActivityTime)
     {
         return await _dbContext.Matches
-                    .Where(match => match.UserId1.Equals(userId) || match.UserId2.Equals(userId))
-                    .Where(match => match.Messages.Any(message => message.CreatedAt >= lastActivityTime))
-                    .AsNoTracking()
-                    .Select(match => match.Id.Value)
-                    .ToListAsync();
+            .AsNoTracking()
+            .Where(match => match.UserId1.Equals(userId) || match.UserId2.Equals(userId))
+            .Where(match => match.Messages.Any(message => message.CreatedAt >= lastActivityTime))
+            .Select(match => match.Id.Value)
+            .ToListAsync();
     }
 
     private async Task<IEnumerable<Guid>> GetMatchesPastGivenActivityTimeAsync(Guid userId, DateTime lastActivityTime)
     {
         return await _dbContext.Matches
-                    .Where(match => match.UserId1.Equals(userId) || match.UserId2.Equals(userId))
-                    .Where(match => match.CreatedAt >= lastActivityTime)
-                    .AsNoTracking()
-                    .Select(match => match.Id.Value)
-                    .ToListAsync();
+            .AsNoTracking()
+            .Where(match => match.UserId1.Equals(userId) || match.UserId2.Equals(userId))
+            .Where(match => match.CreatedAt >= lastActivityTime)
+            .Select(match => match.Id.Value)
+            .ToListAsync();
     }
 
     public async Task<IEnumerable<MatchDto>> HandleAsync(GetUpdates query)
@@ -53,8 +53,11 @@ internal sealed class GetUpdatesHandler : IQueryHandler<GetUpdates, IEnumerable<
             from match in _dbContext.Matches
                 .Include(match => match.Messages
                     .Where(message => message.CreatedAt >= query.LastActivityTime))
-            from user in _dbContext.Users.Include(user => user.Photos).Include(user => user.Settings)
-            where (match.UserId1.Equals(user.Id) || match.UserId2.Equals(user.Id)) && !user.Id.Equals(query.UserId)
+            from user in _dbContext.Users
+                .Include(user => user.Photos)
+                .Include(user => user.Settings)
+            where !user.Id.Equals(query.UserId)
+            where match.UserId1.Equals(user.Id) || match.UserId2.Equals(user.Id)
             where newMessagesAndMatches.Contains(match.Id)
             select new 
             {

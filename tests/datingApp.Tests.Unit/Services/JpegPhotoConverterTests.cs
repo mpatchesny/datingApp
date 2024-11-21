55c7edb0df90ba3b14bc305ab785f883880d495c
mpatchesny <strielok11+github@gmail.com>
2024-11-21 14:35:01 +0000

    fix

--- tests/datingApp.Tests.Unit/Services/JpegPhotoConverterTests.cs
+++ tests/datingApp.Tests.Unit/Services/JpegPhotoConverterTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Infrastructure.Services;
@@ -11,17 +12,62 @@ namespace datingApp.Tests.Unit.Services;
 public class JpegPhotoConverterTests
 {
     [Fact]
-    public void Test1()
+    public async void given_valid_webp_file_convert_to_jpeg_should_success()
     {
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64WebpSample);
+        var exception = await Record.ExceptionAsync(() => _converter.ConvertAsync(imageStream));
+        Assert.Null(exception);
+    }
 
-        Assert.True(true);
+    [Fact]
+    public async void given_valid_jpeg_file_convert_to_jpeg_should_success()
+    {
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64JpgSample);
+        var exception = await Record.ExceptionAsync(() => _converter.ConvertAsync(imageStream));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async void given_valid_png_file_convert_to_jpeg_should_success()
+    {
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64PngSample);
+        var exception = await Record.ExceptionAsync(() => _converter.ConvertAsync(imageStream));
+        Assert.NotNull(exception);
+    }
+
+    [Fact]
+    public async Task given_invalid_image_file_convert_to_jpeg_throws_exception()
+    {
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64DocxSample);
+        var exception = await Record.ExceptionAsync(() => _converter.ConvertAsync(imageStream));
+        Assert.NotNull(exception);
+    }
+
+    [Theory]
+    [InlineData(-5)]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(101)]
+    public async Task given_invalid_image_quality_convert_to_jpeg_throws_exception(int imageQuality)
+    {
+        _options.Value.CompressedImageQuality = imageQuality;
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64PngSample);
+        var exception = await Record.ExceptionAsync(() => _converter.ConvertAsync(imageStream));
+        Assert.NotNull(exception);
     }
 
     private readonly IOptions<PhotoServiceOptions> _options;
+    private readonly JpegPhotoConverter _converter;
     public JpegPhotoConverterTests()
     {
         _options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
-        _options.Value.MinPhotoSizeBytes = 1;
-        _options.Value.MaxPhotoSizeBytes = 999999;
+        _options.Value.CompressedImageQuality = 80;
+        _converter = new JpegPhotoConverter(_options);
+    }
+
+    private static MemoryStream Base64ToMemoryStream(string base64)
+    {
+        var bytes = Convert.FromBase64String(base64);
+        return new MemoryStream(bytes);
     }
 }

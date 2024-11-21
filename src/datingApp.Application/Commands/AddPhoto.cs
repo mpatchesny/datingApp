437b92973fc3bcf1465d53be8cb54e91ba5df208
mpatchesny <strielok11+github@gmail.com>
2024-11-21 13:23:16 +0000

    remove

--- src/datingApp.Application/Commands/AddPhoto.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using datingApp.Application.Abstractions;
-
-namespace datingApp.Application.Commands;
-
-public sealed record AddPhoto(Guid PhotoId, Guid UserId, string Base64Bytes) : AuthenticatedCommandBase;

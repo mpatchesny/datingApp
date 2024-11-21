116ed3e57027bca135525aa7a4c9ee3104da2285
mpatchesny <strielok11+github@gmail.com>
2024-11-21 13:26:03 +0000

    move folder up, fix namespace

--- src/datingApp.Application/Commands/AddPhoto2.cs
+++ src/datingApp.Application/Commands/AddPhoto2.cs
@@ -4,6 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Application.Abstractions;
 
-namespace datingApp.Application.Commands.Handlers;
+namespace datingApp.Application.Commands;
 
 public sealed record AddPhoto2(Guid PhotoId, Guid UserId, Stream PhotoStream) : AuthenticatedCommandBase;

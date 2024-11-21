505d2e691aa1e07ded8c01328de584dd84bb5c09
mpatchesny <strielok11+github@gmail.com>
2024-11-21 13:06:38 +0000

    add

---
+++ src/datingApp.Application/Services/IPhotoConverter.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Application.Services;
+
+public interface IPhotoConverter
+{
+    public Task<Stream> ConvertAsync(Stream input, string targetFormat, int quality);
+}

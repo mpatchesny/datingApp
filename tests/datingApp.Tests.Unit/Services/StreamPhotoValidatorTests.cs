c51276443f5628de0c58d92f1a746e039d12b261
mpatchesny <strielok11+github@gmail.com>
2024-11-21 14:52:59 +0000

    fix tests

--- tests/datingApp.Tests.Unit/Services/StreamPhotoValidatorTests.cs
+++ tests/datingApp.Tests.Unit/Services/StreamPhotoValidatorTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using datingApp.Infrastructure;
@@ -13,118 +14,104 @@ namespace datingApp.Tests.Unit.Services;
 public class StreamPhotoValidatorTests
 {
     [Fact]
-    public void given_empty_base64_content_ProcessBase64Photo_throws_EmptyBase64StringException()
+    public void given_valid_file_which_is_not_image_ValidateExtension_throws_InvalidPhotoException()
     {
-        // var service = new PhotoService(_options);
+        _options.Value.MinPhotoSizeBytes = 1;
+        _options.Value.MaxPhotoSizeBytes = 999999;
+        var validator = new StreamPhotoValidator(_options);
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64DocxSample);
 
-        // var exception = Record.Exception(() => service.ProcessBase64Photo(""));
-        // Assert.NotNull(exception);
-        // Assert.IsType<EmptyBase64StringException>(exception);
+        var exception = Record.Exception(() => validator.ValidateExtension(imageStream, out var extension));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoException>(exception);
     }
 
     [Fact]
-    public void given_valid_file_which_is_not_image_ProcessBase64Photo_throws_InvalidPhotoException()
+    public void given_valid_unsupported_image_file_ValidateExtension_throws_InvalidPhotoException()
     {
-        // _options.Value.MinPhotoSizeBytes = 1;
-        // _options.Value.MaxPhotoSizeBytes = 999999;
-        // var service = new PhotoService(_options);
+        _options.Value.MinPhotoSizeBytes = 1;
+        _options.Value.MaxPhotoSizeBytes = 999999;
+        var validator = new StreamPhotoValidator(_options);
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64GifSample);
 
-        // var exception = Record.Exception(() => service.ProcessBase64Photo(_base64DocxSample));
-        // Assert.NotNull(exception);
-        // Assert.IsType<InvalidPhotoException>(exception);
+        var exception = Record.Exception(() => validator.ValidateExtension(imageStream, out var extension));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoException>(exception);
     }
 
     [Fact]
-    public void given_valid_unsupported_image_file_ProcessBase64Photo_throws_InvalidPhotoException()
+    public void given_valid_bmp_file_ValidateExtension_throws_InvalidPhotoException()
     {
-        // _options.Value.MinPhotoSizeBytes = 1;
-        // _options.Value.MaxPhotoSizeBytes = 999999;
-        // var service = new PhotoService(_options);
+        _options.Value.MinPhotoSizeBytes = 1;
+        _options.Value.MaxPhotoSizeBytes = 999999;
+        var validator = new StreamPhotoValidator(_options);
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64BmpSample);
 
-        // var exception = Record.Exception(() => service.ProcessBase64Photo(_base64GifSample));
-        // Assert.NotNull(exception);
-        // Assert.IsType<InvalidPhotoException>(exception);
+        var exception = Record.Exception(() => validator.ValidateExtension(imageStream, out var extension));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoException>(exception);
     }
 
     [Fact]
-    public void given_valid_bmp_file_ProcessBase64Photo_throws_InvalidPhotoException()
+    public void given_valid_jpg_file_ValidateExtension_return_jpg_extension()
     {
-        // _options.Value.MinPhotoSizeBytes = 1;
-        // _options.Value.MaxPhotoSizeBytes = 999999;
-        // var service = new PhotoService(_options);
+        _options.Value.MinPhotoSizeBytes = 1;
+        _options.Value.MaxPhotoSizeBytes = 999999;
+        var validator = new StreamPhotoValidator(_options);
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64JpgSample);
 
-        // var exception = Record.Exception(() => service.ProcessBase64Photo(_base64GifSample));
-        // Assert.NotNull(exception);
-        // Assert.IsType<InvalidPhotoException>(exception);
+        validator.ValidateExtension(imageStream, out var extension);
+        Assert.Equal("jpg", extension);
     }
 
     [Fact]
-    public void given_valid_jpg_file_ProcessBase64Photo_return_jpg_extension()
+    public void given_valid_png_file_ValidateExtension_return_jpg_extension()
     {
-        // _options.Value.MinPhotoSizeBytes = 1;
-        // _options.Value.MaxPhotoSizeBytes = 999999;
-        // var service = new PhotoService(_options);
+        _options.Value.MinPhotoSizeBytes = 1;
+        _options.Value.MaxPhotoSizeBytes = 999999;
+        var validator = new StreamPhotoValidator(_options);
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64PngSample);
 
-        // var output = service.ProcessBase64Photo(_base64JpgSample);
-        // Assert.Equal("jpg", output.Extension);
+        validator.ValidateExtension(imageStream, out var extension);
+        Assert.Equal("png", extension);
     }
 
     [Fact]
-    public void given_valid_png_file_ProcessBase64Photo_return_jpg_extension()
+    public void given_valid_webp_file_ValidateExtension_return_jpg_extension()
     {
-        // _options.Value.MinPhotoSizeBytes = 1;
-        // _options.Value.MaxPhotoSizeBytes = 999999;
-        // var service = new PhotoService(_options);
+        _options.Value.MinPhotoSizeBytes = 1;
+        _options.Value.MaxPhotoSizeBytes = 999999;
+        var validator = new StreamPhotoValidator(_options);
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64PngSample);
 
-        // var output = service.ProcessBase64Photo(_base64PngSample);
-        // Assert.Equal("jpg", output.Extension);
+        validator.ValidateExtension(imageStream, out var extension);
+        Assert.Equal("webp", extension);
     }
 
     [Fact]
-    public void given_valid_webp_file_ProcessBase64Photo_return_jpg_extension()
+    public void given_passed_valid_photo_is_too_small_ValidateSize_throws_InvalidPhotoSizeException()
     {
-        // _options.Value.MinPhotoSizeBytes = 1;
-        // _options.Value.MaxPhotoSizeBytes = 999999;
-        // var service = new PhotoService(_options);
+        _options.Value.MinPhotoSizeBytes = 380;
+        _options.Value.MaxPhotoSizeBytes = 390;
+        var validator = new StreamPhotoValidator(_options);
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64PngSample);
 
-        // var output = service.ProcessBase64Photo(_base64WebpSample);
-        // Assert.Equal("jpg", output.Extension);
+        var exception = Record.Exception(() => validator.ValidateSize(imageStream));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoSizeException>(exception);
     }
 
     [Fact]
-    public void given_invalid_base64_encoded_string_ProcessBase64Photo_throws_FailToConvertBase64StringToArrayOfBytesException()
+    public void given_passed_valid_photo_is_too_large_ValidateSize_throws_InvalidPhotoSizeException()
     {
-        // _options.Value.MinPhotoSizeBytes = 0;
-        // _options.Value.MaxPhotoSizeBytes = 999999;
-        // var service = new PhotoService(_options);
+        _options.Value.MinPhotoSizeBytes = 1;
+        _options.Value.MaxPhotoSizeBytes = 378;
+        var validator = new StreamPhotoValidator(_options);
+        var imageStream = Base64ToMemoryStream(ImageSamples.Base64PngSample);
 
-        // var exception = Record.Exception(() => service.ProcessBase64Photo("ddd"));
-        // Assert.NotNull(exception);
-        // Assert.IsType<FailToConvertBase64StringToArrayOfBytesException>(exception);
-    }
-
-    [Fact]
-    public void given_passed_valid_photo_is_too_small_ProcessBase64Photo_throws_InvalidPhotoSizeException()
-    {
-        // _options.Value.MinPhotoSizeBytes = 380;
-        // _options.Value.MaxPhotoSizeBytes = 390;
-        // var service = new PhotoService(_options);
-
-        // var exception = Record.Exception(() => service.ProcessBase64Photo(_base64PngSample));
-        // Assert.NotNull(exception);
-        // Assert.IsType<InvalidPhotoSizeException>(exception);
-    }
-
-    [Fact]
-    public void given_passed_valid_photo_is_too_large_ValidatePhoto_throws_InvalidPhotoSizeException()
-    {
-        // _options.Value.MinPhotoSizeBytes = 1;
-        // _options.Value.MaxPhotoSizeBytes = 378;
-        // var service = new PhotoService(_options);
-
-        // var exception = Record.Exception(() => service.ProcessBase64Photo(_base64PngSample));
-        // Assert.NotNull(exception);
-        // Assert.IsType<InvalidPhotoSizeException>(exception);
+        var exception = Record.Exception(() => validator.ValidateSize(imageStream));
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidPhotoSizeException>(exception);
     }
    
     private readonly IOptions<PhotoServiceOptions> _options;
@@ -133,5 +120,11 @@ public class StreamPhotoValidatorTests
         _options = Options.Create<PhotoServiceOptions>(new PhotoServiceOptions());
         _options.Value.MinPhotoSizeBytes = 1;
         _options.Value.MaxPhotoSizeBytes = 999999;
+    }
+
+    private static MemoryStream Base64ToMemoryStream(string base64)
+    {
+        var bytes = Convert.FromBase64String(base64);
+        return new MemoryStream(bytes);
     }
 }

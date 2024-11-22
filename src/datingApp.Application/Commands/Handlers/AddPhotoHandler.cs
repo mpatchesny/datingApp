944167d1bb425d3bf7c4d7ac2508d708ab3ae13a
mpatchesny <strielok11+github@gmail.com>
2024-11-22 08:54:02 +0000

    remove not implemented exception at the end

--- src/datingApp.Application/Commands/Handlers/AddPhotoHandler.cs
+++ src/datingApp.Application/Commands/Handlers/AddPhotoHandler.cs
@@ -55,7 +55,5 @@ public sealed class AddPhotoHandler : ICommandHandler<AddPhoto>
             _userRepository.UpdateAsync(user)
         };
         await Task.WhenAll(tasks);
-
-        throw new NotImplementedException();
     }
 }

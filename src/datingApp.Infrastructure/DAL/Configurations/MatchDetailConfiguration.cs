0d790eab406daed19348e24cb8df255511bc4f2d
M. Patchesny <strielok11+github@gmail.com>
2024-11-25 23:19:27 +0100

    add

---
+++ src/datingApp.Infrastructure/DAL/Configurations/MatchDetailConfiguration.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Core.Entities;
+using datingApp.Core.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace datingApp.Infrastructure.DAL.Configurations;
+
+internal sealed class MatchDetailConfiguration : IEntityTypeConfiguration<MatchDetail>
+{
+    public void Configure(EntityTypeBuilder<MatchDetail> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .HasConversion(x => x.Value, x => new MatchDetailId(x))
+            .IsRequired();
+        builder.Property(x => x.MatchId)
+            .HasConversion(x => x.Value, x => new MatchId(x))
+            .IsRequired();
+        builder.Property(x => x.UserId)
+            .HasConversion(x => x.Value, x => new UserId(x))
+            .IsRequired();
+        builder.HasOne<Match>()
+            .WithMany()
+            .HasForeignKey(x => x.MatchId);
+        builder.HasOne<User>()
+            .WithMany();
+        builder.Property(x => x.IsDisplayed)
+            .IsRequired();
+    }
+}

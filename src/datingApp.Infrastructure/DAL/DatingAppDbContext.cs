d01e01d723b298c1af65843f37fa82a2d6a5cbf3
M. Patchesny <strielok11+github@gmail.com>
2024-11-26 22:46:11 +0100

    remove unused dbSet

--- src/datingApp.Infrastructure/DAL/DatingAppDbContext.cs
+++ src/datingApp.Infrastructure/DAL/DatingAppDbContext.cs
@@ -13,7 +13,6 @@ internal sealed class DatingAppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Match> Matches { get; set; }
     public DbSet<MatchDetail> MatchDetails { get; set; }
-    public DbSet<Message> Messages { get; set; }
     public DbSet<Swipe> Swipes { get; set; }
     public DbSet<Photo> Photos { get; set; }
     public DbSet<AccessCodeDto> AccessCodes { get; set; }

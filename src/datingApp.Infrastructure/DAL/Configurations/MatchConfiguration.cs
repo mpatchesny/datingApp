aa6acea8e980cd47be86df5413d2d7c8af714200
mpatchesny <strielok11+github@gmail.com>
2024-11-28 11:58:45 +0000

    make UserId1, UserId2 readonly property

--- src/datingApp.Infrastructure/DAL/Configurations/MatchConfiguration.cs
+++ src/datingApp.Infrastructure/DAL/Configurations/MatchConfiguration.cs
@@ -17,12 +17,8 @@ internal sealed class MatchConfiguration : IEntityTypeConfiguration<Match>
         builder.Property(x => x.Id)
             .HasConversion(x => x.Value, x => new MatchId(x))
             .IsRequired();
-        builder.Property(x => x.UserId1)
-            .HasConversion(x => x.Value, x => new UserId(x))
-            .IsRequired();
-        builder.Property(x => x.UserId2)
-            .HasConversion(x => x.Value, x => new UserId(x))
-            .IsRequired();
+        builder.Ignore(x => x.UserId1);
+        builder.Ignore(x => x.UserId2);
         builder.Property(x => x.CreatedAt)
             .IsRequired();
         builder.Navigation(x => x.MatchDetails).AutoInclude();

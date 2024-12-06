dea90310f9db7769c4d98c09696cee6a261fba55
M. Patchesny <strielok11+github@gmail.com>
2024-12-06 20:10:43 +0100

    change namespace formatting

--- src/datingApp.Infrastructure/Security/DbAccessCodeStorage.cs
+++ src/datingApp.Infrastructure/Security/DbAccessCodeStorage.cs
@@ -6,32 +6,31 @@ using datingApp.Application.DTO;
 using datingApp.Application.Security;
 using Microsoft.EntityFrameworkCore;
 
-namespace datingApp.Infrastructure.Security
-{
-    internal sealed class DbAccessCodeStorage : IAccessCodeStorage
-    {
-        private readonly DatingAppDbContext _dbContext;
-        public DbAccessCodeStorage(DatingAppDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        public AccessCodeDto Get(string emailOrPhone)
-        {
-            return _dbContext.AccessCodes.FirstOrDefault(x => x.EmailOrPhone == emailOrPhone);
-        }
+namespace datingApp.Infrastructure.Security;
 
-        public void Set(AccessCodeDto code)
+internal sealed class DbAccessCodeStorage : IAccessCodeStorage
+{
+    private readonly DatingAppDbContext _dbContext;
+    public DbAccessCodeStorage(DatingAppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public AccessCodeDto Get(string emailOrPhone)
+    {
+        return _dbContext.AccessCodes.FirstOrDefault(x => x.EmailOrPhone == emailOrPhone);
+    }
+
+    public void Set(AccessCodeDto code)
+    {
+        var existingCode = _dbContext.AccessCodes.FirstOrDefault(x => x.EmailOrPhone == code.EmailOrPhone);
+        if (existingCode != null)
         {
-            var existingCode = _dbContext.AccessCodes.FirstOrDefault(x => x.EmailOrPhone == code.EmailOrPhone);
-            if (existingCode != null)
-            {
-                _dbContext.Entry(existingCode).CurrentValues.SetValues(code);
-            }
-            else
-            {
-                _dbContext.AccessCodes.Add(code);
-            }
-            _dbContext.SaveChanges();
+            _dbContext.Entry(existingCode).CurrentValues.SetValues(code);
         }
+        else
+        {
+            _dbContext.AccessCodes.Add(code);
+        }
+        _dbContext.SaveChanges();
     }
 }

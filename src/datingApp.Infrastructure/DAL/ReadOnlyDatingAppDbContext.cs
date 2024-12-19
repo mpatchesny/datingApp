a7c9ba2ed2d8fa2e602da74f3f4f6b6541f865ff
M. Patchesny <strielok11+github@gmail.com>
2024-12-19 22:59:02 +0100

    remove not used dbsets

--- src/datingApp.Infrastructure/DAL/ReadOnlyDatingAppDbContext.cs
+++ src/datingApp.Infrastructure/DAL/ReadOnlyDatingAppDbContext.cs
@@ -13,10 +13,6 @@ internal sealed class ReadOnlyDatingAppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Match> Matches { get; set; }
     public DbSet<Swipe> Swipes { get; set; }
-    public DbSet<Photo> Photos { get; set; }
-    public DbSet<AccessCodeDto> AccessCodes { get; set; }
-    public DbSet<DeletedEntityDto> DeletedEntities { get; set; }
-    public DbSet<TokenDto> RevokedRefreshTokens { get; set; }
 
     public ReadOnlyDatingAppDbContext(DbContextOptions<ReadOnlyDatingAppDbContext> options) : base(options)
     {

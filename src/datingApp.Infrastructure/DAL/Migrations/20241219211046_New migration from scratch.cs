5eee2637852f7be671eea15563de9de1f5a3c63e
M. Patchesny <strielok11+github@gmail.com>
2024-12-19 22:19:16 +0100

    new migration from scratch

---
+++ src/datingApp.Infrastructure/DAL/Migrations/20241219211046_New migration from scratch.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace datingApp.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Newmigrationfromscratch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccessCodes",
+                columns: table => new
+                {
+                    EmailOrPhone = table.Column<string>(type: "text", nullable: false),
+                    AccessCode = table.Column<string>(type: "text", nullable: false),
+                    ExpirationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Expiry = table.Column<TimeSpan>(type: "interval", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessCodes", x => x.EmailOrPhone);
+                })
+                .Annotation("Npgsql:UnloggedTable", true);
+
+            migrationBuilder.CreateTable(
+                name: "DeletedEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeletedEntities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RevokedRefreshTokens",
+                columns: table => new
+                {
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    ExpirationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RevokedRefreshTokens", x => x.Token);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Swipes",
+                columns: table => new
+                {
+                    SwipedById = table.Column<Guid>(type: "uuid", nullable: false),
+                    SwipedWhoId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Like = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Swipes", x => new { x.SwipedById, x.SwipedWhoId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Phone = table.Column<string>(type: "character varying(9)", maxLength: 9, nullable: false),
+                    Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Name = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: false),
+                    Sex = table.Column<int>(type: "integer", nullable: false),
+                    Job = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Bio = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MatchDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MatchId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsDisplayed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MatchDetails_Matches_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "Matches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MatchDetails_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: false),
+                    Oridinal = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSettings",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PreferredSex = table.Column<int>(type: "integer", nullable: false),
+                    PreferredAge_From = table.Column<int>(type: "integer", nullable: true),
+                    PreferredAge_To = table.Column<int>(type: "integer", nullable: true),
+                    PreferredMaxDistance = table.Column<int>(type: "integer", nullable: false),
+                    Location_Lat = table.Column<double>(type: "double precision", nullable: true),
+                    Location_Lon = table.Column<double>(type: "double precision", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSettings", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_UserSettings_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SendFromId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Text = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    IsDisplayed = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    MatchDetailId = table.Column<Guid>(type: "uuid", nullable: true),
+                    MatchId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_MatchDetails_MatchDetailId",
+                        column: x => x.MatchDetailId,
+                        principalTable: "MatchDetails",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Messages_Matches_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "Matches",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_SendFromId",
+                        column: x => x.SendFromId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeletedEntities_Id",
+                table: "DeletedEntities",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchDetails_MatchId",
+                table: "MatchDetails",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchDetails_UserId",
+                table: "MatchDetails",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_CreatedAt",
+                table: "Messages",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_MatchDetailId",
+                table: "Messages",
+                column: "MatchDetailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_MatchId",
+                table: "Messages",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SendFromId",
+                table: "Messages",
+                column: "SendFromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_UserId",
+                table: "Photos",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Swipes_SwipedById_SwipedWhoId_Like",
+                table: "Swipes",
+                columns: new[] { "SwipedById", "SwipedWhoId", "Like" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Sex_DateOfBirth",
+                table: "Users",
+                columns: new[] { "Sex", "DateOfBirth" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccessCodes");
+
+            migrationBuilder.DropTable(
+                name: "DeletedEntities");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropTable(
+                name: "RevokedRefreshTokens");
+
+            migrationBuilder.DropTable(
+                name: "Swipes");
+
+            migrationBuilder.DropTable(
+                name: "UserSettings");
+
+            migrationBuilder.DropTable(
+                name: "MatchDetails");
+
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

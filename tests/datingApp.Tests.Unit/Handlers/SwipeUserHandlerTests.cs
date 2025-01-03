1289215bf3c2a1cd2f495d8499b367af68828090
M. Patchesny <strielok11+github@gmail.com>
2025-01-03 22:19:50 +0100

    fix tests

--- tests/datingApp.Tests.Unit/Handlers/SwipeUserHandlerTests.cs
+++ tests/datingApp.Tests.Unit/Handlers/SwipeUserHandlerTests.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using datingApp.Application.Commands;
 using datingApp.Application.Commands.Handlers;
 using datingApp.Application.DTO;
+using datingApp.Application.Services;
 using datingApp.Application.Storage;
 using datingApp.Core.Consts;
 using datingApp.Core.Entities;
@@ -32,18 +33,23 @@ public class SwipeUserHandlerTests
         swipeRepository.Setup(x => x.AddAsync(It.IsAny<Swipe>()))
             .Callback<Swipe>(s => addedSwipe = s);
 
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+
         var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
         var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
         isLikedByOtherUserStorage.Setup(x => x.Set(It.IsAny<IsLikedByOtherUserDto>()))
             .Callback<IsLikedByOtherUserDto>(s => isLikedByOtherUser = s);
 
         var like = 1;
-        var command = new SwipeUser(Guid.NewGuid(), Guid.NewGuid(), like);
-        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, isLikedByOtherUserStorage.Object);
+        var swipedWhoId = Guid.NewGuid();
+        var command = new SwipeUser(Guid.NewGuid(), swipedWhoId, like);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
         await handler.HandleAsync(command);
 
         swipeRepository.Verify(x => x.AddAsync(addedSwipe), Times.Once());
         isLikedByOtherUserStorage.Verify(x => x.Set(isLikedByOtherUser), Times.Once());
+        deletedEntitiesService.Verify(x => x.ExistsAsync(swipedWhoId), Times.Never());
         Assert.Equal(like, (int) addedSwipe.Like);
         Assert.Equal(command.SwipedById, addedSwipe.SwipedById.Value);
         Assert.Equal(command.SwipedWhoId, addedSwipe.SwipedWhoId.Value);
@@ -59,10 +65,14 @@ public class SwipeUserHandlerTests
         var userId2 = Guid.NewGuid();
         var swipe = new Swipe(userId1, userId2, Like.Like, DateTime.UtcNow);
         var swipeRepository = new Mock<ISwipeRepository>();
-        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>())).Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe }));
+        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>()))
+            .Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe }));
         var addedSwipe = (Swipe) null;
         swipeRepository.Setup(x => x.AddAsync(It.IsAny<Swipe>()))
             .Callback<Swipe>(s => addedSwipe = s);
+
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(false));
 
         var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
         var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
@@ -71,7 +81,7 @@ public class SwipeUserHandlerTests
 
         var like = (int) Like.Like;
         var command = new SwipeUser(userId1, userId2, like);
-        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, isLikedByOtherUserStorage.Object);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
         await handler.HandleAsync(command);
 
         swipeRepository.Verify(x => x.AddAsync(addedSwipe), Times.Never());
@@ -88,7 +98,11 @@ public class SwipeUserHandlerTests
         var userId2 = Guid.NewGuid();
         var swipe = new Swipe(userId1, userId2, Like.Like, DateTime.UtcNow);
         var swipeRepository = new Mock<ISwipeRepository>();
-        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>())).Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe }));
+        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>()))
+            .Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe }));
+
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(false));
 
         var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
         var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
@@ -97,9 +111,10 @@ public class SwipeUserHandlerTests
 
         var like = 1;
         var command = new SwipeUser(userId1, userId2, like);
-        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, isLikedByOtherUserStorage.Object);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
         await handler.HandleAsync(command);
 
+        deletedEntitiesService.Verify(x => x.ExistsAsync(userId2), Times.Never());
         Assert.False(isLikedByOtherUser.IsLikedByOtherUser);
         isLikedByOtherUserStorage.Verify(x => x.Set(isLikedByOtherUser), Times.Once());
     }
@@ -115,7 +130,11 @@ public class SwipeUserHandlerTests
         var swipe = new Swipe(userId1, userId2, Like.Like, DateTime.UtcNow);
         var otherUserSwipe = new Swipe(userId2, userId1, Like.Pass, DateTime.UtcNow);
         var swipeRepository = new Mock<ISwipeRepository>();
-        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>())).Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe, otherUserSwipe }));
+        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>()))
+            .Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe, otherUserSwipe }));
+
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(false));
 
         var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
         var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
@@ -124,7 +143,7 @@ public class SwipeUserHandlerTests
 
         var like = (int) Like.Like;
         var command = new SwipeUser(userId1, userId2, like);
-        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, isLikedByOtherUserStorage.Object);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
         await handler.HandleAsync(command);
 
         Assert.False(isLikedByOtherUser.IsLikedByOtherUser);
@@ -142,7 +161,11 @@ public class SwipeUserHandlerTests
         var swipe = new Swipe(userId1, userId2, Like.Like, DateTime.UtcNow);
         var otherUserSwipe = new Swipe(userId2, userId1, Like.Like, DateTime.UtcNow);
         var swipeRepository = new Mock<ISwipeRepository>();
-        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>())).Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe, otherUserSwipe }));
+        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>()))
+            .Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe, otherUserSwipe }));
+
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(false));
 
         var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
         var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
@@ -151,7 +174,7 @@ public class SwipeUserHandlerTests
 
         var like = (int) Like.Like;
         var command = new SwipeUser(userId1, userId2, like);
-        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, isLikedByOtherUserStorage.Object);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
         await handler.HandleAsync(command);
 
         Assert.True(isLikedByOtherUser.IsLikedByOtherUser);
@@ -169,7 +192,11 @@ public class SwipeUserHandlerTests
         var swipe = new Swipe(userId1, userId2, Like.Like, DateTime.UtcNow);
         var otherUserSwipe = new Swipe(userId2, userId1, Like.Like, DateTime.UtcNow);
         var swipeRepository = new Mock<ISwipeRepository>();
-        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>())).Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe, otherUserSwipe }));
+        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>()))
+            .Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe, otherUserSwipe }));
+
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(false));
 
         var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
         var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
@@ -178,7 +205,7 @@ public class SwipeUserHandlerTests
 
         var like = (int) Like.Like;
         var command = new SwipeUser(userId1, userId2, like);
-        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, isLikedByOtherUserStorage.Object);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
         await handler.HandleAsync(command);
 
         Assert.True(isLikedByOtherUser.IsLikedByOtherUser);
@@ -186,7 +213,7 @@ public class SwipeUserHandlerTests
     }
 
     [Fact]
-    public async void given_two_users_liked_each_ohter_SwipeUserHanlder_adds_new_match_1()
+    public async void given_two_users_liked_each_other_and_other_user_is_not_in_deleted_entities_SwipeUserHanlder_adds_new_match_1()
     {
         var matchRepository = new Mock<IMatchRepository>();
         Match match = null;
@@ -197,7 +224,11 @@ public class SwipeUserHandlerTests
         var userId2 = Guid.NewGuid();
         var otherUserSwipe = new Swipe(userId2, userId1, Like.Like, DateTime.UtcNow);
         var swipeRepository = new Mock<ISwipeRepository>();
-        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>())).Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ otherUserSwipe }));
+        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>()))
+            .Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ otherUserSwipe }));
+
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(false));
 
         var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
         var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
@@ -206,17 +237,18 @@ public class SwipeUserHandlerTests
 
         var like = (int) Like.Like;
         var command = new SwipeUser(userId1, userId2, like);
-        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, isLikedByOtherUserStorage.Object);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
         await handler.HandleAsync(command);
 
         matchRepository.Verify(x => x.AddAsync(match), Times.Once());
+        deletedEntitiesService.Verify(x => x.ExistsAsync(userId2), Times.Once());
         Assert.Equal(userId1, match.UserId1.Value);
         Assert.Equal(userId2, match.UserId2.Value);
         isLikedByOtherUserStorage.Verify(x => x.Set(isLikedByOtherUser), Times.Once());
     }
-    
+
     [Fact]
-    public async void given_two_users_liked_each_ohter_SwipeUserHanlder_adds_new_match_2()
+    public async void given_two_users_liked_each_other_and_other_user_is_not_in_deleted_entities_SwipeUserHanlder_adds_new_match_2()
     {
         var matchRepository = new Mock<IMatchRepository>();
         Match match = null;
@@ -231,6 +263,9 @@ public class SwipeUserHandlerTests
         swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>()))
             .Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe, otherUserSwipe }));
 
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(false));
+
         var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
         var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
         isLikedByOtherUserStorage.Setup(x => x.Set(It.IsAny<IsLikedByOtherUserDto>()))
@@ -238,12 +273,48 @@ public class SwipeUserHandlerTests
 
         var like = (int) Like.Like;
         var command = new SwipeUser(userId1, userId2, like);
-        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, isLikedByOtherUserStorage.Object);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
         await handler.HandleAsync(command);
 
         matchRepository.Verify(x => x.AddAsync(match), Times.Once());
+                deletedEntitiesService.Verify(x => x.ExistsAsync(userId2), Times.Once());
         Assert.Equal(userId1, match.UserId1.Value);
         Assert.Equal(userId2, match.UserId2.Value);
         isLikedByOtherUserStorage.Verify(x => x.Set(isLikedByOtherUser), Times.Once());
+    }
+
+    [Fact]
+    public async void given_two_users_liked_each_other_and_other_user_is_in_deleted_entities_SwipeUserHanlder_do_not_add_new_match_and_returns_false()
+    {
+        var matchRepository = new Mock<IMatchRepository>();
+        Match match = null;
+        matchRepository.Setup(x => x.AddAsync(It.IsAny<Match>()))
+            .Callback<Match>(s => match = s);
+
+        var userId1 = Guid.NewGuid();
+        var userId2 = Guid.NewGuid();
+        var swipe =new Swipe(userId1, userId2, Like.Like, DateTime.UtcNow);
+        var otherUserSwipe = new Swipe(userId2, userId1, Like.Like, DateTime.UtcNow);
+        var swipeRepository = new Mock<ISwipeRepository>();
+        swipeRepository.Setup(x => x.GetBySwipedBy(It.IsAny<UserId>(), It.IsAny<UserId>()))
+            .Returns(Task.FromResult<List<Swipe>>(new List<Swipe>(){ swipe, otherUserSwipe }));
+
+        var deletedEntitiesService = new Mock<IDeletedEntityService>();
+        deletedEntitiesService.Setup(x => x.ExistsAsync(It.IsAny<Guid>())).Returns(Task.FromResult(true));
+
+        var isLikedByOtherUserStorage = new Mock<IIsLikedByOtherUserStorage>();
+        var isLikedByOtherUser = (IsLikedByOtherUserDto) null;
+        isLikedByOtherUserStorage.Setup(x => x.Set(It.IsAny<IsLikedByOtherUserDto>()))
+            .Callback<IsLikedByOtherUserDto>(s => isLikedByOtherUser = s);
+
+        var like = (int) Like.Like;
+        var command = new SwipeUser(userId1, userId2, like);
+        var handler = new SwipeUserHandler(swipeRepository.Object, matchRepository.Object, deletedEntitiesService.Object, isLikedByOtherUserStorage.Object);
+        await handler.HandleAsync(command);
+
+        matchRepository.Verify(x => x.AddAsync(match), Times.Never());
+        deletedEntitiesService.Verify(x => x.ExistsAsync(userId2), Times.Once());
+        isLikedByOtherUserStorage.Verify(x => x.Set(isLikedByOtherUser), Times.Once());
+        Assert.False(isLikedByOtherUser.IsLikedByOtherUser);
     }
 }

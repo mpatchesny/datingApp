2798a56f0fe290b80794fe06a4fa943911d479a9
mpatchesny <strielok11+github@gmail.com>
2025-01-03 11:03:28 +0000

    fix tests, add one test

--- tests/datingApp.Tests.Integration/Repositories/MatchRepositoryTests.cs
+++ tests/datingApp.Tests.Integration/Repositories/MatchRepositoryTests.cs
@@ -90,7 +90,7 @@ public class MatchRepositoryTests : IDisposable
         _dbContext.ChangeTracker.Clear();
 
         var match2 = new Match(match.Id, user1.Id, user3.Id, DateTime.UtcNow);
-        var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(match));
+        var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(match2));
         Assert.NotNull(exception);
     }
 
@@ -103,6 +103,17 @@ public class MatchRepositoryTests : IDisposable
         _dbContext.ChangeTracker.Clear();
 
         var match2 = new Match(Guid.NewGuid(), user1.Id, user2.Id, DateTime.UtcNow);
+        var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(match2));
+        Assert.NotNull(exception);
+    }
+
+    [Fact]
+    public async void add_match_with_user_id_that_not_exists_in_Users_throws_exception()
+    {
+        var user1 = await IntegrationTestHelper.CreateUserAsync(_dbContext);
+        _dbContext.ChangeTracker.Clear();
+
+        var match = new Match(Guid.NewGuid(), user1.Id, Guid.NewGuid(), DateTime.UtcNow);
         var exception = await Record.ExceptionAsync(async () => await _repository.AddAsync(match));
         Assert.NotNull(exception);
     }

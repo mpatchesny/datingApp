66d9ac67e9c6f3bf1d16e35a55f0069631ded7bd
M. Patchesny <strielok11+github@gmail.com>
2025-01-04 16:52:58 +0100

    add migration

---
+++ src/datingApp.Infrastructure/DAL/Migrations/20250104155247_Add indices to match, remove index from swipes.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace datingApp.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addindicestomatchremoveindexfromswipes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Swipes_SwipedById_SwipedWhoId_Like",
+                table: "Swipes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_CreatedAt",
+                table: "Matches",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_LastActivityTime",
+                table: "Matches",
+                column: "LastActivityTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Matches_CreatedAt",
+                table: "Matches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Matches_LastActivityTime",
+                table: "Matches");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Swipes_SwipedById_SwipedWhoId_Like",
+                table: "Swipes",
+                columns: new[] { "SwipedById", "SwipedWhoId", "Like" },
+                unique: true);
+        }
+    }
+}

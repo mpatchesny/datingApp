9e5cd761087fb9fb192b7647c81365fe3c076f41
M. Patchesny <strielok11+github@gmail.com>
2025-02-09 22:33:37 +0100

    add

---
+++ src/datingApp.Infrastructure/Services/DbPhotoDuplicateChecker.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingApp.Application.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace datingApp.Infrastructure.Services;
+
+internal sealed class DbPhotoDuplicateChecker : IPhotoDuplicateChecker
+{
+    private readonly DatingAppDbContext _dbContext;
+    public DbPhotoDuplicateChecker(DatingAppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<bool> IsDuplicate(Guid userId, Stream photo)
+    {
+        using (var md5 = System.Security.Cryptography.MD5.Create())
+        {
+            var hashBytes = await md5.ComputeHashAsync(photo);
+            var checksum = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+            var query = _dbContext
+                .Users.AnyAsync(user => user.Id.Equals(userId) &&
+                    user.Photos.Any(photo => photo.Checksum.Equals(checksum)));
+            return await query;
+        }
+    }
+}

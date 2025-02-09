cdf90f673b7f809a8b4461705f1d166916befbeb
M. Patchesny <strielok11+github@gmail.com>
2025-02-10 00:02:42 +0100

    add migration

---
+++ src/datingApp.Infrastructure/DAL/Migrations/20250209230225_Add checksum to photo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace datingApp.Infrastructure.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addchecksumtophoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Checksum",
+                table: "Photos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Checksum",
+                table: "Photos");
+        }
+    }
+}

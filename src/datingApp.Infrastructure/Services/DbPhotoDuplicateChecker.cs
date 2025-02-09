c7dc9624dee84d57eddcae1b165a53aa33cd4fd1
M. Patchesny <strielok11+github@gmail.com>
2025-02-09 23:49:34 +0100

    refactor

--- src/datingApp.Infrastructure/Services/DbPhotoDuplicateChecker.cs
+++ src/datingApp.Infrastructure/Services/DbPhotoDuplicateChecker.cs
@@ -17,15 +17,18 @@ internal sealed class DbPhotoDuplicateChecker : IPhotoDuplicateChecker
 
     public async Task<bool> IsDuplicate(Guid userId, Stream photo)
     {
-        using (var md5 = System.Security.Cryptography.MD5.Create())
-        {
-            photo.Position = 0;
-            var hashBytes = await md5.ComputeHashAsync(photo);
-            var checksum = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
-            var query = _dbContext
-                .Users.AnyAsync(user => user.Id.Equals(userId) &&
-                    user.Photos.Any(photo => photo.Checksum.Equals(checksum)));
-            return await query;
-        }
+        var checksum = await ComputeHashAsync(photo);
+        var query = _dbContext
+            .Users.AnyAsync(user => user.Id.Equals(userId) &&
+                user.Photos.Any(photo => photo.Checksum.Equals(checksum)));
+        return await query;
+    }
+
+    private static async Task<string> ComputeHashAsync(Stream stream)
+    {
+        stream.Position = 0;
+        var hashBytes = await System.Security.Cryptography.MD5.Create()
+            .ComputeHashAsync(stream);
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
     }
 }

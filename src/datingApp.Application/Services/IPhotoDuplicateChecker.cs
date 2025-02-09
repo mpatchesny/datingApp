51c4e9b9e2277c777420ee04cc47b9525b2a18b7
M. Patchesny <strielok11+github@gmail.com>
2025-02-09 22:21:29 +0100

    add

---
+++ src/datingApp.Application/Services/IPhotoDuplicateChecker.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace datingApp.Application.Services
+{
+    public interface IPhotoDuplicateChecker
+    {
+        public Task<bool> IsDuplicate(Stream photo);
+    }
+}

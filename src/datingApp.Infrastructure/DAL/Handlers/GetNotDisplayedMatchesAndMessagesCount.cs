6b777850f7a4812be7752875bc18b029b4b70703
M. Patchesny <strielok11+github@gmail.com>
2025-04-23 22:36:28 +0200

    add

---
+++ src/datingApp.Infrastructure/DAL/Handlers/GetNotDisplayedMatchesAndMessagesCount.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using datingApp.Application.Exceptions;
+using datingApp.Application.Queries;
+
+namespace datingApp.Infrastructure.DAL.Handlers;
+
+internal sealed class GetNotDisplayedMatchesAndMessagesCount
+{
+    private ReadOnlyDatingAppDbContext _dbContext;
+    public GetNotDisplayedMatchesAndMessagesCount(ReadOnlyDatingAppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Tuple<int, int>> HandleAsync(GetNotDisplayedMatchesAndMessages query)
+    {
+        if (!await _dbContext.Users.AnyAsync(user => user.Id.Equals(query.UserId)))
+        {
+            throw new UserNotExistsException(query.UserId);
+        }
+
+        var notDisplayedMessagesCountQuery = _dbContext.Matches
+            .Where(match => match.Users.Any(user => user.Id.Equals(query.UserId)))
+            .SelectMany(match => match.Messages)
+            .Where(message => !message.IsDisplayed && !message.SendFromId.Equals(query.UserId))
+            .CountAsync();
+
+        var notDisplayedMatchesCountQuery = _dbContext.Matches
+            .Where(match => match.Users.Any(user => user.Id.Equals(query.UserId)))
+            .Where(match => !match.IsDisplayedByUser(query.UserId))
+            .CountAsync();
+
+        var notDisplayedMessagesCount = await notDisplayedMessagesCountQuery;
+        var notDisplayedMatchesCount = await notDisplayedMatchesCountQuery;
+
+        return new Tuple<int, int>(notDisplayedMatchesCount, notDisplayedMessagesCount);
+    }
+}
